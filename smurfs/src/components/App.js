0d599fdf6c39f97a24c612b9281b000bd680875c
Julian Kohlman <juliankohlman@gmail.com>
2018-01-26 14:00:39 -0500

    Create POST view/component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getSmurfs } from '../actions';
+import { connect } from 'react-redux';
 /*
  to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
+ I'll let you do this part on your own.
  Just remember, `how do I connect my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
 class App extends Component {
+
+  // component did mount
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,4 +28,17 @@ class App extends Component {
   }
 }
 
-export default App;
+// mapStateToProps
+const mapStateToProps = state => {
+  console.log(state)
+  return {
+    smurfs: state.smurfs,
+    fetching: state.fetching,
+    fetched: state.fetched,
+    error: state.error
+  }
+}
+
+// connect
+export default connect(mapStateToProps, { getSmurfs })(App);
+// export default App;
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,14 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import { combineReducers } from 'redux'
+import {
+  FETCHING_SMURFS,
+  SMURFS_FETCHED,
+  ADDING_SMURF,
+  SMURF_ADDED,
+  ERROR
+} from '../actions';
 /*
  Your initial/default state for this project could look a lot like this
  {
@@ -13,10 +20,35 @@
    error: true
  }
 */
+const initState = {
+  smurfs: [],
+  fetching: false,
+  fetched: false,
+  error: null
+};
 
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+
+function smurfReducer(state = initState, action) {
+  switch (action.type) {
+    case FETCHING_SMURFS:
+      return { ...state, fetching: true };
+    case SMURFS_FETCHED:
+      return { ...state, fetching: false, fetched: true, friends: action.payload };
+    // ADDING_SMURF
+    // SMURF ADDED
+    case ERROR:
+      return { ...state, fetching: false, error: action.payload };
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({smurfReducer});
+
+export default rootReducer;
