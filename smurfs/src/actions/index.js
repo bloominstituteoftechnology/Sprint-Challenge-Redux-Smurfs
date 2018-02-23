7d680d452dbf30bead2cd2ad065c418a0f9cbe87
Peter Gray <peter@petergraycreative.com>
2018-02-23 17:37:19 -0600

    Create basic actions, and get reducer
    Also added in middleware to src/index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,14 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
+
+export const ERROR = 'ERROR';
+export const GET_SMURFS = 'GET_SMURFS';
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const CREATING_SMURF = 'CREATING_SMURF';
+export const CREATE_SMURF = 'CREATE_SMURF';
+export const UPDATE_SMURF = 'UPDATE_SMURF';
+export const DELETE_SMURF = 'DELETE_SMURF';
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const DELETING_SMURF = 'DELETING_SMURF';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +20,18 @@
    U - updateSmurf
    D - deleteSmurf
 */
+const URL = 'http://localhost:5000/smurfs';
+
+export const getSmurfs = () => {
+  const friends = axios.get(`${URL}/get`);
+  return dispatch => {
+    dispatch({ type: GETTING_SMURFS });
+    friends
+      .then(response => {
+        dispatch({ type: GET_SMURFS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
