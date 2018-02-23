d395c8e762ebb38609821cae48b7b9cc41b1dbcb
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-23 16:58:52 -0600

    add reducer with fetching functionality

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,22 +1,33 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { FETCHING, FETCHED, ERROR_FETCHING } from '../actions';
 
-/*
- Your initial/default state for this project could look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurfs: false
-   error: true
- }
-*/
+const initialState = {
+  fetching: false,
+  fetched: false,
+  error: null,
+  smurfs: [],
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  Components can read your store as, `state` and not `state.fooReducer`.
-*/
+const smurfs = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING:
+      return {
+        ...state,
+        fetching: true,
+      };
+    case FETCHED:
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        smurfs: action.payload,
+      };
+    case ERROR_FETCHING:
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload,
+      };
+  }
+};
+
+export default smurfs;
