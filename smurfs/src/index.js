e619aefd736f59e3c16e6f2e4e69b08b36a8da52
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-02-23 16:48:17 -0600

    Install Dependencies
    install redux, react-redux, thunk, logger, and axios in smurfs dir

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sore of reducer */ './reducers';
+import rootReducer from './reducers';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk, logger),
 );
+
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
