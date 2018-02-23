2bcc3ff626e36cf13e8a6f65df7a8d693443f724
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-23 17:57:11 -0600

    add adding functionality to reducer
    lines 5-7 import add types from actions
    
    lines 13-14 initial state now includes adding and added

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,8 +1,17 @@
-import { FETCHING, FETCHED, ERROR_FETCHING } from '../actions';
+import {
+  FETCHING,
+  FETCHED,
+  ERROR_FETCHING,
+  ADDING,
+  ADDED,
+  ERROR_ADDING,
+} from '../actions';
 
 const initialState = {
   fetching: false,
   fetched: false,
+  adding: false,
+  added: false,
   error: null,
   smurfs: [],
 };
@@ -27,6 +36,23 @@ const smurfs = (state = initialState, action) => {
         fetching: false,
         error: action.payload,
       };
+    case ADDING:
+      return {
+        ...state,
+        adding: true,
+      }
+    case ADDED:
+      return {
+        ...state,
+        adding: false,
+        added: true,
+        smurfs: [...smurfs, action.payload]
+      }
+    case ERROR_ADDING:
+      return {
+        ...state,
+        error: action.payload,
+      }
     default:
       return state;
   }
