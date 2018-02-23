95f6e39fc85472483a578f835453174cfc34c505
Smithface <kylewibblesmith@gmail.com>
2018-02-23 18:53:06 -0500

    completed SmurfList.js
    again, its ugly and in need of styling

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const SmurfList = (props) => {
-  return null;
+  return (
+    <ul>
+      {props.smurfs.map((smurf, i) => {
+        return <li key={smurf.id}>
+          <div>Name</div>
+          <div>{smurf.name}</div>
+          <div>Age</div>
+          <div>{smurf.age} smurfs old</div>
+          <div>Height</div>
+          <div>{smurf.height} cm tall</div>
+        </li>
+      })}
+    </ul>
+  );
 };
 
 export default SmurfList;
