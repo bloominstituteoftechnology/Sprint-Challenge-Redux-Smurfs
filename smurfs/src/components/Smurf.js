25ab3f723b1e8068a50adf9f78a6c04397040ea6
Mephestys <mephestys@gmail.com>
2018-02-23 17:57:09 -0700

    Added minimal styling for smurf list display. - Removed bullet points, added horizontal rule seperator.

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -7,6 +7,7 @@ class Smurf extends React.Component {
         Name: {this.props.smurf.name} <br />
         Age: {this.props.smurf.age} years old<br />
         Height: {this.props.smurf.height} cm tall<br />
+        <hr />
       </li>
     );
   };
