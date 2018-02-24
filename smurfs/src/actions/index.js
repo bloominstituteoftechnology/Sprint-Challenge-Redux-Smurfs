68ff3747c66312aec9b22fb185ae55167eb5ac88
Mephestys <mephestys@gmail.com>
2018-02-23 17:00:21 -0700

    Changes in this commit: - Added actions and reducer (only supports get and create). - Redux store and middleware established.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,42 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
+
+export const ERROR = 'ERROR';
+export const GET_SMURFS = 'GET_SMURFS';
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const ADD_SMURF = 'ADD_SMURF';
+export const ADDING_SMURF = 'ADDING_SMURF';
+
+const URL = `http://localhost:3333/`;
+
+export const getSmurfs = () => {
+  const smurfs = axios.get(`${URL}/smurfs`);
+  return dispatch => {
+    dispatch({ type: GETTING_SMURFS });
+    smurfs
+      .then(response => {
+        dispatch({ type: GET_SMURFS, payload: response.data });
+      })
+      .catch(error => {
+        dispatch({ type: ERROR, payload: error });
+      });
+  };
+};
+
+export const addSmurf = smurf => {
+  const newSmurf = axios.post(`${URL}/smurfs`, smurf);
+  return dispatch => {
+    dispatch({ type: ADDING_SMURF });
+    newSmurf
+      .then(({ data }) => {
+        dispatch({ type: ADD_SMURF, payload: data });
+      })
+      .catch(error => {
+        dispatch({ type: ERROR, payload: error });
+      });
+  };
+};
+
+// TODO updateSmurf, deleteSmurf
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,35 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import * as actionTypes from '../actions';
 
-/*
- Your initial/default state for this project could look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurfs: false
-   error: true
- }
-*/
+const initialState = {
+  smurfs: [],
+  gettingSmurfs: false,
+  addingSmurf: false,
+  // updatingSmurf: false,
+  // deletingSmurfs: false,
+  error: true,
+}
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.GETTING_SMURFS:
+      return { ...state, gettingSmurfs: true };
+    case actionTypes.GET_SMURFS:
+      return { ...state, smurfs: action.payload, gettingSmurfs: false };
+    case actionTypes.ADDING_SMURF:
+      return { ...state, addingSmurf: true};
+    case actionTypes.ADD_SMURF:
+      return { ...state, smurfs: action.payload, addingSmurf: false };
+    case actionTypes.ERROR:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        addingSmurf: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 /*
   You'll only need one smurf reducer for this project.
