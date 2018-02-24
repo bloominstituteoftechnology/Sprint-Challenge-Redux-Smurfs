d7e27ff6dff6b965112d014fb0dc17471da635c2
Peter Gray <peter@petergraycreative.com>
2018-02-23 18:03:25 -0600

    Import rootReducer
    Corrected spelling, destructured

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,10 +6,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers';
+import { rootReducer } from './reducers';
 
 const store = createStore(
-  rootReducers, // this is the most basic reducer. Replace it.
+  rootReducer, // this is the most basic reducer. Replace it.
   applyMiddleware(thunk, logger)
 );
 
