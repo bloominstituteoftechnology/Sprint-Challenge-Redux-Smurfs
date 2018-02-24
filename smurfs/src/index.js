635d2bc6c7aff00a60493f234edbb9a60af67dc3
Arkoma <aaron@adburk.net>
2018-02-24 14:19:33 -0600

    change imported reducer name from smurfs to rootReducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,10 +6,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import smurfs from './reducers';
+import rootReducer from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(smurfs, composeEnhancers(
+const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk, logger)
 ));
 
