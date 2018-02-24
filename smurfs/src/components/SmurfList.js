50d2c043cccd0c50b45e20c2df3bc5909e786425
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-23 18:37:28 -0600

    add style to list items and changed heading
    removed the bullet

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -3,13 +3,13 @@ import React from 'react';
 export const SmurfList = props => {
   return (
     <div>
-      SmurfList
+      <h1>Smurfs!</h1>
       {props.smurfs.map(smurf => {
         return (
           <ul key={smurf.id}>
-            <li>Name: {smurf.name}</li>
-            <li>Age: {smurf.age} years</li>
-            <li>Height: {smurf.height} cm</li>
+            <li style={{ listStyle: 'none' }}>Name: {smurf.name}</li>
+            <li style={{ listStyle: 'none' }}>Age: {smurf.age} years</li>
+            <li style={{ listStyle: 'none' }}>Height: {smurf.height} cm</li>
           </ul>
         );
       })}
