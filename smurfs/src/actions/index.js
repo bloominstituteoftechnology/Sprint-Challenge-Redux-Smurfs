c29a3c45cb35263a5537e4770c9de316965b7fd7
Arkoma <aaron@adburk.net>
2018-02-24 14:27:10 -0600

    set up action  with  axios get call called getSmurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,24 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+export const IS_FETCHING = 'IS_FETCHING';
+export const SMURFS_FETCHED = 'SMURFS_FETCHED';
+export const ERROR_FETCHING_SMURFS = 'ERROR_FETCHING_SMURFS';
+
+export const getSmurfs = () => {
+    const smurfs = axios.get('http://localhost:3333/smurfs'); 
+       return dispatch => {
+           dispatch({ type: IS_FETCHING });
+           smurfs
+             .then(response => {
+                 dispatch({ type: SMURFS_FETCHED, payload: response.data });
+                })
+             .catch(err => {
+                dispatch({ type: ERROR_FETCHING_SMURFS, payload: err });
+        });
+    };   
+}; 
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
