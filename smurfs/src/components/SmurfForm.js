02775df1e557fda9b8e29833d749eee39555c2e2
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-23 18:16:22 -0600

    create SmurfForm component
    - SmurfForm will update the respective state property as an input field
    is filled in
    
    - addSmurf action is connected to SmurfForm using connect
    
    - On submitting the form, it calls addSmurf from props and passes in the
    current state, then resets the state

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addSmurf } from '../actions';
+
+class SmurfForm extends React.Component {
+  state = {
+    name: '',
+    age: '',
+    height: '',
+  }
+
+  updateNewSmurf = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  submitNewSmurf = (event) => {
+    event.preventDefault();
+    this.props.addSmurf(this.state);
+    this.setState({
+      name: '',
+      age: '',
+      height: '',
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitNewSmurf}>
+          <input type="text" placeholder='Name' onChange={this.updateNewSmurf} name='name' value={this.state.name} />
+          <input type="text" placeholder='Age' onChange={this.updateNewSmurf} name='age' value={this.state.age} />
+          <input type="text" placeholder='Height' onChange={this.updateNewSmurf} name='height' value={this.state.height} />
+          <input type='submit' value='submit' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect(null, { addSmurf })(SmurfForm);
