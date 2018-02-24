a43aa41e94ac7c14aa089bf058547f4d42a32112
Amanda Phillips <amanda@phillipsdev.org>
2018-02-24 08:08:30 -0600

    Reducers added

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -19,7 +19,7 @@ const initialState = {
   smurfs: [],
   gettingSmurfs: false,
   gotSmurfs: false,
-  creatingSmurfs: false,
+  creatingSmurf: false,
   createdSmurf: false,
   error: true,
 }
@@ -32,7 +32,48 @@ const initialState = {
 */
 
 export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GETTING_SMURFS:
+      return {
+        ...state,
+        gettingSmurfs: true,
+      };
 
+    case GOT_SMURFS:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        gotSmurfs: true,
+        smurfs: action.payload,
+      };
+
+    case CREATING_SMURF:
+      return {
+        ...state,
+        creatingSmurf: true,
+      };
+
+    case CREATED_SMURF:
+      return {
+        ...state,
+        creatingSmurf: false,
+        createdSmurf: true,
+        smurfs: action.payload,
+      };
+
+    case ERROR:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        gotSmurfs: false,
+        creatingSmurfs: false,
+        createdSmurf: false,
+        error: action.payload,
+      };
+    
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
