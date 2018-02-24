a21323fa14ceb6a50177fe6bffa600c3bfca054e
Smithface <kylewibblesmith@gmail.com>
2018-02-23 19:01:49 -0500

    main assignment complete
    next to implement delete

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './smurfList.css';
 
 const SmurfList = (props) => {
   return (
-    <ul>
+    <ul className="container">
       {props.smurfs.map((smurf, i) => {
-        return <li key={smurf.id}>
-          <div>Name</div>
+        return <li key={smurf.id} className="container__item">
+          <div className="container__item-header" >Name</div>
           <div>{smurf.name}</div>
-          <div>Age</div>
-          <div>{smurf.age} smurfs old</div>
-          <div>Height</div>
+          <div className="container__item-header" >Age</div>
+          <div>{smurf.age} smurf-years old</div>
+          <div className="container__item-header" >Height</div>
           <div>{smurf.height} cm tall</div>
         </li>
       })}
