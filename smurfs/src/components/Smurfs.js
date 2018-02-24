21c9308c0c43ef9073351662a40b5f804b3da02d
Arkoma <aaron@adburk.net>
2018-02-24 15:07:26 -0600

    Smurfs.js created and connected to the redux store

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Smurfs = props => {
+    return (
+             <div className="smurfs">
+                 {props.smurfs ? props.smurfs.map((smurf) => {
+                     return (
+                         <ul key={smurf.id} className="friend">
+                         <li>name: {smurf.name}</li>
+                         <li>age: {smurf.age}</li>
+                         <li>height: {smurf.height}</li>
+                         </ul>
+                     )}
+                 ) :
+                 <div></div> }
+             </div>
+     )
+ }
+ 
+     
+ 
+ export default Smurfs;
