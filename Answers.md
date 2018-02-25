2b0577c4300e1e8c7be61781b904f5ebb43cae48
Michael Marshal <mikemarshalman@gmail.com>
2018-02-24 20:00:05 -0600

    initial commit

---
+++ Answers.md
@@ -0,0 +1,8 @@
+# 1) concat, map, filter, reduce
+# 2) Store - holds the state, Actions - functions dispatched by store to the reducer, Reducer - takes state and action as parameters and returns an altered state (immutable)
+#
+#
+#
+#
+#
+#
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 /*
   Be sure to import in all of the action types from `../actions`
 */
@@ -16,7 +17,7 @@
 
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
