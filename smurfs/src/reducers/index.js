1023b4a2180f3a9942f732d8aa5a49ed270b102f
Peter Gray <peter@petergraycreative.com>
2018-02-24 22:28:18 -0600

    Remove data from intital state and added smurfsFetched
    Made corrected to reducers to get and render out data

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,19 +1,13 @@
 import * as actionTypes from '../actions';
 
 const initialState = {
-  smurfs: [
-    {
-      name: 'Brainey',
-      age: 200,
-      height: '5cm',
-      id: 0,
-    },
-  ],
   fetchingSmurfs: false,
+  smurfsFetched: false,
   addingSmurf: false,
   updatingSmurf: false,
   deletingSmurfs: false,
   error: true,
+  smurfs: [],
 };
 
 /*
@@ -23,11 +17,28 @@ const initialState = {
   Components can read your store as, `state` and not `state.fooReducer`.
 */
 export const rootReducer = (state = initialState, action) => {
+  console.log(`The reducer ran ${action.type}`);
   switch (action.type) {
     case actionTypes.GETTING_SMURFS:
-      return { ...state, fetchingSmurfs: true };
+      return { ...state, fetchingSmurfs: true, };
     case actionTypes.GET_SMURFS:
-      return { ...state, friends: action.payload, fetchingSmurfs: false };
+      return { ...state, smurfs: action.payload, fetchingSmurfs: false, smurfsFetched: true, };
+    case actionTypes.CREATE_SMURF:
+      return {
+        ...state,
+        addingSmurf: true,
+        fetchingSmurfs: true,
+        smurfsFetched: false,
+      };
+    case actionTypes.SMURF_CREATED:
+      return {
+        ...state,
+        smurfCreated: true,
+        fetchingSmurfs: false,
+        smurfsFetched: true,
+        smurfs: action.payload,
+        addingSmurf: false,
+      };
     case actionTypes.ERROR:
       return {
         ...state,
