67f1a05540102b21751e3027d0bf85528991e0f1
german gonzalez <gcomlnk@gmail.com>
2018-02-25 17:11:17 -0500

    Adding actions to all and hooking up with connect and bringing states thought mapStateToProps

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux';
+import {getSmurfs} from '../actions';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -19,4 +22,12 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+    const {smurfs_reducer} = state;
+    return {
+        smurfs: smurfs_reducer.smurfs,
+    }
+};
+
+export default connect(mapStateToProps, {getSmurfs})(App);
+
