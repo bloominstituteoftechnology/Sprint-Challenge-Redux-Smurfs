f964ec4ca76da9ad61e8d3b5ed16b65351cbc855
german gonzalez <gcomlnk@gmail.com>
2018-02-25 17:13:49 -0500

    adding a default info for testing and develop

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ const sendUserError = (msg, res) => {
   return;
 };
 
-let smurfs = [];
+let smurfs = [
+    {
+        name: 'Brainey',
+        age: 200,
+        height: '5cm'
+    }
+];
+
 server.get('/smurfs', (req, res) => {
   res.json(smurfs);
 });
