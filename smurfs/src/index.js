9e0a90245a558600c53f25507ab747e80e784ad7
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-25 18:48:22 -0500

    Installed Dependencies, Moved App, Finished index.js
    I moved App because it just seemed weird having it in components...
    
    src/index.js --- imported soon to be written rootReducer, instituted thunk.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sore of reducer */ './reducers';
+import { rootReducer } from './reducers/reducers.js';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
