5c55d845bde43c9fca17839db3a812e7b1a1fd46
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-02-24 20:49:04 -0600

    Map State to Props
    in App.js:
    
    mapStateToProps to retrieve smurfs
    console log results
    create Smurfs.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PostSmurf from './PostSmurf';
+// import Smurfs from './Smurfs';
 import { connect } from 'react-redux';
 import { getSmurfs } from '../actions';
 import styled from 'styled-components';
@@ -17,12 +18,6 @@ const StyledDiv = styled.div`
   }
 `;
 
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I connect my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
 class App extends Component {
 
   componentDidMount = () => {
@@ -34,11 +29,19 @@ class App extends Component {
       <StyledDiv>
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <PostSmurf />
+        {/* <Smurfs /> */}
+        {this.props.smurfs.forEach(val => {
+          console.log(val);
+        })}
       </StyledDiv>
     );
   }
 }
 
+const mappStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+  }
+};
 
-
-export default connect(null, { getSmurfs })(App);
+export default connect(mappStateToProps, { getSmurfs })(App);
