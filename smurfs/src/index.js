4071a2625923dc15c89655e7acafd19b83ceca12
Peter Gray <peter@petergraycreative.com>
2018-02-24 22:28:33 -0600

    Add support for redux dev tools

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers';
 
-const store = createStore(
-  rootReducer, // this is the most basic reducer. Replace it.
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk, logger)
-);
+));
 
 ReactDOM.render(
   <Provider store={store}>
