98cf0373f786ceb4da8a804183925924c3d767c0
german gonzalez <gcomlnk@gmail.com>
2018-02-25 16:53:09 -0500

    initializing state importing actions for redurec to use

---
+++ smurfs/src/reducers/smurfs_reducer.js
@@ -0,0 +1,11 @@
+import {GET_SMURFS,ADD_SMURF, UPDATE_SMURF, DELETE_SMURF, FETCHING, ERROR_GETTING_SMURFS} from '../actions/';
+
+const initialState = {
+    smurfs: [],
+    error: null,
+};
+
+export const smurfs_reducer = (state = initialState, action) => {
+
+    return true;
+};
