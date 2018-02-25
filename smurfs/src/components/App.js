16d29b19e928c2c6810591ac99a3ab66d22c09d0
Mike Streltsoff <MStreltsoff@gmail.com>
2018-02-25 09:46:29 -0500

    Initial Commit

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import Smurfs from './Smurf';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +9,31 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
   render() {
     return (
       <div className="App">
+      <header className="App-header">
         <h1>SMURFS! 2.0 W/ Redux</h1>
+
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        </header>
+        {this.props.error ? <h3> Error Fetching Smurfs</h3> : null}
+        <div className="Flex-Container">
+          {this.props.gettingSmurfs ? (
+            <img src="https://tse4.mm.bing.net/th?id=OIP.LKq9lyY23CR_jgEQ_y4TPgHaEK&w=286&h=160&c=7&o=5&pid=1.7" className="App-logo" alt="logo" />
+          ) : (
+            <Smurfs smurfs={this.props.smurfs} />
+          )}
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -13,6 +13,53 @@
    error: true
  }
 */
+import * as actionTypes from '../actions';
+
+const initialState = {
+  smurfs: [],
+  gettingSmurfs: false,
+  updatingSmurf: false,
+  creatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
+
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.GETTING_SMURFS:
+      return { 
+      ...state, 
+      gettingSmurfs: true };
+
+    case actionTypes.GET_SMURFS:
+      return { ...state, smurfs: action.payload, gettingSmurfs: false };
+    case actionTypes.UPDATING_SMURF:
+      return { ...state, updatingSmurf: true };
+    case actionTypes.UPDATE_SMURF:
+      return { ...state, smurfs: action.payload, updatingSmurfs: false };
+    case actionTypes.DELETING_SMURF:
+      return { ...state, deletingSmurf: true };
+    case actionTypes.DELETE_SMURF:
+      return { ...state, smurfs: action.payload, deletingSmurf: false };
+    case actionTypes.CREATING_SMURF:
+      return { ...state, creatingSmurf: true };
+    case actionTypes.CREATE_SMURF:
+      return { ...state, smurfs: action.payload, creatingSmurf: false };
+    case actionTypes.ERROR:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        creatingSmurf: false,
+        deletingSmurf: false,
+        updatingSmurf: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
+export default rootReducer
+
 
 /*
   You'll only need one smurf reducer for this project.
