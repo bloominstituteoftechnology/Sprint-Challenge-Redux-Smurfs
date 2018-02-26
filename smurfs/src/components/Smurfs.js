b835fb3478bc641b5d488b3d45c16c3961023670
german gonzalez <gcomlnk@gmail.com>
2018-02-25 20:51:33 -0500

    Creating the logic to switch the fomr and teh list

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,46 +1,57 @@
 import React, {Component} from 'react';
 import CreateSmurfForm from './CreateSmurfForm'
 import {Table, Grid, Col} from 'react-bootstrap';
+import {showCreateForm} from '../actions'
+import styled from 'styled-components';
+import {connect} from 'react-redux';
 
 class Smurfs extends Component {
+    state = {
+        showButtonHideForm:true,
+    };
+
+    showCreateForm = (val) => {
+        // val is true when click
+        this.props.showCreateForm(val);
+        this.setState({showButtonHideForm: false});
+    };
 
     render() {
         return (
             <Grid>
                 <Col>
-                    {console.log(this.props.smurfs.length)}
-                    {(this.props.smurfs.length > 0)?
-                    <Table striped bordered condensed hover>
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Age</th>
-                            <th>Height</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-
-                        {this.props.smurfs.map((smurf, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{index}</td>
-                                    <td>{smurf.name}</td>
-                                    <td>{smurf.age}</td>
-                                    <td>{smurf.height}</td>
-                                    {/*<td>*/}
-
-                                        {/*<button className={"btn btn-success btn-sm"} onClick={() => this.handleUpdateVisibility(smurf, index)}> Update </button>*/}
-                                        {/*&nbsp; &nbsp;*/}
-                                        {/*<button className={"btn btn-danger btn-sm"} onClick={() => this.handleDelete(index)}> Delete </button>*/}
-
-                                    {/*</td>*/}
+                    {(this.props.smurfs.length > 0 && !this.props.showCreateFormL) ?
+                        <SmurfsContainer>
+                            <div className="create-btn">
+                                <button className={"btn btn-primary btn-sm"} onClick={() => this.showCreateForm(true)}>
+                                    Create New
+                                </button>
+                            </div>
+                            <Table striped bordered condensed hover>
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Age</th>
+                                    <th>Height</th>
                                 </tr>
-                            );
-                        })}
-                        </tbody>
-                    </Table>
-                    : <CreateSmurfForm showButtonHideForm={true}/>}
+                                </thead>
+                                <tbody>
+
+                                {this.props.smurfs.map((smurf, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{index}</td>
+                                            <td>{smurf.name}</td>
+                                            <td>{smurf.age}</td>
+                                            <td>{smurf.height}</td>
+                                        </tr>
+                                    );
+                                })}
+                                </tbody>
+                            </Table>
+                        </SmurfsContainer>
+                        : <CreateSmurfForm showButtonHideForm={this.state.showButtonHideForm}/>}
                 </Col>
             </Grid>
         );
@@ -48,5 +59,22 @@ class Smurfs extends Component {
 
 }
 
-export default Smurfs;
+const mapStateToProps = state => {
+    const {smurfs_reducer} = state;
+    return {
+        showCreateFormL: smurfs_reducer.showCreateForm,
+    }
+};
 
+export default connect(mapStateToProps, {showCreateForm})(Smurfs);
+
+
+
+const SmurfsContainer = styled.div`
+
+    .create-btn {
+        text-align:left;
+        padding:5px 0px 5px 0px;
+    }
+
+`;
