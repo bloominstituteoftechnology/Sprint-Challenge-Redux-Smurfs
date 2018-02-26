ef98f95d030fee3a9f47e50d7a097695474884f5
german gonzalez <gcomlnk@gmail.com>
2018-02-25 20:53:08 -0500

    smurf reducer addign logic to showboxes

--- smurfs/src/reducers/smurfs_reducer.js
+++ smurfs/src/reducers/smurfs_reducer.js
@@ -1,11 +1,27 @@
-import {GET_SMURFS,ADD_SMURF, UPDATE_SMURF, DELETE_SMURF, FETCHING, ERROR_GETTING_SMURFS} from '../actions/';
+import {GET_SMURFS, ADD_SMURF, SHOW_CREATE_FORM,  UPDATE_SMURF, DELETE_SMURF, FETCHING, ERROR_GETTING_SMURFS} from '../actions/';
 
 const initialState = {
     smurfs: [],
+    fetching:false,
     error: null,
+    showCreateForm:false,
 };
 
 export const smurfs_reducer = (state = initialState, action) => {
-
-    return true;
+    switch (action.type) {
+        case FETCHING:
+            return {...state, fetching: action.fetching};
+        case GET_SMURFS:
+            return {
+                ...state,
+                smurfs: action.payload,
+                fetching: false,
+            };
+        case ADD_SMURF:
+            return {...state, smurfs: action.payload};
+        case SHOW_CREATE_FORM:
+            return {...state, showCreateForm: action.showCreateForm};
+        default:
+            return state;
+    }
 };
