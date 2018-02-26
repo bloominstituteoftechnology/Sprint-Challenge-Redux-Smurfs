d58c98ea04ab3e7fcc1285973ea50615bef2824d
german gonzalez <gcomlnk@gmail.com>
2018-02-25 20:49:03 -0500

    deleting spaces anconsole logs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -4,14 +4,7 @@ import {connect} from 'react-redux';
 import {getSmurfs} from '../actions';
 import Loading from '../components/Loading';
 import Smurfs from '../components/Smurfs';
-// import {Grid, Col} from 'react-bootstrap'
 
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I connect my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
 class App extends Component {
 
     componentDidMount() {
@@ -35,7 +28,6 @@ class App extends Component {
 
 const mapStateToProps = state => {
     const {smurfs_reducer} = state;
-    console.log('State from reducers:', smurfs_reducer);
     return {
         smurfs: smurfs_reducer.smurfs,
         fetching: smurfs_reducer.fetching,
