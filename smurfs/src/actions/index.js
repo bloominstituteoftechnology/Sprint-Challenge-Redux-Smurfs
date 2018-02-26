6398960ba8245e9a06cac45a2d8a60bdf4f5c4cd
algorithmsrock <algorithmsrock@gmail.com>
2018-02-25 23:02:11 -0800

    Sprint Progress

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,14 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+export const FETCH_SMURFS = 'FETCHING_SMURFS';
+export const UPLOADED_SMURFS = 'UPLOADED_SMURFS';
+export const ADD_SMURFS = 'ADD_SMURFS';
+export const NEWLY_ADDED_SMURFS = 'NEWLY_ADDED_SMURFS';
+export const GET_SMURFS = 'GET_SMURFS';
+export const DELETE_SMURFS = 'DELETE_SMURFS';
+export const ERROR = 'ERROR';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +21,41 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+	return dispatch => {
+		dispatch({ type: FETCH_SMURFS })
+		axios.get('http://localhost:3333/smurfs')	
+		.then(({ data }) => {
+			dispatch({ type: UPLOADED_SMURFS, payload: data })
+			})
+		.catch(error => {
+				dispatch({ type: ERROR, payload: error })
+			})
+	 }
+}
+
+export const addSmurf = (smurf) => {
+	return dispatch => {
+		dispatch({ type: ADD_SMURFS })
+		axios.post('http://localhost:3333/smurfs', smurf)
+		.then(({ data }) => {
+			dispatch({ type: NEWLY_ADDED_SMURFS })
+   })
+	  .then(( { data }) => {
+				dispatch({ type: FETCH_SMURFS })
+				axios.get('http://localhost:3333/smurfs')
+				.then(({ data }) => {
+						dispatch({ type: UPLOADED_SMURFS, payload: data })
+					})
+				.catch(error => {
+						dispatch({ type: ERROR, payload: error })
+					})
+       })
+		   .catch(error => {
+             dispatch({ type: ERROR, payload: error })
+           })
+         })
+	    }
+  }
+   	   
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { getSmurfs} from '../actions';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,7 +9,11 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
-  render() {
+  componentDidMount() {
+		this.props.getSmurfs();
+	}
+
+	render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
