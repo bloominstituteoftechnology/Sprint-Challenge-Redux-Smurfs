074c6e392f705ab00a40955e5b314af5425231de
Dan-Lara <endtrippy@gmail.com>
2018-02-25 23:42:43 -0700

    Added an update button

--- smurfs/src/components/SmurfGen.js
+++ smurfs/src/components/SmurfGen.js
@@ -7,13 +7,13 @@ const SmurfGen = props => {
         <button className="x" onClick={() => props.delete(smurf.id)}>
           X
         </button>
-        <li className="stats">{`Name: ${smurf.name}`}</li>
+        <li icon="clickable" className="stats">{`Name: ${smurf.name}`}</li>
         <li className="stats">{`Age: ${smurf.age}`}</li>
         <li className="stats">{`Height: ${smurf.height}`}</li>
+        <button>Update</button>
       </div>
     );
   });
 };
 
 export default SmurfGen;
-  
