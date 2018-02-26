a2e6b4772db4968e6287c13d6af67ba7f545ef1c
german gonzalez <gcomlnk@gmail.com>
2018-02-25 19:04:35 -0500

    adding Loading component and Initial List fo smurfs empty

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {connect} from 'react-redux';
 import {getSmurfs} from '../actions';
-import {Grid, Col} from 'react-bootstrap'
+import Loading from '../components/Loading';
+import Smurfs from '../components/Smurfs';
+// import {Grid, Col} from 'react-bootstrap'
 
 /*
  to wire this component up you're going to need a few things.
@@ -11,22 +13,32 @@ import {Grid, Col} from 'react-bootstrap'
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        this.props.getSmurfs();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-Title">Smurfs App</h1>
+                </header>
+
+                {this.props.fetching ? <Loading/>: <Smurfs smurfs={this.props.smurfs}/>}
+
+            </div>
+        );
+    }
+
 }
 
 const mapStateToProps = state => {
     const {smurfs_reducer} = state;
+    console.log('State from reducers:', smurfs_reducer);
     return {
         smurfs: smurfs_reducer.smurfs,
+        fetching: smurfs_reducer.fetching,
     }
 };
 
