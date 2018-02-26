ebbc68e85b810bc986765ec0fd151c79fb39fe27
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-25 22:42:57 -0500

    Created SmurfForm Component
    SmurfForm renders a form that accepts input for Name, Age and Height.
    
    It is connected to the addSmurfs action which is run when the user
    submits the form.

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { addSmurfs } from '../actions';
+
+class SmurfForm extends Component {
+  state = {
+    name: '',
+    age: '',
+    height: '',
+  };
+  handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value});
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, age, height } = this.state;
+    this.props.addSmurfs({ name, age, height });
+    this.setState({ name: '', age: '', height: ''});
+  };
+
+  render(){
+    console.log('SmurfForm.js props', this.props);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          type="text" 
+          value={this.state.name} 
+          name="name" 
+          placeholder="name" 
+          onChange={this.handleInputChange}
+        />
+        <input 
+          type="text" 
+          value={this.state.age} 
+          name="age" 
+          placeholder="age" 
+          onChange={this.handleInputChange}
+        />
+        <input 
+          type="text" 
+          value={this.state.height} 
+          name="height" 
+          placeholder="height" 
+          onChange={this.handleInputChange}
+        />
+        <button>Add New Smurf</button>
+      </form>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    error: state.error,
+    savingSmurfs: state.savingSmurfs,
+  }
+}
+
+export default connect (mapStateToProps, { addSmurfs })(SmurfForm);
