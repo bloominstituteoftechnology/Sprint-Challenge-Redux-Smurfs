9859373550603be26718e844302821016d02ebdd
Megan Williamson <meganawilliamson@gmail.com>
2018-03-23 09:53:42 -0700

    SmurfsList component setup, ready to render smurfs

--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,12 +1,37 @@
+// React
 import React, { Component } from 'react';
+// Redux
+import { connect } from "react-redux";
+// actions
+import { fetch_smurfs } from '../actions';
 
 class SmurfsList extends Component {
     render() {
         return (
-            <h1>Hi</h1>
+            <React.Fragment>
+                <h1>Still rendering..</h1>
+                {this.props.smurfs.map(smurf => {
+                    return (
+                        <div>
+                            <div>{smurf.name}</div>
+                            <div>{smurf.age}</div>
+                            <div>{smurf.height}</div>
+                        </div>
+                    )
+                })}
+            </React.Fragment>
         );
     }
 
+    componentDidMount() {
+        this.props.fetch_smurfs();
+    }
 }; // end of SmurfsList component
 
-export default SmurfsList;
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs
+    };
+};
+
+export default connect(mapStateToProps, { fetch_smurfs })(SmurfsList);
