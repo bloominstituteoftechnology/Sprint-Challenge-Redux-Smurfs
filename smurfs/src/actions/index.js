67f139011bf6b86bdf3b40b927db1a587be5b23b
sagdish <forsi1@yahoo.com>
2018-03-23 11:35:47 -0700

    questions answered, work in progress

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,29 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+//export const LOADING = 'LOADING';
+export const GET_SMURF = 'GET_SMURF';
+export const ADD_SMURF = 'ADD_SMURF';
+export const DEL_SMURF = 'DEL_SMURF';
+
+export const showSmurfAction = () => dispatch => {
+  axios.
+  get('http://localhost:3333/smurfs')
+  .then(response = {
+    dispatch({ type: GET_SMURF, smurts: response.data })
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+export const addSmurfAction = () => dispatch => {
+  
+}
+
+
+
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { showSmurfAction } from '../actions';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -7,13 +9,74 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newSmurf: {
+        name: '',
+        age: '',
+        height: '',
+      }
+    }
+  }
+  
+  addSmurf = (event) => {
+    event.preventDefault();
+    this.props.addSmurfAction(newSmurf);
+    
+    this.setState({
+      name: '',
+      age: '',
+      height: ''
+    });
+  }
+
+  updateName = (event) => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  updateAge = (event) => {
+    this.setState({
+      age: event.target.value
+    });
+  }
+
+  updateHeight = (event) => {
+    this.setState({
+      height: event.target.value
+    });
+  }
+  
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+      <div className="container">
+        <form onSubmit={this.addSmurf}>
+          <input
+            onChange={this.updateName}
+            placeholder="name"
+            value={this.state.name}
+          />
+          <input
+            onChange={this.updateAge}
+            placeholder="age"
+            value={this.state.age}
+          />
+          <input
+            onChange={this.updateHeight}
+            placeholder="height"
+            value={this.state.height}
+          />
+          <button type="submit">ADD NEW SMURF</button>
+        </form>
+
+        <div>
+          {this.props.smurfs.map(smurf => {
+            return <Smurflist smurf={smurf} key={smurf.id} />
+          })}
+        </div>
+
       </div>
     );
   }
