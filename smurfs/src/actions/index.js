023bff90f642fedb0d41f90b437aa11a4308ee77
joshuacoyne <josh.j.coyne@gmail.com>
2018-03-23 13:51:00 -0400

    Completed Base Goals and Delete

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,54 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const FETCHING = 'FETCHING';
+export const FETCHED  = 'FETCHED';
+export const ADDING = 'ADDING';
+export const ADDED = 'ADDED';
+export const UPDATING = 'UPDATING';
+export const UPDATED = 'UPDATED';
+export const DELETING = 'DELETING';
+export const DELETED = 'DELETED';
+export const ERROR = 'ERROR';
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({type: FETCHING})
+
+  axios
+    .get('http://localhost:3333/smurfs')
+      .then(response => {
+        dispatch({type: FETCHED, smurfs: response.data});
+      })
+      .catch(error => {
+        dispatch({type: ERROR, error});
+      })
+};
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({type: ADDING})
+
+  axios
+    .post('http://localhost:3333/smurfs', smurf)
+      .then(response => {
+        dispatch({type: FETCHED, smurfs: response.data});
+      })
+      .catch(error => {
+        dispatch({type: ERROR, error});
+      })
+}
+
+export const deleteSmurf = smurf => dispatch => {
+  dispatch({type:DELETING});
+
+  axios
+    .delete(`http://localhost:3333/smurfs/${smurf.id}`)
+      .then(response => {
+        dispatch({type: DELETED});
+      })
+      .catch(error => {
+        dispatch({type: ERROR, error});
+      })
+
+
+}
+
+
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux';
+import { addSmurf, fetchSmurfs, deleteSmurf } from '../actions';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -8,15 +11,66 @@ import './App.css';
  */
 class App extends Component {
   render() {
+    let nameInput, ageInput, heightInput, delInput;
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        
+          {this.props.smurfs.map(smurf => {
+            return(
+              <ul>
+                <li>Name:{smurf.name}</li>
+                <br/>
+                <li>Age:{smurf.age}</li>
+                <br/>
+                <li>Height:{smurf.height}</li>
+                <br/><br/>
+              </ul>
+            )
+          })}
+        
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          this.props.addSmurf({name: nameInput.value, age: ageInput.value, height: heightInput.value})
+          nameInput.value = '';
+          ageInput.value = '';
+          heightInput.value = '';
+          document.getElementById('nameField').focus();
+          }}>
+          <input ref={name => nameInput = name} placeholder='Name' id='nameField' autoFocus />
+          <input ref={age => ageInput = age} placeholder='Age' />
+          <input ref={height => heightInput = height} placeholder='height' />
+          <button>Add Smurf</button>
+        </form>
+        <br/>
+        <form onSubmit={(event) => {
+          //event.preventDefault();
+          let smurf = this.props.smurfs.filter((smurf) => smurf.name === delInput.value )
+          this.props.deleteSmurf(...smurf)
+          delInput.value = '';
+        }}>
+        <input ref={del => delInput = del} placeholer="Enter the name of the smurf you'd like to delete."/>
+        <button>DELETE SMURF</button>
+        </form>
+        
       </div>
     );
   }
+
+  componentDidMount() {
+    this.props.fetchSmurfs();
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+  smurfs: state.smurfs,
+  fetchingSmurfs: state.fetchingSmurfs,
+  addingSmurf: state.addingSmurf,
+  updatingSmurf: state.updatingSmurf,
+  deletingSmurfs: state.deletingSmurf,
+  error: state.eorror
+}
+};
+
+export default connect(mapStateToProps, {fetchSmurfs, addSmurf, deleteSmurf })(App);
