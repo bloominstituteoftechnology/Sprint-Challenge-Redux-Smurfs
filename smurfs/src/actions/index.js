18634009f7b193eed3d6cae09b5a9d3992083164
RiaClay <sherria.rydyn@gmail.com>
2018-03-23 12:05:32 -0500

    created the store and added thunk and logger. created 2 actions: getSmurfs and addSmurf using axios.get and axios.post

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,4 +1,33 @@
-/* 
+import axios from 'axios';
+
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_ERROR = 'FETCH_SMURFS_ERROR';
+export const ADD_SMURF = 'ADD_SMURF';
+
+export const getSmurfs = () => dispatch => {
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(({data}) => {
+      dispatch({ type: FETCH_SMURFS_SUCCESS, smurfs: data })
+    })
+    .catch(error => {
+      dipatch({ type: FETCH_SMURFS_ERROR })
+    })
+}
+
+export const addSmurfs = () => dispatch => {
+  axios
+    .post('http://localhost:3333/smurfs', {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    })
+    .then(({data}) => {
+      dispatch({ type: ADD_SMURF, smurfs: data })
+    })
+}
+
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
@@ -8,8 +37,8 @@
    and 2 more for the stretch problem.
    Be sure to include action types for each type of action creator. Also, be sure to mind
      the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
+   C - addSmurf (create)
+   R - getSmurfs (fetch)
+   U - updateSmurf (update)
+   D - deleteSmurf (delete)
 */
