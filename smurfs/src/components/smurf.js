e1866b194ee18d62c0c76ef2ea0a51a930fefcfe
Edward Manda <lilkups@yahoo.com>
2018-03-23 16:12:06 -0500

    Started styling it

--- smurfs/src/components/smurf.js
+++ smurfs/src/components/smurf.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './smurf.css';
 
 class Smurf extends React.Component {
     render() {
--- smurfs/src/components/smurfs.js
+++ smurfs/src/components/smurfs.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 //import styled from 'styled-components';
 
 import Smurf from './smurf';
 import SmurfForm from './smurfform';
+
+import './smurfs.css';
+
 const Smurfs = (props) => {
     console.log(props)
     return (
-        <div className="Smurfs">
-            <h1>Smurf Village</h1>
-            <ul>
-                { props.smurfs.map((smurf) => {
-                    return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id} />;
-                })}
-            </ul>
+        <Fragment>
+            <div className="Smurfs">
+    {props.smurfs.length === 0 ? (<h3>Please add a smurf!!</h3>) : ("") }
+                <Fragment>
+                    { props.smurfs.map((smurf) => {
+                        return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id} />;
+                    })}
+                </Fragment>
+            </div>
             <SmurfForm />
-        </div>
+        </Fragment>
     );
 };
 
