fc5ce5309e0b1dcc8b46da4f4e2438036387f745
Megan Williamson <meganawilliamson@gmail.com>
2018-03-23 09:29:48 -0700

    made App component presentational, created SmurfsList component

---
+++ smurfs/src/components/SmurfsList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SmurfsList extends Component {
+    render() {
+        return (
+            <h1>Hi</h1>
+        );
+    }
+
+}; // end of SmurfsList component
+
+export default SmurfsList;
