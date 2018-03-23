27ae68230342c5adf9800821f179d0dd344cca6a
runranron <williamrpelton@gmail.com>
2018-03-23 13:11:15 -0500

    couldn't figure out why my post isn't working

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,25 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+export const ADD_SMURF = 'ADD_SMURF';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const getSmurfs = () => {
+  return dispatch => {
+    dispatch({type: FETCHING});
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(result => dispatch({type: FETCHED, smurfs: result.data}))
+      .catch(err => dispatch({type: ERROR, errorMessage: "Dropped your smurfs!"}));
+  }
+}
+
+export const addSmurf = smurf => dispatch => {
+    console.log(smurf);
+    axios
+      .post('http://localhost:3333/smurfs', smurf).then(response => {
+        dispatch({
+          type: ADD_SMURF,
+        })
+      }).catch(error => dispatch({type: ERROR, errorMessage: error.response.data}))
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,19 +1,45 @@
+import { 
+  FETCHED, 
+  ERROR,
+  FETCHING,
+  ADD_SMURF
+   } from "../actions";
+
 /*
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could look a lot like this
- {
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurfs: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurfs: false,
    error: null
  }
-*/
 
+export default (state=initialState, action) => {
+  switch(action.type) {
+    case FETCHING:
+      return Object.assign({}, state, {
+        fetchingSmurfs: true,
+      });
+    case FETCHED:
+      return Object.assign({}, initialState, {
+        smurfs: action.smurfs,
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+          error: action.errorMessage,
+      });
+    case ADD_SMURF:
+      return Object.assign({}, state, {
+        addingSmurf: true,
+      })
+      default:
+        return state;
+  }
+}
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
