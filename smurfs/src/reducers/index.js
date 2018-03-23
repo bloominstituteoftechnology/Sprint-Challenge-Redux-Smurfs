2374345db4a79e5b4293ce3ee5de2308b55b7ef8
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-03-23 11:50:13 -0400

    Stone Chris | Sprint-Challenge--Redux
    Stone Chris | Sprint-Challenge--Redux

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,17 +2,15 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could look a lot like this
- {
+const initialState ={
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurfs: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurfs: false,
    error: null
- }
-*/
+ };
+
 
 /*
   You'll only need one smurf reducer for this project.
@@ -20,3 +18,30 @@
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case fetchingSmurfs:
+      console.log('Fetching...');
+      return {...state, fetchingSmurfs:true};
+    case addingSmurf:
+      return{
+        ...state,
+        fetchingSmurfs: false,
+        addingSmurf: true,
+        smurfs: action.payload,
+      };
+    case updatingSmurf:
+      console.log('Updating...');
+      return{...state, updatingSmurf: true, visible:action.visible};
+  
+    case deletingSmurfs:
+      console.log('Deleting...');
+      return{...state, deletingSmurfs: true, smurfs: action.payload,};
+
+    case errorFetching:
+      return{...state, fetchingSmurfs: false, error: action.payload};
+  
+    default: return state;
+    };
+}
