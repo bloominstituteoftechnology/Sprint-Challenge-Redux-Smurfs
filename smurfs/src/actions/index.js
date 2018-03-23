ce0204f069ac08eeb1a65ba1d49444cf073417ca
Juber Ahmed <a.juber@hotmail.com>
2018-03-23 13:52:13 -0400

    Actions and reducers completed

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,9 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
-
+import axios from 'axios';
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCHED_SMURFS = 'FETCHED_SMURFS';
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADDED_SMURF = 'ADDED_SMURF';
+export const ERROR = 'ERROR';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +14,31 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+  const smurfs = axios.get('http://localhost:3333/smurfs');
+  return dispatch => {
+    dispatch({ type: FETCHING_SMURFS });
+    smurfs
+      .then(payload => {
+        dispatch({ type: FETCHED_SMURFS, payload: payload.data });
+      })
+      .catch(error => {
+        dispatch({ type: ERROR, payload: error });
+      });
+  };
+};
+
+export const addSmurf = newSmurf => {
+  const smurfs = axios.post('http://localhost:3333/smurfs/', newSmurf);
+  return dispatch => {
+    dispatch({ type: ADDING_SMURF });
+    smurfs
+      .then(payload => {
+        dispatch({ type: ADDED_SMURF, payload: payload.data });
+      })
+      .catch(error => {
+        dispatch({ type: ERROR, payload: error });
+      });
+  };
+};
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,8 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import { FETCHING_SMURFS, FETCHED_SMURFS, ADDING_SMURF, ADDED_SMURF, ERROR } from '../actions';
+
 /*
  Your initial/default state for this project could look a lot like this
  {
@@ -14,9 +16,55 @@
  }
 */
 
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  error: null
+};
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+    case FETCHED_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        fetchedSmurfs: true,
+        smurfs: action.payload
+      };
+    case ADDING_SMURF:
+      return {
+        ...state,
+        addingSmurf: true
+      };
+    case ADDED_SMURF:
+      return {
+        ...state,
+        addingSmurf: false,
+        addedSmurf: true,
+        smurfs: action.payload
+      };
+    case ERROR:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        fetchedSmurfs: false,
+        addingSmurf: false,
+        addedSmurf: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
