d12b58c6b4cc5c3b8f6308e84496f9ced0e336f8
astro11 <aquila.strother@asu.edu>
2018-03-23 13:02:06 -0400

    smurfForm post

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,12 @@
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+
+export const ADDING = 'ADDING';
+export const ADDED = 'ADDED';
+export const ADDERROR = 'ADDERROR';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
@@ -13,3 +22,28 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getFriends = () => (dispatch) => {
+
+    dispatch({ type: FETCHING });
+
+    axios.get('http://localhost:3333/smurfs')
+    .then(response => {
+        dispatch({ type: FETCHED, smurfs: response.data });
+    })
+    .catch(err => {
+        dispatch({ type: ERROR, errorMessage: 'Error fetching the smurfs' });
+    })
+}
+
+export const addFriends = (smurfs) => (dispatch) => {
+    dispatch({ type: ADDING });    
+    axios.post('http://localhost:3333/smurfs', smurfs)
+       
+    .then(response => {
+        dispatch({ type: ADDED, smurfs: response.data });
+    })
+    .catch(err => {
+        dispatch({ type: ADDERROR, errorMessage: 'Error adding the smurfs' });
+    })
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -20,3 +20,35 @@
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+import { FETCHED, FETCHING, ERROR } from '../actions';
+import { ADDED, ADDING, ADDERROR } from '../actions';
+
+const initialState = {
+  smurfs: [],
+  fetched: false,
+  fetching: false,
+  error: null,
+  adding: false,
+  added: false,
+  adderror: null,
+}
+
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case FETCHING:
+      return { ...state, fetching: true }
+      case ERROR:
+      return { ...state, error: action.errorMessage }
+    case FETCHED:
+      return { ...state, smurfs: action.smurfs, fetching: false, error: null } 
+    case ADDING:
+      return { ...state, adding: true }
+    case ADDERROR:
+      return { ...state, adderror: action.errorMessage }
+    case ADDED:
+      return { ...state, added: true }
+    default:
+    return state;
+  }
+}
