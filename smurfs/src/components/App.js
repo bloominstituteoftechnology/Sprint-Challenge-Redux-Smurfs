c6b6ce592d3d5fa8fc5d49e4eae2cae03c7f7daa
unknown <jacobiswinning@gmail.com>
2018-03-23 10:52:37 -0600

    working through boiler plate and adding smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getSmurfs } from '../actions';
+import { connect } from 'react-redux';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
