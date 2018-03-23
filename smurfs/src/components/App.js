2baacd39d910713151b9b8b5cf18b80c80cc1237
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-03-23 13:26:38 -0500

    reduxII sprint wip: Erik Alonso

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,92 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { getSm, addSm } from "../actions";
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
- Just remember, `how do I connect my components to redux?`
- `How do I ensure that my component links the state to props?`
+ Just remember, `how do I connect my components to redux?` (connect())
+ `How do I ensure that my component links the state to props?` (mapStateToProps)
  */
 class App extends Component {
   render() {
+    let nameInput, ageInput, heightInput, delInput;
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+
+        {this.props.smurfs.map(smurf => {
+          return (
+            <ul>
+              <li>Name:{smurf.name}</li>
+              <br />
+              <li>Age:{smurf.age}</li>
+              <br />
+              <li>Height:{smurf.height}</li>
+              <br />
+              <br />
+            </ul>
+          );
+        })}
+
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            this.props.getSm({
+              name: nameInput.value,
+              age: ageInput.value,
+              height: heightInput.value
+            });
+            nameInput.value = "";
+            ageInput.value = "";
+            heightInput.value = "";
+            document.getElementById("nameField").focus();
+          }}
+        >
+          <input
+            ref={name => (nameInput = name)}
+            placeholder="Name"
+            id="nameField"
+            autoFocus
+          />
+          <input ref={age => (ageInput = age)} placeholder="Age" />
+          <input ref={height => (heightInput = height)} placeholder="height" />
+          <button>Add Smurf</button>
+        </form>
+        <br />
+        <form
+          onSubmit={event => {
+            //event.preventDefault();
+            let smurf = this.props.smurfs.filter(
+              smurf => smurf.name === delInput.value
+            );
+            this.props.deleteSmurf(...smurf);
+            delInput.value = "";
+          }}
+        >
+          <input
+            ref={del => (delInput = del)}
+            placeholer="Enter the name of the smurf you'd like to delete."
+          />
+          <button>DELETE SMURF</button>
+        </form>
       </div>
     );
   }
+
+  componentDidMount() {
+    this.props.addSm();
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    sm: state.smurfs,
+    getSm: state.getSm,
+    addSm: state.addSm,
+    error: state.eorror
+  };
+};
+
+export default connect(mapStateToProps, { addSm, getSm })(App);
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import /* You need some sore of reducer */ './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
