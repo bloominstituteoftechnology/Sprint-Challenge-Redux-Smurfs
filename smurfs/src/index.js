d26e9d71416678a3abd6b32d3f47280df637d8fc
DragonRydrz <DragonRydrz@me.com>
2018-03-23 11:57:01 -0400

    answers.md completed

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./components/App";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
