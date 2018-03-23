3417889c14cb40a05da0c81f72e1a71a2bcab12f
Megan Williamson <meganawilliamson@gmail.com>
2018-03-23 09:13:20 -0700

    action index.js setup

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,34 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+// axios
+import axios from 'axios';
+// action types
+  export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+  export const FETCHING_SMURFS_ERROR = 'FETCHING_SMURFS_ERROR';
+  export const FETCHED_SMURFS = 'FETCHED_SMURFS';
+  export const POSTING_SMURF = 'POSTING_SMURF';
+  export const POSTING_SMURF_ERROR = 'POSTING_SMURF_ERROR';
+  export const POSTED_SMURF = 'POSTED_SMURF';
+  export const DELETING_SMURF = 'DELETING_SMURF';
+  export const DELETING_SMURF_ERROR = 'DELETING_SMURF_ERROR';
+  export const DELETED_SMURF = 'DELETED_SMURF';
+  export const PUTTING_SMURF = 'PUTTING_SMURF';
+  export const PUTTING_SMURF_ERROR = 'PUTTING_SMURF_ERROR';
+  export const PUT_SMURF = 'PUT_SMURF'; // !! there is a better way to do this
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const fetch_smurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS });
+
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      dispatch({ type: FETCHED_SMURFS, smurfs: response.data });
+    })
+    .catch(err => {
+      dispatch({ type: FETCHING_SMURFS_ERROR, errorMessage: 'Error fetching smurfs' });
+    });
+};
+
+// export const post_smurf
+
+// export const delete_smurf
+
+// export const put_smurf
