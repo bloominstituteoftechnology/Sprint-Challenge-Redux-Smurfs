5deea997c0743bdc436c35a2ded0de9709df3f37
liz <elizabethbraggisgreat@gmail.com>
2018-03-26 00:46:52 -0400

    got the form working

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,12 +6,14 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sore of reducer */ './reducers';
+import rootReducer from './reducers';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. Replace it.
-  applyMiddleware(thunk, logger)
-);
+
+// const store = createStore(
+//   () => {}, // this is the most basic reducer. Replace it.
+//   applyMiddleware(thunk, logger)
+// );
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 ReactDOM.render(
   <Provider store={store}>
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,15 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { FETCHING_SMURFS, FETCHED_SMURFS, ERROR, ADD_SMURF } from "../actions";
 
-/*
- Your initial/default state for this project could look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurfs: false
-   error: null
- }
-*/
+
+
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  error: null,
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -20,3 +17,27 @@
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_SMURFS:
+      return Object.assign({}, state, {
+        fetchingSmurfs: true
+      });
+    case FETCHED_SMURFS:
+      return Object.assign({}, initialState, {
+        smurfs: action.smurfs
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        error: action.errorMessage
+      });
+    case ADD_SMURF:
+      return Object.assign({}, state, {
+        addingSmurf: true,
+        smurfs: []
+      });
+
+    default:
+      return state;
+  }
+};
