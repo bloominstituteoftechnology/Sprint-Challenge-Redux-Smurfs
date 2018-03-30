a98265532dd034c5a5bb644f9b1e92e77de7c430
JaredRCooper <jaredrcoopet@gmail.com>
2018-03-30 14:31:08 -0400

    everything is working, just need to change z indexing

--- smurfs/src/components/smurfform.js
+++ smurfs/src/components/smurfform.js
@@ -1,68 +1,51 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { addSmurf } from '../actions';
+import {connect} from 'react-redux';
+import {addSmurf} from '../actions';
 
 class SmurfForm extends React.Component {
     constructor() {
         super();
         this.state = {
-          name: '',
-          age: '',
-          height: ''
+            name: '',
+            age: '',
+            height: ''
         };
     }
 
     render() {
         return (
-          <div className="SmurfForm">
-            <form onSubmit={this.addSmurf}>
-              <input
-                onChange={this.updateName}
-                placeholder="name"
-                value={this.state.name}
-              />
-              <input
-                onChange={this.updateAge}
-                placeholder="age"
-                value={this.state.age}
-              />
-              <input
-                onChange={this.updateHeight}
-                placeholder="height"
-                value={this.state.height}
-              />
-              <button type="submit">Add to the village</button>
-            </form>
-          </div>
+            <div className="SmurfForm" >
+                <form onSubmit={this.addSmurf}>
+                    <input onChange={this.updateName} placeholder="name" value={this.state.name}/>
+                    <input onChange={this.updateAge} placeholder="age" value={this.state.age}/>
+                    <input
+                        onChange={this.updateHeight}
+                        placeholder="height"
+                        value={this.state.height}/>
+                    <button type="submit">Add to the village</button>
+                </form>
+            </div>
         );
     }
 
     addSmurf = event => {
-    event.preventDefault();
-    this.props.addSmurf(this.state);
-    this.setState({
-        name: '',
-        age: '',
-        height: ''
-    });
+        event.preventDefault();
+        this
+            .props
+            .addSmurf(this.state);
+        this.setState({name: '', age: '', height: ''});
     }
 
     updateName = (event) => {
-    this.setState({
-        name: event.target.value
-    });
+        this.setState({name: event.target.value});
     }
 
     updateAge = (event) => {
-    this.setState({
-        age: event.target.value
-    });
+        this.setState({age: event.target.value});
     }
 
     updateHeight = (event) => {
-    this.setState({
-        height: event.target.value
-    });
+        this.setState({height: event.target.value});
     }
 };
 
--- smurfs/src/components/smurfs.js
+++ smurfs/src/components/smurfs.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react';
-import { connect } from 'react-redux';
+import React, {Fragment} from 'react';
+import {connect} from 'react-redux';
 import Smurf from './smurf';
 import SmurfForm from './smurfform';
 
@@ -8,18 +8,23 @@ const Smurfs = (props) => {
     return (
         <Fragment>
             <div className="Smurfs">
-    {props.smurfs.length = 0 ? (<h2>add a smurf</h2>) : ("") }
+                {props.smurfs.length = 0
+                    ? (
+                        <h2>add a smurf</h2>
+                    )
+                    : ("")}
                 <Fragment>
-                    { props.smurfs.map((smurf) => {
-                        return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id} />;
-                    })}
+                    {props
+                        .smurfs
+                        .map((smurf) => {
+                            return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id}/>;
+                        })}
                 </Fragment>
             </div>
             <SmurfForm />
         </Fragment>
     );
 };
-
 
 const mapStateToProps = state => {
     return state;
