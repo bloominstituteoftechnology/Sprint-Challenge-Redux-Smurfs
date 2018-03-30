cc28d27fb490867c6e04c9d568ca03cab5164027
Supaturbolocity <mrhutton1@yahoo.com>
2018-03-30 11:20:40 -0700

    completed index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import /* You need some sore of reducer */ './reducers';
+import axios from 'axios';
+export const GET_SMURFS = 'GET_SMURFS';
+export const POST_SMURFS = 'POST_SMURFS';
+export const PUT_SMURFS = 'PUT_SMURFS';
+export const DELETE_SMURFS = 'DELETE_SMURFS';
+export const ERROR = 'ERROR';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+export const getSmurf = () => dispatch => {
+  axios.get(`http://localhost:3333/smurfs`).then(
+    response => { dispatch({ type: GET_SMURFS, smurfs: response.data })}
+  ).catch(
+    error => { dispatch( {type: ERROR, errorMessage: error.response.data.Error})}
+  )
+}
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export const postSmurf = (smurf) => dispatch => {
+  axios.post(`http://localhost:3333/smurfs`, smurf).then(
+    response => { dispatch({ type: POST_SMURFS, smurfs: response.data })}
+  ).catch(
+    error => { dispatch( {type: ERROR, errorMessage: error.response.data.Error})}
+  )
+}
+
+export const putSmurf = (id, smurf) => dispatch => {
+  axios.put(`http://localhost:3333/smurfs/${id}`, smurf).then(
+    response => { dispatch({ type: PUT_SMURFS, smurfs: response.data })}
+  ).catch(
+    error => { dispatch( {type: ERROR, errorMessage: error.response.data.Error})}
+  )
+}
+
+export const deleteSmurf = (id) => dispatch => {
+  axios.delete(`http://localhost:3333/smurfs/${id}`).then(
+    response => { dispatch({ type: DELETE_SMURFS, id: response.data.SmurfRemoved.id })}
+  ).catch(
+    error => { dispatch( {type: ERROR, errorMessage: error.response.data.Error})}
+  )
+}
