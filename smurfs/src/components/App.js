04ac9f3f3924495d4bf70bf5a20e087b489d19c9
axhon <axhonruiz@gmail.com>
2018-04-20 08:39:08 -1000

    made smurfs component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types'
 import { fetchSmurfs } from '../actions'
 import './App.css'
 
+import Smurfs from './Smurfs'
+
 class App extends Component {
   componentDidMount () {
     this.props.fetchSmurfs()
@@ -13,16 +15,15 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <Smurfs smurfs={this.props.smurfs} />
       </div>
     )
   }
 }
 
 App.propTypes = {
-  fetchSmurfs: PropTypes.func
+  fetchSmurfs: PropTypes.func,
+  smurfs: PropTypes.array
 }
 
 const mapStateToProps = state => ({
