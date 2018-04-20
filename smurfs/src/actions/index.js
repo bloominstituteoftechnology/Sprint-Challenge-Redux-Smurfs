61cdace02254f7c2b748fe410090a63fae2809a3
Justin Rigler <justinmrigler@gmail.com>
2018-04-20 12:13:39 -0400

    imported reducer to index.js and set up middleware, started building actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,22 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const GET_SMURFS = 'GET_SMURFS';
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const ERROR = 'ERROR';
+export const ADD_SMURF = 'ADD_SMURF';
+
+const URL = 'http://localhost:3333/smurfs';
+
+export const getSmurfs = () => {
+  axios
+    .get(URL);
+    return (dispatch) => {
+    dispatch({ type: GETTING_SMURFS})
+  }
+}
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
