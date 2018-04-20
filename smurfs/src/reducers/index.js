f716261a6341ac4e7ad361672d6ec7f0f7c2bd80
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-20 09:01:37 -0700

    filled out reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import { PENDING_SMURFS, SUCCESS_SMURFS, ERROR_SMURFS} from '../actions';
 /*
  Your initial/default state for this project could look a lot like this
  {
@@ -13,10 +13,28 @@
    error: null
  }
 */
-
+const initialState = {
+  pending: false,
+  error: null,
+  smurfs: []
+};
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+const smurfsReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case PENDING_SMURFS:
+      return Object.assign({}, state, { pending: true });
+    case SUCCESS_SMURFS:
+      return Object.assign({}, state, { pending: false, smurfs: [...action.payload] });
+    case ERROR_SMURFS:
+      return Object.assign({}, state, { pending: false, error: action.payload });
+    default:
+      return state;
+  }
+};
+
+export default smurfsReducer;
