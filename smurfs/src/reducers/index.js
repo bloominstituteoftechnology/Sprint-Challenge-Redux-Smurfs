7081eca3f0b1a1b847c94559aac2cc4e7ebd9dd2
Devon Bacon <devon.bacon@gmail.com>
2018-04-20 09:55:58 -0700

    Set up switch statement in reducers index.js

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -12,21 +12,10 @@ const initialState = {
   deletingSmurfs: false,
   error: null
 }
-/*
- Your initial/default state for this project could look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurfs: false
-   error: null
- }
-*/
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  Components can read your store as, `state` and not `state.fooReducer`.
-*/
+// Create new state depending on action type
+export const smurfsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    
+  }
+}
