9210ab7e39c04e376ba19d2b3c91764904d1ee60
sadrak105 <sadrak10@gmail.com>
2018-04-20 12:23:59 -0500

    added fetchSmurfs in actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,27 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+// Action Types and Import Axios
+import axios from 'axios';
+export const PENDING_SMURFS = 'PENDING_SMURFS';
+export const SUCCESS_SMURFS = 'SUCCESS_SMURFS';
+export const ERROR_SMURFS = 'ERROR_SMURFS';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+// Action Types Functions
+
+export const fetchSmurfs = () => {
+  const smurfs = axios.get('http://localhost:5555/api/smurfs');
+  return dispatch => {
+    dispatch({ type: PENDING_SMURFS });
+    smurfs
+      .then(response => {
+        dispatch({
+          type: SUCCESS_SMURFS,
+          payload: response.data
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: ERROR_SMURFS,
+          payload: 'ERROR GETTING SMURFS'
+        });
+      });
+  };
+};
