e2884af47426df091f795e7f7dc4b964619e26b6
Devon Bacon <devon.bacon@gmail.com>
2018-04-20 10:11:21 -0700

    Added form field for name in SmurfForm.js

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -5,7 +5,15 @@ export const SmurfForm = props => {
     // Set up input fields that control
     // the state of our App component  
     <div className="smurfForm">
-      
+      <form className="smurfForm_inputFields">
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={props.formInput.name}
+          onChange={props.updateInputState}
+        />
+      </form>
     </div>
   )
 }
