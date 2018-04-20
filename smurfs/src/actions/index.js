a8d84d43aace135e510ed17485f6f0d910df62bf
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-04-20 10:32:04 -0700

    added get and post

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,13 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+import axios from 'axios';
+import { create } from 'domain';
+export const FETCH_SMURFS = 'FETCH_SMURFS';
+export const FETCHED_SMURFS = 'FETCHED_SMURFS';
+export const ERROR_FETCHING = 'ERROR_FETCHING';
+export const ADD_SMURF = 'ADD_SMURF';
+export const ERROR_ADDING = 'ERROR_ADDING';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +19,39 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+  const smurfs = axios.get(`http://localhost:3333/smurfs`);
+  return dispatch => {
+    dispatch({ type: FETCH_SMURFS });
+    smurfs
+      .then(response => {
+        dispatch({
+          type: FETCHED_SMURFS,
+          payload: response.data
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: ERROR_FETCHING,
+          payload: 'ERROR Fetching Smurfs'
+        });
+      });
+  };
+};
+
+export const addSmurf = data => {
+  const smurfs = axios.post(`http://localhost:3333/smurfs`, data);
+    return dispatch => {
+      smurfs
+      .then(response=> {
+        dispatch({ type: ADD_SMURF, payload: response.data })
+      })
+      .catch(err => {
+        dispatch({
+          type: ERROR_ADDING,
+          payload: 'ERROR adding Smurf'
+        });
+      });
+    };
+};
