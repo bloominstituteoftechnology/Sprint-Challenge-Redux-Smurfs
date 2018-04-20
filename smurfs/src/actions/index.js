dc0f51f519ce6dcf2bfe586a002792a161573e8d
unknown <bsa6.23.94@gmail.com>
2018-04-20 11:36:33 -0400

    get and post done

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,13 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const FETCHING = "FETCHING";
+export const FETCHED = "FETCHED";
+export const SAVE = 'SAVE';
+export const FAIL = 'FAIL';
+export const SAVED = 'SAVED';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +20,31 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurf = () => {
+  const loco = axios.get(`http://localhost:3333/smurfs`);
+  return dispatch => {
+    loco
+    .then(response => {
+      dispatch({ type: FETCHED, payload: response.data })
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FAIL});
+    })
+  }
+}
+
+export const addSmurf = data => {
+  const create = axios.post(`http://localhost:3333/smurfs`, data);
+  return dispatch => {
+    create
+    .then(response => {
+      dispatch({ type: SAVED, payload: response.data })
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FAIL });
+    })
+  }
+}
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getSmurf, addSmurf} from '../actions';
+import {connect} from 'react-redux';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +9,64 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      newName: '',
+      newAge: '',
+      newHeight: ''
+    }
+  }
+    componentDidMount() {
+      this.props.getSmurf();
+    }
+    newSmurf = () => {
+      const smurfE = {
+        name: this.state.newName,
+        age: this.state.newAge,
+        height: this.state.newHeight
+      }
+      this.props.addSmurf(smurfE);
+      this.setState({ newName: '', newAge: '', newHeight: ''});
+    }
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+         <input 
+          type='text'
+          placeholder="name"
+          name="newName"
+          value={this.state.newName}
+          onChange={e => this.setState({ [e.target.name]: e.target.value})}
+/>
+<input 
+          type='number'
+          placeholder="height"
+          name="newHeight"
+          value={this.state.newHeight}
+          onChange={e => this.setState({ [e.target.name]: e.target.value})}
+/>
+<input 
+          type='number'
+          placeholder="age"
+          name="newAge"
+          value={this.state.newAge}
+          onChange={e => this.setState({ [e.target.name]: e.target.value})}
+/>
+<button 
+onClick={this.newSmurf}>Click Here</button>
+{this.props.smurfs.map(smurf => {
+  return <ul key={smurf.id}>{smurf.name} <br/> {smurf.height} <br/> {smurf.age}</ul>
+})}
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  }
+}
 
-export default App;
+export default connect (mapStateToProps, { getSmurf, addSmurf })(App);
