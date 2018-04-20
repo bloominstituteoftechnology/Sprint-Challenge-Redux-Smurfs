866597fcd0869515b31675e681e81c129751a0c3
Jonathan Greene <greeneselectronics@gmail.com>
2018-04-20 11:03:48 -0600

    Setup the project and got my store and actions done, imported all dependicies

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,13 @@
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
-*/
+  */
+  import axios from 'axios';
+  export const FETCHING_SMURFS = "FETCHING_SMURFS";
+  export const FETCHED_SMURFS = "FETCHED_SMURFS";
+  export let ERROR = "ERROR";
+  export const ADD_SMURF ="ADD_SMURF";
+
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +19,15 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export function addSmurf (test) {
+  console.log(test);
+  return {type:ADD_SMURF, payload: test }
+}
+export function retriveSmurf (test) {
+  console.log(test);
+  return {type:FETCHING_SMURFS, payload: test }
+}
+// export function addSmurf (test) {
+//   console.log(test);
+// }
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,22 +1,28 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {ADD_SMURF, ERROR, FETCHED_SMURFS, FETCHING_SMURFS, addSmurf} from '../actions';
 
+
+//default state
+const initialState = [{text:'testy smurf', completed: false, id: 0 }]
 /*
- Your initial/default state for this project could look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurfs: false
-   error: null
- }
-*/
-
+ Your initial/default state for this project could look a lot like thi
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+
+export const smurfsReducer = (statev = initialState, action) => {
+  switch(action) {
+    case ADD_SMURF:
+    case FETCHING_SMURFS:
+    case  FETCHED_SMURFS:
+    default:
+    return
+
+  }
+};
+
