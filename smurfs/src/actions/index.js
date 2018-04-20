dc9ac5278a807a6587137926a14d039cc40cc568
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-20 08:50:04 -0700

    added middleware dependencies/ filled out action types/creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const PENDING_SMURFS = 'PENDING_SMURFS';
+export const SUCCESS_SMURFS = 'SUCCESS_SMURFS';
+export const ERROR_SMURFS = 'ERROR_SMURFS';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +16,32 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurfs = () => {
+  const smurfs = axios.get('http://localhost:3333/api/smurfs');
+  return dispatch => {
+    dispatch({ type: PENDING_SMURFS });
+    smurfs
+      .then(response => {
+        console.log(response);
+        dispatch({ type: SUCCESS_SMURFS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR_SMURFS, payload: 'ERROR FETCHING SMURFS' });
+      });
+  }
+}
+
+export const createSmurf = data => {
+  const smurfs = axios.post('http://localhost:3333/api/smurfs', data);
+  return dispatch => {
+    dispatch({ type: PENDING_SMURFS });
+    smurfs
+      .then(response => {
+        console.log(response);
+        dispatch(fetchSmurfs());
+      })
+      .catch(err => {
+        dispatch({ type: ERROR_SMURFS, payload: 'ERROR POSTING SMURFS' });
+      });
+  }
+}
