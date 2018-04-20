996b6d83e582d06081d15a371a71ef8293d4a91b
Devon Bacon <devon.bacon@gmail.com>
2018-04-20 10:01:58 -0700

    Added JSX to display smurf info in SmurfCard.js

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -3,7 +3,11 @@ import React from 'react';
 export const SmurfCard = props => {
   return ( // Take data from props to display smurf
     <div className="smurfCard">
-      
+      <div className="smurfCard_info">
+        <div>{props.smurf.name} is</div>
+        <div>{props.smurf.age} years old</div>
+        <div>and {props.smurf.height} inches tall!</div>
+      </div>
     </div>
   )
 }
