2e8d575ff4ad1983d3b3af5488c11fb2ec8534a2
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-20 12:34:06 -0400

    Set up the rootReducer, fixed some spelling errors in ReadMe

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sore of reducer */ './reducers';
+import { createStore, applyMiddleware } from 'redux';
+
+import './index.css';
+import App from './components/App';
+import rootReducer from './reducers';
 
 const store = createStore(
-  () => {}, // this is the most basic reducer. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
+  rootReducer, // this is the most basic reducer. Replace it.
+  applyMiddleware(thunk,logger)
 );
 
 ReactDOM.render(
