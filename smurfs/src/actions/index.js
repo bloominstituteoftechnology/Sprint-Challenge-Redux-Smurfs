01686f1e9c2573b964285ee25b3168e1f44b1d8b
tanking <tanking@github.com>
2018-04-20 13:50:20 -0400

    Set up actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -32,7 +32,7 @@ export const fetchSmurfs = () => {
     promise
     .then((response) => {
       console.log(response);
-      dispatch( {type: SUCCESS_FETCHING_SMURFS, payload: ''});
+      dispatch( {type: SUCCESS_FETCHING_SMURFS, payload: response.data});
     })
     .catch(err => {
       dispatch( {type: ERROR_FETCHING_SMURFS, payload: 'Error Fetching Data' });
@@ -47,7 +47,7 @@ export const postSmurfs = () => {
     promise
     .then((response) => {
       console.log(response);
-      dispatch({ type: SUCCESS_POSTING_SMURFS });
+      dispatch({ type: SUCCESS_POSTING_SMURFS, payload: response.data });
     })
     .catch(err => {
       dispatch( {type: ERROR_POSTING_SMURFS, payload: 'Error Posting Data' });
