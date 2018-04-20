5b53f32260fdfeef424ee76154815aa7e90345f3
Justin Hong <jhong2009@gmail.com>
2018-04-20 09:34:52 -0700

    in reducers/index.js: set up switch() cases for fetching, fetched, and error

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -8,15 +8,21 @@ const initialState = {
   deletingSmurfs: false,
   error: null
 };
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  Components can read your store as, `state` and not `state.fooReducer`.
-*/
-
+// root reducer
 const smurfsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING_SMURFS:
+      return Object.assign({}, state, { fetchingSmurfs: true });
+    case FETCHED_SMURFS:
+      return Object.assign({}, state, {
+        fetchingSmurfs: false,
+        smurfs: [...action.payload];
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        fetchingSmurfs: false,
+        error: action.payload
+      });
     default:
       return state;
   }
