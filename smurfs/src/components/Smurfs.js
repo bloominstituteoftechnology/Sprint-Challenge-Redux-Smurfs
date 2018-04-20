3fecae47732b8430624be20d818efc0700b52da8
Justin Hong <jhong2009@gmail.com>
2018-04-20 11:00:17 -0700

    in Smurfs.js: adjust component to render all smurf's data. in reducers/index.js: fixed bug where duplicates were being made

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
 const Smurfs = props => {
-	console.log("PROPS", props);
+	// console.log("PROPS", props);
 	return (
 		<ul>
 			{props.smurfs.map((smurf, index) => {
-				return <li>smurf.name</li>;
+				return [
+					<li>{smurf.name}</li>,
+					<li>{smurf.age}</li>,
+					<li>{smurf.height}</li>
+				];
 			})}
 		</ul>
 	);
