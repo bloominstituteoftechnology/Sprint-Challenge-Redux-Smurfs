ebd977c6b68dd4a33633f556d9ddd6c0ba9db6f0
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-20 09:02:56 -0700

    imported rootReducer and added it to create store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,10 +6,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sore of reducer */ './reducers';
+import rootReducer from './reducers';
 
 const store = createStore(
-  () => {}, // this is the most basic reducer. Replace it.
+  rootReducer, // this is the most basic reducer. Replace it.
   applyMiddleware(thunk, logger)
 );
 
