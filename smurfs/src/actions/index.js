9d45143c10bad555714c9c2fd4dbd8710ccf438c
Henry K <mykim1891@hotmail.com>
2018-04-20 09:48:54 -0700

    Finished the Actions part

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,87 @@
+import axios from "axios";
+
+import * as Actions from "./actionTypes";
+
+export { fetchSmurf, addSmurf, deleteSmurf, updateSmurf}
+
+const fetchSmurf = () => {
+  const smurfs = axios.get(`http://localhost:3333/smurfs`);
+  return dispatch => {
+    dispatch({ type: Actions.FETCHING_SMURF });
+
+    smurfs
+      .then(response => {
+        dispatch({
+          type: Actions.SUCCESS_FRIENDS,
+          payload: response.data
+        });
+      })
+
+      .catch(err => {
+        dispatch({
+          type: Actions.ERROR_SMURFS,
+          payload: "ERROR: fetching Smurfs lists"
+        });
+      });
+  };
+};
+
+const addSmurf = data => {
+  const smurfs = axios.post(`http://localhost:3333/smurfs`, data);
+  return dispatch => {
+    dispatch({ type: Actions.ADDING_SMURF });
+    smurfs
+      .then(response => {
+        dispatch(fetchSmurf());
+      })
+      .catch(err => {
+        dispatch({
+          type: Actions.ERROR_SMURFS,
+          payload: "ERROR: Could not add new smurfs."
+        });
+      });
+  };
+};
+
+const deleteSmurf = smurfId => {
+  const smurfs = axios.delete(`http://localhost:3333/smurfs/${smurfId}`, smurfId);
+  return dispatch => {
+    dispatch({ type: Actions.DELETING_SMURF });
+
+    smurfs
+      .then(response => {
+        dispatch(fetchSmurf());
+      })
+      .catch(err => {
+        dispatch({
+          type: Actions.ERROR_SMURFS,
+          payload: "ERROR: Deletion failed."
+        });
+      });
+  };
+};
+
+const updateSmurf = data => {
+  const smurfs = axios.post(`http://localhost:3333/smurfs`, data);
+  return dispatch => {
+    dispatch({ type: Actions.UPDATING_SMURF });
+
+    smurfs
+      .then(response => {
+        dispatch({
+          type: Actions.SUCCESS_FRIENDS,
+          payload: response.data
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: Actions.ERROR_SMURFS,
+          payload: "ERROR could not add new smurfs."
+        });
+      });
+  };
+};
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from  './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./reducers";
 
 const store = createStore(
   rootReducer, // this is the most basic reducer. Replace it.
@@ -17,5 +17,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
