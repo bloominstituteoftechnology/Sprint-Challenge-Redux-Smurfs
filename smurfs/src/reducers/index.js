8056ae2392f1fb5c7e1188a947d43d0e99bd288b
Justin Hong <jhong2009@gmail.com>
2018-04-20 09:19:33 -0700

    in reducers/index.js: set up smurfsReducers() with switch()

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,16 +1,25 @@
 import { FETCHING_SMURFS, FETCHED_SMURFS, ERROR } from "../actions/index.js";
 
-const initial state = {
+const initialState = {
   smurfs: [],
   fetchingSmurfs: false,
   addingSmurf: false,
   updatingSmurf: false,
   deletingSmurfs: false,
   error: null
-}
+};
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+
+const smurfsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default smurfsReducer;
