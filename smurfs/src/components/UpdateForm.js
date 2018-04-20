53ecfb6072cb5e42280ae67af17082d51c5d4db1
Justin Hong <jhong2009@gmail.com>
2018-04-20 16:16:32 -0700

    in components/UpdateForm.js: set up handleUpdateForm() to trigger updateSmurfs() action creator

--- smurfs/src/components/UpdateForm.js
+++ smurfs/src/components/UpdateForm.js
@@ -1,30 +1,64 @@
 import React from "react";
 
-const UpdateForm = props => {
-	console.log("Props: ", props);
-	return (
-		<form>
-			<input
-				type="text"
-				name="name"
-				placeholder="Enter smurf name"
-				onChange={props.handleNewInput}
-			/>
-			<input
-				type="text"
-				name="age"
-				placeholder="Enter smurf age"
-				onChange={props.handleNewInput}
-			/>
-			<input
-				type="text"
-				name="height"
-				placeholder="Enter smurf height"
-				onChange={props.handleNewInput}
-			/>
-			<button type="button">Save Changes</button>
-		</form>
-	);
-};
+class UpdateForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: "",
+			age: "",
+			height: "",
+			id: this.props.id
+		};
+	}
+	// update state with new smurf data
+	handleNewInput = event => {
+		this.setState({ [event.target.name]: [event.target.value] });
+	};
+	// trigger updateForm() action creator
+	handleUpdateForm = () => {
+		const smurf = {
+			name: this.state.name,
+			age: this.state.age,
+			height: this.state.height
+		};
+		this.props.update(smurf);
+		this.setState({ name: "", age: "", height: "" });
+	};
+	render() {
+		// console.log("name: ", this.state.name);
+		// console.log("age: ", this.state.age);
+		// console.log("height: ", this.state.height);
+		return (
+			<form>
+				<input
+					type="text"
+					name="name"
+					placeholder="Enter smurf name"
+					onChange={this.handleNewInput}
+				/>
+				<input
+					type="text"
+					name="age"
+					placeholder="Enter smurf age"
+					onChange={this.handleNewInput}
+				/>
+				<input
+					type="text"
+					name="height"
+					placeholder="Enter smurf height"
+					onChange={this.handleNewInput}
+				/>
+				<button
+					type="button"
+					onClick={() => {
+						this.handleUpdateForm();
+					}}
+				>
+					Save Changes
+				</button>
+			</form>
+		);
+	}
+}
 
 export default UpdateForm;
