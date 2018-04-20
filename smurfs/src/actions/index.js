e8d1b2c40a8734986d347835f48a9357013ad898
SyCoDe <SyCoDe@gmail.com>
2018-04-20 11:47:15 -0600

    project finished including stretch, working on styling and tweaks

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,49 @@
+import axios from 'axios';
+export const GET_SMURFS = 'GET_SMURFS';
+export const POST_SMURFS = 'POST_SMURFS';
+export const PUT_SMURFS = 'PUT_SMURFS';
+export const DELETE_SMURFS = 'DELETE_SMURFS';
+export const ERROR = 'ERROR';
+
+export const getSmurf = () => dispatch => {
+  axios.get(`http://localhost:3333/smurfs`)
+  .then(
+    response => { dispatch({ type: GET_SMURFS, smurfs: response.data })}
+  )
+  .catch(
+    error => { dispatch( {type: ERROR, errorMessage: error.response.data.Error})}
+  )
+}
+
+export const postSmurf = (smurf) => dispatch => {
+  axios.post(`http://localhost:3333/smurfs`, smurf)
+  .then(
+    response => { dispatch({ type: POST_SMURFS, smurfs: response.data })}
+  )
+  .catch(
+    error => { dispatch( {type: ERROR, errorMessage: error.response.data.Error})}
+  )
+}
+
+export const putSmurf = (id, smurf) => dispatch => {
+  axios.put(`http://localhost:3333/smurfs/${id}`, smurf)
+  .then(
+    response => { dispatch({ type: PUT_SMURFS, smurfs: response.data })}
+  )
+  .catch(
+    error => { dispatch( {type: ERROR, errorMessage: error.response.data.Error})}
+  )
+}
+
+export const deleteSmurf = (id) => dispatch => {
+  axios.delete(`http://localhost:3333/smurfs/${id}`)
+  .then(
+    response => { dispatch({ type: DELETE_SMURFS, id: response.data.SmurfRemoved.id })}
+  )
+  .catch(
+    error => { dispatch( {type: ERROR, errorMessage: error.response.data.Error})}
+  )
+}
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
