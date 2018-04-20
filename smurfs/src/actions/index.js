3041433dc59e72f5e0bd8a9aa90539cf65763fbb
Jared Cuffe <jcuffe@gmail.com>
2018-04-20 12:12:08 -0700

    dried up some actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -9,57 +9,57 @@ export const UPDATED_SMURF = 'updated-smurf'
 export const DELETING_SMURF = 'deleting-smurf'
 export const DELETED_SMURF = 'deleted-smurf'
 
-const fetchStart = () => ({ type: FETCH_START })
 const fetchError = (payload) => ({ type: FETCH_ERROR, payload })
 const fetchedSmurfs = (payload) => ({ type: FETCHED_SMURFS, payload })
 const fetchedSmurf = (payload) => ({ type: FETCHED_SMURF, payload })
-const updatingSmurf = () => ({ type: UPDATING_SMURF })
-const updatedSmurf = () => ({ type: UPDATED_SMURF })
-const deletingSmurf = () => ({ type: DELETING_SMURF })
-const deletedSmurf = () => ({ type: DELETED_SMURF })
+const fetchStart = { type: FETCH_START }
+const updatingSmurf = { type: UPDATING_SMURF }
+const updatedSmurf = { type: UPDATED_SMURF }
+const deletingSmurf = { type: DELETING_SMURF }
+const deletedSmurf = { type: DELETED_SMURF }
 
 export const fetchSmurfs = () => (dispatch) => {
-  dispatch(fetchStart())
+  dispatch(fetchStart)
   axios.get('http://localhost:3333/smurfs/')
     .then(({ data }) => dispatch(fetchedSmurfs(data)))
     .catch((error) => dispatch(fetchError(error)))
 }
 
 export const fetchSmurf = (id) => (dispatch) => {
-  dispatch(fetchStart())
+  dispatch(fetchStart)
   axios.get(`http://localhost:3333/smurfs/${id}`)
   .then(({ data }) => {
       dispatch(fetchedSmurf(data))
-      dispatch(updatingSmurf())
+      dispatch(updatingSmurf)
     })
     .catch((error) => dispatch(fetchError(error)))
 }
 
 export const createSmurf = ({ name, age, height }) => (dispatch) => {
   const smurf = { name, age, height }
-  dispatch(fetchStart())
+  dispatch(fetchStart)
   axios.post('http://localhost:3333/smurfs/', smurf)
     .then(({ data }) => dispatch(fetchedSmurfs(data)))
     .catch((error) => dispatch(fetchError(error)))
 }
 
 export const updateSmurf = (smurf) => (dispatch) => {
-  dispatch(fetchStart())
+  dispatch(fetchStart)
   axios.put(`http://localhost:3333/smurfs/${smurf.id}`, smurf)
     .then(({ data }) => {
       dispatch(fetchedSmurfs(data))
-      dispatch(updatedSmurf())
+      dispatch(updatedSmurf)
     })
     .catch((error) => dispatch(fetchError(error)))
 }
 
 export const deleteSmurf = (id) => (dispatch) => {
-  dispatch(fetchStart())
-  dispatch(deletingSmurf())
+  dispatch(fetchStart)
+  dispatch(deletingSmurf)
   axios.delete(`http://localhost:3333/smurfs/${id}`)
     .then(({ data }) => {
       dispatch(fetchedSmurfs(data))
-      dispatch(deletedSmurf())
+      dispatch(deletedSmurf)
     })
     .catch((error) => dispatch(fetchError(error)))
 }
