7131cf5df0906cd975615de604033c4f92bc7fab
Devon Bacon <devon.bacon@gmail.com>
2018-04-20 10:04:13 -0700

    Set up empty SmurfForm component in src dir

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const SmurfForm = props => {
+  return ( 
+    // Set up input fields that control
+    // the state of our App component  
+    <div className="smurfForm">
+      
+    </div>
+  )
+}
