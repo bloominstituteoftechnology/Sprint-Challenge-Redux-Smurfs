bfb56de1115a9a868c08f132bb524f06e32b5d0f
nickbrenn <nbren@tutanota.com>
2018-04-20 12:38:34 -0400

    Basic files set up and smurphs can be fetched and displayed

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,22 +1,67 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  FETCHING,
+  FETCHED,
+  CREATING,
+  CREATED,
+  UPDATING,
+  UPDATED,
+  DELETING,
+  DELETED,
+  ERROR
+} from "../actions";
 
-/*
- Your initial/default state for this project could look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurfs: false
-   error: null
- }
-*/
+const initialState = {
+  // starting smurphs to confirm .map is working before adding a way to create new smurphs
+  smurfs: [
+    { id: 0, name: "Eddie Smurphy", age: 50, height: "short" },
+    { id: 1, name: "Eduardo Smurphificius", age: 42, height: "shortish" }
+  ],
+  fetchingSmurfs: false,
+  creatingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  Components can read your store as, `state` and not `state.fooReducer`.
-*/
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING:
+      return Object.assign({}, state, { fetchingSmurfs: true });
+    case FETCHED:
+      return Object.assign({}, state, {
+        smurfs: [...state.friends, ...action.payload],
+        fetchingSmurfs: false
+      });
+    case CREATING:
+      return Object.assign({}, state, { creatingSmurf: true });
+    case CREATED:
+      return Object.assign({}, state, {
+        smurfs: [...action.payload],
+        creatingSmurf: false
+      });
+    case UPDATING:
+      return Object.assign({}, state, { updatingSmurf: true });
+    case UPDATED:
+      return Object.assign({}, state, {
+        smurfs: [...action.payload],
+        updatingSmurf: false
+      });
+    case DELETING:
+      return Object.assign({}, state, { deletingSmurf: true });
+    case DELETED:
+      return Object.assign({}, state, {
+        smurfs: [...action.payload],
+        deletingSmurf: false
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        fetchingSmurfs: false,
+        creatingSmurf: false,
+        updatingSmurf: false,
+        deletingSmurf: false,
+        error: "Error fetching or creating smurfs"
+      });
+    default:
+      return state;
+  }
+};
