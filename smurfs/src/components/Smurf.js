59c1d137875d241cf81ee171e1ce361bbc414713
Justin Hong <jhong2009@gmail.com>
2018-04-20 15:20:36 -0700

    in Smurf.js: converted component to a class component with state holding showUpdateForm bool. created update btn

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -2,22 +2,47 @@ import React from "react";
 import { connect } from "react-redux";
 import { deleteSmurfs } from "../actions/";
 
-const Smurf = props => {
-	return (
-		<div>
-			<p>{`Name: ${props.smurf.name}`}</p>
-			<p>{`Age: ${props.smurf.age}`}</p>
-			<p>{`Height: ${props.smurf.height}`}</p>
-			<button
-				onClick={() => {
-					props.deleteSmurfs(props.smurf.id);
-				}}
-			>
-				Delete Smurf
-			</button>
-		</div>
-	);
-};
+// convert to class component
+// change props to this.props
+// destructure values from this.props
+// create a showUpdateForm: false boolean
+
+class Smurf extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showUpdateForm: false
+		};
+	}
+	render() {
+		const { name, age, height, id } = this.props.smurf;
+		return (
+			<div>
+				<p>{`Name: ${name}`}</p>
+				<p>{`Age: ${age}`}</p>
+				<p>{`Height: ${height}`}</p>
+				<button
+					type="button"
+					onClick={() => {
+						this.props.deleteSmurfs(id);
+					}}
+				>
+					Delete Smurf
+				</button>
+				{/* Button toggler for the update form*/}
+				<button
+					type="button"
+					onClick={() => {
+						this.setState({ showUpdateForm: !this.state.showUpdateForm });
+					}}
+				>
+					Update smurf
+				</button>
+				{/* Show update form if boolean is true */}
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = state => {
 	return {
