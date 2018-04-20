0f33690dab1747d5ea97b24e419c3aaa8c363352
Henry K <mykim1891@hotmail.com>
2018-04-20 12:41:20 -0700

    GET, POST, DELETE are complete

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -27,77 +27,94 @@ class App extends Component {
   // }
 
   render() {
-    console.log("this.props.whatever:", this.props.smurfs); // returning normal ✓
+    console.log("this.props._:", this.props.fetchingSmurfs); // returning normal ✓
     return (
       <div className="App">
         <header>
           <h1>SMURFS! 2.0 W/ Redux</h1>
           <div>Welcome to your Redux version of Smurfs!</div>
-          <div>Start inside of your `src/index.js` file!</div>
-          <div>Have fun!</div>
         </header>
-
+        <br />
+        {/* <InputField inputState={this.state} onChangeFunc={this.onChangeFunc()} /> */}
         <div className="MainContainer">
-          {/* {this.props.fetchSmurf ? <h1>Loading Smurfs</h1> : null} */}
-          {/* <InputField inputState={this.state} onChangeFunc={this.onChangeFunc()} /> */}
+          {this.props.fetchingSmurfs ? (
+            <h1>Loading Smurfs</h1>
+          ) : (
+            <div>
+              <div className="InputField">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  value={this.state.name}
+                  onChange={e =>
+                    this.setState({ [e.target.name]: e.target.value })
+                  }
+                />
 
-          <div className="InputField">
-            <input
-              type="text"
-              name="name"
-              placeholder="name"
-              value={this.state.name}
-              onChange={e => this.setState({ [e.target.name]: e.target.value })}
-            />
+                <input
+                  type="number"
+                  name="age"
+                  placeholder="age"
+                  value={this.state.age}
+                  min="0"
+                  onChange={e =>
+                    this.setState({ [e.target.name]: e.target.value })
+                  }
+                />
 
-            <input
-              type="number"
-              name="age"
-              placeholder="age"
-              value={this.state.age}
-              onChange={e => this.setState({ [e.target.name]: e.target.value })}
-            />
+                <input
+                  type="number"
+                  name="height"
+                  placeholder="height"
+                  value={this.state.height}
+                  min="0"
+                  onChange={e =>
+                    this.setState({ [e.target.name]: e.target.value })
+                  }
+                />
 
-            <input
-              type="text"
-              name="height"
-              placeholder="height"
-              value={this.state.height}
-              onChange={e => this.setState({ [e.target.name]: e.target.value })}
-            />
+                <button
+                  onClick={() => {
+                    this.props.addSmurf({
+                      name: this.state.name,
+                      age: this.state.age,
+                      height: this.state.height
+                    });
+                    this.setState({ name: "", age: "", height: "" });
+                  }}
+                >
+                  Add smurf
+                </button>
 
-            <button
-              onClick={() => {
-                this.props.addSmurf({
-                  name: this.state.name,
-                  age: this.state.age,
-                  height: this.state.height
-                });
-                this.setState({ name: "", age: "", height: "" });
-              }}
-            >
-              Add smurf
-            </button>
-
-            {/*end of 'InputField' */}
-          </div>
-
-          <div className="SmurfList">
-            <h1>SmurfList</h1>
-            {console.log("this.props.smurfs", this.props.smurfs)}
-            {this.props.smurfs.map(eachSmurf => (
-              <div key={eachSmurf.id}>
-                {console.log("eachSmurf console", eachSmurf)}
-                <div>Name: {eachSmurf.name}</div>
-                <div>Age: {eachSmurf.age}</div>
-                <div>height: {eachSmurf.height}</div>
+                {/*end of 'InputField' */}
               </div>
-            ))}
-          </div>
 
-          {/*end of 'MainContainer' */}
+              <div className="SmurfList">
+                <h1>Smurf List</h1>
+                {this.props.smurfs.map(eachSmurf => (
+                  <div key={eachSmurf.id}>
+                    {console.log("eachSmurf console", eachSmurf)}
+                    <div>Name: {eachSmurf.name}</div>
+                    <div>Age: {eachSmurf.age}</div>
+                    <div>height: {eachSmurf.height}cm</div>
+                    <br />
+                    <div className="DeleteSmurf">
+                      <button
+                        onClick={() => this.props.deleteSmurf(eachSmurf.id)}
+                      >
+                        Remove Smurf
+                      </button>
+                    </div>
+                  </div>
+                ))}
+                {/*end of 'SmurfList' */}
+              </div>
+
+              {/*end of 'MainContainer' */}
+            </div>
+          )}
         </div>
-
         {/*end of 'App' */}
       </div>
     );
@@ -108,7 +125,7 @@ const mapStateToProps = state => {
   return {
     smurfs: state.smurfs,
     error: state.error,
-    fetchSmurf: state.fetchSmurf,
+    fetchingSmurfs: state.fetchingSmurfs,
     addingSmurf: state.addingSmurf,
     deleteSmurf: state.deleteSmurf,
     updateSmurf: state.updateSmurf
