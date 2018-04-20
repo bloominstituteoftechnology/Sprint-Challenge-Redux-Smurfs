093fcffd8f08f4fdc5eff9b824c40a551efe20a4
Devon Bacon <devon.bacon@gmail.com>
2018-04-20 09:52:14 -0700

    Removed comment from bottom of actions index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -82,14 +82,3 @@ export const deleteSmurf = id => {
       });
   }
 }
-
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
