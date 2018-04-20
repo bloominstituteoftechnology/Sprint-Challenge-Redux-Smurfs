f4e0d4fda7a34cb22e7307cdc1a5c64ebb23941c
Henry K <mykim1891@hotmail.com>
2018-04-20 09:08:58 -0700

    Setting up general structure of Reducers

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,25 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { FETCHING_SMURF, ADDING_SMURF, UPDATING_SMURF, DELETING_SMURF, SUCCESS_SMURFS, ERROR_SMURFS } from "../actions"
 
+const initialState = {
+    smurfs: [],
+    fetchingSmurfs: false
+    addingSmurf: false
+    updatingSmurf: false
+    deletingSmurfs: false
+    error: null
+}
+
+const smurfsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_SMURF:
+    return Object.assign((), state, {fetchingSmurfs: true})
+    case ADDING_SMURF:
+  }
+
+}
 /*
  Your initial/default state for this project could look a lot like this
  {
