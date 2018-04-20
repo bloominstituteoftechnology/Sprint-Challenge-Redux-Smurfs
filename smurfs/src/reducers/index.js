252f0f8b17b8cf0f927188e68b4255318818575e
Justin Hong <jhong2009@gmail.com>
2018-04-20 09:40:37 -0700

    in reducers/index.js: added case for AddingSmurf

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,4 +1,9 @@
-import { FETCHING_SMURFS, FETCHED_SMURFS, ERROR } from "../actions/index.js";
+import {
+  FETCHING_SMURFS,
+  ADDING_SMURFS,
+  FETCHED_SMURFS,
+  ERROR
+} from "../actions/index.js";
 
 const initialState = {
   smurfs: [],
@@ -13,10 +18,13 @@ const smurfsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCHING_SMURFS:
       return Object.assign({}, state, { fetchingSmurfs: true });
+    case ADDING_SMURFS:
+      return Object.assign({}, state, { addingSmurf: true });
     case FETCHED_SMURFS:
       return Object.assign({}, state, {
         fetchingSmurfs: false,
-        smurfs: [...action.payload];
+        addingSmurf: false,
+        smurfs: [...action.payload]
       });
     case ERROR:
       return Object.assign({}, state, {
