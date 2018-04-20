3417d0d26b916f745765947812407ffbdbd4954c
sadrak105 <sadrak10@gmail.com>
2018-04-20 12:40:49 -0500

    modified actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -7,11 +7,12 @@ export const ERROR_SMURFS = 'ERROR_SMURFS';
 // Action Types Functions
 
 export const fetchSmurfs = () => {
-  const smurfs = axios.get('http://localhost:5555/api/smurfs');
+  const smurfs = axios.get('http://localhost:3333/api/smurfs');
   return dispatch => {
     dispatch({ type: PENDING_SMURFS });
     smurfs
       .then(response => {
+        console.log(response);
         dispatch({
           type: SUCCESS_SMURFS,
           payload: response.data
