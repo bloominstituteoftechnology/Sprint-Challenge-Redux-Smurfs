c02cc534eee96e5a1da91af5515f62fc1af438e4
Devon Bacon <devon.bacon@gmail.com>
2018-04-20 09:32:17 -0700

    Added imports to App component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getSmurfs, addSmurf, updateSmurf, deleteSmurf } from '../actions';
 import './App.css';
+import { SmurfCard } from './SmurfCard';
+import { SmurfForm } from './SmurfForm';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I connect my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
 class App extends Component {
   render() {
     return (
@@ -20,3 +27,5 @@ class App extends Component {
 }
 
 export default App;
+
+
