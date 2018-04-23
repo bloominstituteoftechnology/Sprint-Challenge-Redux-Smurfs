a8a4fecbd8740cb783e2c5db373938827bbcfa8f
Justin Hong <jhong2009@gmail.com>
2018-04-22 20:58:56 -0700

    in Smurf.js: contained smurf data inside a Card component

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-import { deleteSmurfs, updateSmurfs } from "../actions/";
 import RaisedButton from "material-ui/RaisedButton";
+import {
+	Card,
+	CardActions,
+	CardHeader,
+	CardMedia,
+	CardTitle,
+	CardText
+} from "material-ui/Card";
+import FlatButton from "material-ui/FlatButton";
+import { deleteSmurfs, updateSmurfs } from "../actions/";
+
 import UpdateForm from "./UpdateForm";
 
 class Smurf extends React.Component {
@@ -17,36 +27,41 @@ class Smurf extends React.Component {
 		};
 		const { name, age, height, id } = this.props.smurf;
 		return (
-			<div>
-				<p>{`Name: ${name}`}</p>
-				<p>{`Age: ${age}`}</p>
-				<p>{`Height: ${height}`}</p>
-				<RaisedButton
-					primary={true}
-					style={style}
-					type="button"
-					onClick={() => {
-						this.props.deleteSmurfs(id);
-					}}
-				>
-					Delete Smurf
-				</RaisedButton>
-				{/* Button toggler for the update form*/}
-				<RaisedButton
-					primary={true}
-					style={style}
-					type="button"
-					onClick={() => {
-						this.setState({ showUpdateForm: !this.state.showUpdateForm });
-					}}
-				>
-					Update smurf
-				</RaisedButton>
-				{/* Show update form if boolean is true */}
-				{this.state.showUpdateForm ? (
-					<UpdateForm id={id} update={this.props.updateSmurfs} />
-				) : null}
-			</div>
+			<Card>
+				<CardHeader title={name} />
+				<CardTitle title="Smurf Information" subtitle={`Smurf ${id}`} />
+				<CardText>
+					<p>{`Age: ${age}`}</p>
+					<p>{`Height: ${height}`}</p>
+				</CardText>
+				<CardActions>
+					<RaisedButton
+						primary={true}
+						style={style}
+						type="button"
+						onClick={() => {
+							this.props.deleteSmurfs(id);
+						}}
+					>
+						Delete
+					</RaisedButton>
+					{/* Button toggler for the update form*/}
+					<RaisedButton
+						primary={true}
+						style={style}
+						type="button"
+						onClick={() => {
+							this.setState({ showUpdateForm: !this.state.showUpdateForm });
+						}}
+					>
+						Update
+					</RaisedButton>
+					{/* Show update form if boolean is true */}
+					{this.state.showUpdateForm ? (
+						<UpdateForm id={id} update={this.props.updateSmurfs} />
+					) : null}
+				</CardActions>
+			</Card>
 		);
 	}
 }
