88369ddb15d39594dd76c2b39fb05256bfa317c2
Jesse Reichel <31119775+KryoKorpz@users.noreply.github.com>
2018-04-28 12:52:05 -0700

    final commit

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 export const Smurfs = (props) => {
-
+if (!props) {
+    return <h1> Finding your smurfs </h1>
+} else {
     return (
         <div>
             {/* {props.map((smurf) => {
                 return <h1> {smurf} </h1>
             })} */}
-            {console.log(props)}
+            {console.log}
                     </div>
     )
   }
+}
