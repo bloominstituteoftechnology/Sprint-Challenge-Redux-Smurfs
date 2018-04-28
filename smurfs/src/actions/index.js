e37e9866089ef55037820ef76372e927df7b2723
Andrew Jarrett <ahrjarrett@gmail.com>
2018-04-28 12:23:06 -0600

    clean up actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,23 +13,14 @@ const ROOT_URL = 'http://localhost:3333'
 export const getSmurfs = () => dispatch => {
   dispatch({ type: FETCHING })
   axios.get(`${ROOT_URL}/smurfs`)
-    .then((response) => {
-
-      console.log('response:', response)
-
-      dispatch({ type: SUCCESS, payload: response.data })
-    }).catch(error => dispatch({ type: ERROR, error }))
+    .then(({ data }) => dispatch({ type: SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: ERROR, error }))
 }
 
 export const addSmurf = smurf => dispatch => {
   dispatch({ type: CREATING })
   axios.post(`${ROOT_URL}/smurfs`, smurf)
-    .then(({ data }) => {
-      dispatch({ type: SUCCESS, payload: data })
-
-    })
-    .catch(error => {
-      dispatch({ type: ERROR, error })
-    })
+    .then(({ data }) => dispatch({ type: SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: ERROR, error }))
 }
 
