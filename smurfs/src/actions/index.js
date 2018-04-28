b58081922efa6245518a2fb76bd2f74fd454e3bf
Bhavik Ravani <ravanibhavik@gmail.com>
2018-04-28 13:48:43 -0400

    Completed Get Smurfs.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,14 @@
-/* 
+import axios from 'axios';
+
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const GET_SMURFS = 'GET_SMURFS';
+export const POST_SMURFS = 'POST_SMURFS';
+export const FETCHED = 'FETCHED';
+export const FETCHING = 'FETCHING';
+export const ERROR = 'ERROR';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +20,15 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+  const request = axios.get('http://localhost:3333/smurfs');
+  return (dispatch) => {
+    dispatch({type: 'FETCHING'})
+    request.then((response) => {
+      dispatch({type: 'FETCHED', payload: response.data})
+    }).catch((error) => {
+      dispatch({type: ERROR, error: 'ERROR FETCHING DATA'})
+    })
+  }
+}
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,21 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
+ I'll let you do this part on your own.
  Just remember, `how do I connect my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
+
+
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <div>
+          { this.props.fetchingSmurfs ?
+            <h3>FETCHING SMURFS DATA...</h3> :
+            <ul>
+            { this.props.smurfs.map(smurf => (
+              <li>{ smurf.name }</li>
+            ))}
+            </ul>
+          }
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  }
+}
+
+export default connect(mapStateToProps, { getSmurfs })(App);
