7292948e702f5b9e6a3f5fc450920a6b365a632f
unknown <alexisreyes90@gmail.com>
2018-04-28 14:06:09 -0400

    Axios.get and axios.post funcionality complete and working

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+
+import { getSmurfs, postSmurf } from '../actions';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I connect my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
 class App extends Component {
+
+  state = {
+    name: '',
+    age: '',
+    height: '',
+  }
+
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
+  handleInputChange = e => {
+    // set the state
+
+    this.setState({[ e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    const { name, age, height } = this.state;
+    //takes click event set all to state on button press
+
+    e.preventDefault();
+    this.props.postSmurf({ name, age, height });
+    this.setState({ name: '', age: '', height: '' })
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,9 +45,58 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <h2> Add a Smurf! </h2>
+          <input
+            onChange={this.handleInputChange}
+            value={this.state.name}
+            type="text"
+            name="name"
+            placeholder="smurf name"
+            />
+          <input
+            onChange={this.handleInputChange}
+            value={this.state.age}
+            type="text"
+            name="age"
+            placeholder="smurf age"
+            />
+          <input
+            onChange={this.handleInputChange}
+            value={this.state.height}
+            type="text"
+            name="height"
+            placeholder="smurf height"
+            />
+            <button onClick={this.handleSubmit}>Add a Smurf!</button>
+
+            <p className="App-intro">
+            My Smurfs
+          </p>
+          <div>
+            {this.props.smurfs.map(smurf => {
+              return(
+                <div key={smurf.id} className="Smurf-box">
+                  <div>Smurf Name: {smurf.name}</div>
+                  <div>Smurf Age: {smurf.age}</div>
+                  <div>Smurf Height: {smurf.height}</div>
+                </div>
+              );
+            })}
+          </div>
+
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log("state", state);
+  return {
+    smurfs: state.smurfs,
+    loading: state.loading,
+    success: state.success,
+    error: "",
+  };
+}
+
+export default connect(mapStateToProps, { getSmurfs, postSmurf })(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,8 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import { LOADING, SUCCESS, ERROR } from '../actions';
+
 /*
  Your initial/default state for this project could look a lot like this
  {
@@ -14,9 +16,34 @@
  }
 */
 
+// adding fetching functionality first
+
+const defaultState = {
+  smurfs: [],
+  smurf: {},
+  fetching: false,
+  error: "",
+};
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
   This will guard your namespacing issues.
   Components can read your store as, `state` and not `state.fooReducer`.
 */
+
+export default (state = defaultState, action) => {
+  switch(action.type) {
+    case LOADING:
+      return Object.assign({}, state, { LOADING: true })
+
+    case SUCCESS:
+      return Object.assign({}, state, { smurfs: action.smurfs, LOADING: false })
+
+    case ERROR:
+      return Object.assign({}, state, { error: action.error , LOADING: false })
+
+    default:
+      return state;
+  }
+}
