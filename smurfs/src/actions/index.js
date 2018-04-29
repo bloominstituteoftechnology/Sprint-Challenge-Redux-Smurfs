1c7b5890743d557d59aeb0d4dd9ed0abd5052139
Andrew Jarrett <ahrjarrett@gmail.com>
2018-04-29 11:57:30 -0600

    refactor actions to dispatch action creators instead of raw actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -12,20 +12,22 @@ import {
 } from './types'
 
 const ROOT_URL = 'http://localhost:3333'
-const fetchSuccess = data => ({ type: SUCCESS, payload: data })
+const sucessWithPayload = data => ({ type: SUCCESS, payload: data })
+const sendErrorMessage = error => ({ type: ERROR, error: error })
+export const inputChange = change => ({ type: INPUT_CHANGE, payload: change })
 
 export const getSmurfs = () => dispatch => {
   dispatch({ type: FETCHING })
   axios.get(`${ROOT_URL}/smurfs`)
-    .then(({ data }) => dispatch(fetchSuccess(data)))
-    .catch(error => dispatch({ type: ERROR, error }))
+    .then(({ data }) => dispatch(sucessWithPayload(data)))
+    .catch(error => dispatch(sendErrorMessage(error)))
 }
 
 export const addSmurf = smurf => dispatch => {
   dispatch({ type: CREATING })
   axios.post(`${ROOT_URL}/smurfs`, smurf)
-    .then(({ data }) => dispatch({ type: SUCCESS, payload: data }))
-    .catch(error => dispatch({ type: ERROR, error }))
+    .then(({ data }) => dispatch(sucessWithPayload(data)))
+    .catch(error => dispatch(sendErrorMessage(error)))
 }
 
 export const editSmurf = id => ({ type: EDITING, payload: id })
@@ -36,19 +38,13 @@ export const updateSmurf = smurf => dispatch => {
   dispatch({ type: UPDATING })
   axios.put(`${ROOT_URL}/smurfs/${id}`, { name, age, height })
     .then(_ => dispatch(getSmurfs()))
-    .catch(error => dispatch({ type: ERROR, error }))
+    .catch(error => dispatch(sendErrorMessage(error)))
 }
 
 export const deleteSmurf = id => dispatch => {
   dispatch({ type: DELETING })
   axios.delete(`${ROOT_URL}/smurfs/${id}`)
     .then(_ => dispatch(getSmurfs()))
-    .catch(error => dispatch({ type: ERROR, error }))
+    .catch(error => dispatch(sendErrorMessage(error)))
 }
 
-export const inputChange = smurfProp => {
-  return {
-    type: INPUT_CHANGE,
-    payload: smurfProp,
-  }
-}
