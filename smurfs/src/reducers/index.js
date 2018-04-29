79527c264d9e24ba362ecf504a5b60ed9337b461
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-04-29 17:26:50 -0500

    Built my reducer.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,4 +1,32 @@
+import { PENDING, GET_SMURFS, ADD_SMURF, DELETE_SMURF, ERROR } from '.../actions';
 
+const startingState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurfs: false,
+  error: null
+}
+
+const reducer = (state = startingState, action) => {
+  switch (action.type) {
+    case PENDING:
+      return Object.assign({}, state, { fetchingSmurfs: true });
+    case GET_SMURFS:
+      return Object.assign({}, state, { fetchingSmurfs: false, smurfs: state.smurfs.concat(action.smurfs) });
+    case ADD_SMURF:
+      return Object.assign({}, state, { addingSmurf: true });
+    case: DELETE_SMURF:
+      return Object.assign({}, state, { deletingSmurfs: true, smurfs: action.smurfs })
+    case ERROR:
+      return Object.assign({}, state, { fetchingSmurfs: false, error: action.error });
+    default: 
+      return state;
+  }
+}
+
+export default reducer;
 
 /*
   Be sure to import in all of the action types from `../actions`
