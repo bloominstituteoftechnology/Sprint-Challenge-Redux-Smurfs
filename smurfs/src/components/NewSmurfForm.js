5d4d82dd56f5cedc3f9ecee7055c4044ff2da7fe
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-18 11:35:38 -0500

    added inputs, button, & handlers to NewSmurfForm

--- smurfs/src/components/NewSmurfForm.js
+++ smurfs/src/components/NewSmurfForm.js
@@ -9,12 +9,30 @@ class NewSmurfForm extends Component {
             newSmurfHeight: ""
         }
     
+    handleNewSmurf = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    } 
+
+    handleSubmitSmurf = () => {
+        this.setState({ newSmurfName: "", newSmurfAge: "", newSmurfHeight: ""})
+    }
 
     render() {
         return [
-            <input></input>,
-            <input></input>,
-            <input></input>
+            <form>
+
+                <input type="text" placeholder="name" name="newSmurfName" value={this.state.newSmurfName} onChange={this.handleNewSmurf}>
+                </input>,
+                
+                <input type="text" placeholder="age" name="newSmurfAge" value={this.state.newSmurfAge} onChange={this.handleNewSmurf}>
+                </input>,
+
+                <input type="text" placeholder="height" name="newSmurfHeight" value={`${this.state.newSmurfHeight} cm`} onChange={this.handleNewSmurf}>
+                </input>
+
+                <button onClick={this.handleSubmitSmurf}>Add Smurf</button>
+                
+            </form>
         ]
             
         
