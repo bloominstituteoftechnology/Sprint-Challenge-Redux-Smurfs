ab6237218b747303d5559fdad187df595137894a
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-18 19:43:59 -0400

    Additional styling to AddEditModal

--- smurfs/src/components/AddEditModal/AddEditModal.js
+++ smurfs/src/components/AddEditModal/AddEditModal.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import './AddEditModal.css';
+
 
 class AddEditModal extends Component {
   constructor(props) {
@@ -9,7 +11,11 @@ class AddEditModal extends Component {
       image: (this.props.smurf !== undefined) ? this.props.smurf.image : '',
       name: (this.props.smurf !== undefined) ? this.props.smurf.name : '',
       age: (this.props.smurf !== undefined) ? this.props.smurf.age : '',
-      height: (this.props.smurf !== undefined) ? this.props.smurf.height : ''
+      height: (this.props.smurf !== undefined) ? this.props.smurf.height : '',
+      imageFocused: false,
+      nameFocused: false,
+      ageFocused: false,
+      heightFocused: false
     }
     this.toggle = this.toggle.bind(this);
   };
@@ -20,6 +26,13 @@ class AddEditModal extends Component {
   };
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
+  };
+  setFocus = event => {
+    this.removeFocus();
+    this.setState({ [event.target.dataset.focus]: true });
+  };
+  removeFocus = () => {
+    this.setState({ imageFocused: false, nameFocused: false, ageFocused: false, heightFocused: false });
   };
   primaryAction = () => {
     if (this.props.smurf !== undefined) {
@@ -48,41 +61,66 @@ class AddEditModal extends Component {
           {(this.props.modalBtn.hasIcon) ? <i className={`icon ${this.props.modalBtn.icon}`}></i> : null}
           <span>{this.props.modalBtn.text}</span>
         </button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle}>
-          <ModalHeader toggle={this.toggle}>{this.props.modalTitle}</ModalHeader>
-          <ModalBody>
-            <input
-              name="image"
-              type="text"
-              placeholder="Image"
-              value={this.state.image}
-              onChange={this.handleChange}
-            />
-            <input
-              name="name"
-              type="text"
-              placeholder="Name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <input
-              name="age"
-              type="number"
-              placeholder="Age"
-              value={this.state.age}
-              onChange={this.handleChange}
-            />
-            <input
-              name="height"
-              type="height"
-              placeholder="height"
-              value={this.state.height}
-              onChange={this.handleChange}
-            />
+        <Modal className="test" isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader className="add-edit-header">{this.props.modalTitle}</ModalHeader>
+          <ModalBody className="add-edit-fields">
+            <div className="input-container">
+              <input
+                name="image"
+                className="image"
+                type="text"
+                data-focus="imageFocused"
+                value={this.state.image}
+                onChange={this.handleChange}
+                onFocus={this.setFocus}
+                onBlur={this.removeFocus}
+              />
+              <label className={(this.state.imageFocused || this.state.image !== '') ? 'active' : null}>Image</label>
+            </div>
+            <div className="input-container">
+              <input
+                name="name"
+                type="text"
+                data-focus="nameFocused"
+                value={this.state.name}
+                onChange={this.handleChange}
+                onFocus={this.setFocus}
+                onBlur={this.removeFocus}
+              />
+              <label className={(this.state.nameFocused || this.state.name !== '') ? 'active' : null}>Name</label>
+            </div>
+            <div className="input-container">
+              <input
+                name="age"
+                type="number"
+                data-focus="ageFocused"
+                value={this.state.age}
+                onChange={this.handleChange}
+                onFocus={this.setFocus}
+                onBlur={this.removeFocus}
+              />
+              <label className={(this.state.ageFocused || this.state.age !== '') ? 'active' : null}>Age</label>
+            </div>
+            <div className="input-container">
+              <input
+                name="height"
+                type="height"
+                data-focus="heightFocused"
+                value={this.state.height}
+                onChange={this.handleChange}
+                onFocus={this.setFocus}
+                onBlur={this.removeFocus}
+              />
+              <label className={(this.state.heightFocused || this.state.height !== '') ? 'active' : null}>Height</label>
+            </div>
           </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.primaryAction}>{this.props.actionTitle}</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          <ModalFooter className="add-edit-actions">
+            <button className="primary" onClick={this.primaryAction}>
+              <span>{this.props.actionTitle}</span>
+            </button>
+            <button className="secondary" onClick={this.toggle}>
+              <span>Cancel</span>
+            </button>
           </ModalFooter>
         </Modal>
       </React.Fragment>
