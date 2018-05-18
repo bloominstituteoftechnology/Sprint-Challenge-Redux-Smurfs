69cde5db70051d18dc44915db25c78c79193ca5c
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-05-18 23:38:36 +0800

    Started working on /src/index.js.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+
+import './index.css';
+import App from './components/App';
+import MainReducer from './reducers';
+
+const store = createStore(MainReducer, applyMiddleware(thunk, logger));
+//   () => {}, // this is the most basic reducer. A function that returns an object. Replace it.
+//   applyMiddleware(/* be sure to throw in the proper middlewares here*/)
+// );
 
 ReactDOM.render(
   <Provider store={store}>
