b43e2fbeb749cd7b4919ef4e624522e3c90c89b0
Thuy Pham <tpham045@hotmail.com>
2018-05-18 08:50:10 -0700

    feat: get all smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,48 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const FETCHED_SMURFS = 'FETCHED_SMURFS'
+export const FETCHING_SMURFS = 'FETCHING_SMURFS'
+export const ADDING_SMURF = 'ADDING_SMURF'
+export const UPDATING_SMURF = 'UPDATING_SMURF'
+export const DELETING_SMURF = 'DELETING_SMURF'
+export const ERROR = 'ERROR'
+
+export const getSmurfs = () => {
+  return (dispatch) => {
+    const request = axios.get('http://localhost:3333/smurfs')
+    dispatch({ type: FETCHING_SMURFS})
+    request
+    .then(({data}) => dispatch({ type: FETCHED_SMURFS, smurfs:data}))
+    .catch(error => dispatch({ type: ERROR, error}))
+  }
+}
+
+export const addSmurf = () => {
+  return (dispatch) => {
+    const request = axios.get('http://localhost:3333/smurfs')
+    dispatch({ type: FETCHING_SMURFS})
+    request
+    .then(({data}) => dispatch({ type: ADDING_SMURF, smurfs:data}))
+    .catch(error => dispatch({ type: ERROR, error}))
+  }
+}
+
+export const deleteSmurf = () => {
+  return (dispatch) => {
+    const request = axios.get('http://localhost:3333/smurfs')
+    dispatch({ type: FETCHING_SMURFS})
+    request
+    .then(({data}) => dispatch({ type: DELETING_SMURF, smurfs:data}))
+    .catch(error => dispatch({ type: ERROR, error}))
+  }
+}
+
+export const updateSmurf = () => {
+  return (dispatch) => {
+    const request = axios.get('http://localhost:3333/smurfs')
+    dispatch({ type: FETCHING_SMURFS})
+    request
+    .then(({data}) => dispatch({ type: UPDATING_SMURF, smurfs:data}))
+    .catch(error => dispatch({ type: ERROR, error}))
+  }
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,58 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  FETCHING_SMURFS,
+  ADDING_SMURF,
+  UPDATING_SMURF,
+  DELETING_SMURF,
+  FETCHED_SMURFS,
+  ERROR
+} from '../actions'
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+ const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+export const smurfReducer = (state=initialState, action) => {
+  switch (action.type) {
+    case FETCHED_SMURFS: 
+      return {
+        smurfs: action.smurfs,
+        fetchingSmurfs: false,
+        addingSmurf: false,
+        updatingSmurf: false,
+        deletingSmurf: false,
+        error: null
+      }
+    case FETCHING_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      }
+    case ADDING_SMURF:
+      return {
+        ...state,
+        addingSmurfs: true
+      }
+    case UPDATING_SMURF:
+      return {
+        ...state,
+        updatingSmurfs: true
+      }
+    case DELETING_SMURF:
+      return {
+        ...state,
+        deletingSmurfs: true
+      }
+    case ERROR:
+      return {
+        ...state,
+        error: action.error
+      }
+    default:
+      return state
+  }
+}
