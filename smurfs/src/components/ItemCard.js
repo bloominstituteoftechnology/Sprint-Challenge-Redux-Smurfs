89710272ef1e363e55ce26e991ed1bff6b1d8ddd
jesuarva <jean.ariza@jesuarva.com>
2018-05-18 19:48:25 +0200

    ADD id to smurf in the Server.

---
+++ smurfs/src/components/ItemCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ItemCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ItemCard;
