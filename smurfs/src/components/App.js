8ba3ca4350ae8089cac27b6a9e23abf599ddcea5
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-18 12:26:32 -0400

    -imported connected and mapStateToProps

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,6 +9,9 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount(){
+    this.props
+  }
   render() {
     return (
       <div className="App">
@@ -19,4 +24,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    prop: state.prop
+  }
+}
+
+export default connect(mapStateToProps) (App);
