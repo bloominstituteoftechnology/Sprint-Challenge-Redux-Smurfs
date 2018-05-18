508627ec07e3599955d30063480eea8f7bd08e7a
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-18 13:06:08 -0400

    Finished describing actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -12,17 +12,84 @@ import {
 } from './ActionTypes';
 
 export const getSmurfs = () => {
-
+  const getAllSmurfs = axios.get('http://localhost:3333/smurfs');
+  return (dispatch) => {
+    dispatch({ type: FETCHING_SMURFS });
+    getAllSmurfs
+      .then(response => {
+        dispatch({
+          type: SMURFS_FETCHED,
+          payload: response.data
+        });}
+      )
+      .catch(err => {
+        dispatch({
+          type: ERROR,
+          payload: err
+        });
+      });
+  };
 };
 
-export const addSmurf = () => {
-
+export const addSmurf = smurfObj => {
+  const addNewSmurf = axios.post('http://localhost:3333/smurfs', smurfObj);
+  return (dispatch) => {
+    dispatch({ type: ADDING_SMURF });
+    addNewSmurf
+      .then(response => {
+        dispatch({
+          type: SMURF_ADDED,
+          payload: response.data
+        });
+      }
+      )
+      .catch(err => {
+        dispatch({
+          type: ERROR,
+          payload: err
+        });
+      });
+  };
 };
 
-export const updateSmurf = () => {
-
+export const updateSmurf = (smurfId, smurfObj) => {
+  const modifySmurf = axios.put(`http://localhost:3333/smurfs${smurfId}`, smurfObj);
+  return (dispatch) => {
+    dispatch({ type: UPDATING_SMURF });
+    modifySmurf
+      .then(response => {
+        dispatch({
+          type: SMURF_UPDATED,
+          payload: response.data
+        });
+      }
+      )
+      .catch(err => {
+        dispatch({
+          type: ERROR,
+          payload: err
+        });
+      });
+  };
 };
 
-export const deleteSmurf = () => {
-
+export const deleteSmurf = smurfId => {
+  const removeSmurf = axios.delete(`http://localhost:3333/smurfs${smurfId}`);
+  return (dispatch) => {
+    dispatch({ type: DELETING_SMURF });
+    removeSmurf
+      .then(response => {
+        dispatch({
+          type: SMURF_DELETED,
+          payload: response.data
+        });
+      }
+      )
+      .catch(err => {
+        dispatch({
+          type: ERROR,
+          payload: err
+        });
+      });
+  };
 };
