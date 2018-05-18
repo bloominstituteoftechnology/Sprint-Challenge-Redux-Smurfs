e6287770f28e8a78387e0a929da3b9f95d351229
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-18 08:28:18 -0700

    smurfReducer added and serviced through middleware

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -6,7 +6,24 @@ const initialState = {
   smurfs: []
 };
 
-
+export const smurfsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case PENDING_SMURFS:
+      return Object.assign({}, state, { pending: true });
+    case SUCCESS_SMURFS:
+      return Object.assign({}, state, {
+        pending: false,
+        smurfs: action.payload
+      });
+    case ERROR_SMURFS:
+      return Object.assign({}, state, {
+        pending: false,
+        error: action.payload
+      });
+    default:
+      return state;
+  }
+};
 
 
 
