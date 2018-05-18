eb54cfe1b1567e1bcaa1327f555c322ddc530abe
udykas <udykas@gmail.com>
2018-05-18 15:07:58 -0400

    Added an image

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -9,6 +9,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome to Smurf Village!</h1>
+        <img src="http://www.wallsticker.top/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/t/h/the_smurfs3532_1.jpg" 
+        className="smurf-image"
+        alt='A Smurf'
+        />
         <CreateSmurf />
         <Smurfs />
       </div>
