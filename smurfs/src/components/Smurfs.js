bde36c12be0f5b02780ef5dc0f6302c4574e4f2d
udykas <udykas@gmail.com>
2018-05-18 12:33:33 -0400

    Smurfs component is complete, and showing smurf data on screen

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
 
-import { conect } from 'react-redux';
+import { connect } from 'react-redux';
+import { fetchSmurfs } from '../actions/index';
 
 
 class Smurfs extends Component {
 
     componentDidMount() {
-        
+        this.props.fetchSmurfs();
     }
 
     render() { 
         return ( 
-            <div className='smurf-card'>
-                <h2>{}</h2>
-                <h4>{}</h4>
-                <h4>{}</h4>
+            <div className="smurf-container">
+                {this.props.fetchingSmurfs ? (
+                    <span>Fetching Smurfs</span>
+                ) : (
+                    <ul>
+                        {this.props.smurfs.map(smurf => {
+                            return(
+                                <div key={smurf.name + smurf.age} className="smurf-card">
+                                    <h2>{smurf.name}</h2>
+                                    <h4>{smurf.age}</h4>
+                                    <h4>{smurf.height}</h4>
+                                </div>
+                            ) 
+                        })}
+                    </ul>
+                )}
             </div>
          )
     }
 }
+
+const mapStateToProps = state => {
+    console.log("STATE:", state)
+    return {
+        smurfs: state.smurfs,
+        fetchingSmurfs: state.fetchingSmurfs
+    }
+}
  
-export default Smurfs;
+export default connect(mapStateToProps, { fetchSmurfs }) (Smurfs);
