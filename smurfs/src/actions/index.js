e12bc7942e84a4f2e507499cd6a94451ae713000
emortalix <emortalix@welcometoforever.io>
2018-05-18 12:00:18 -0400

    refactoring actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,49 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios'
+import {
+  FETCHING,
+  CREATING,
+  UPDATING,
+  DELETING,
+  SUCCESS,
+  ERROR,
+  EDITING,
+  CANCEL_EDIT,
+  INPUT_CHANGE,
+} from './types'
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+const ROOT_URL = 'http://localhost:3333'
+const sucessWithPayload = data => ({ type: SUCCESS, payload: data })
+const sendErrorMessage = error => ({ type: ERROR, error: error })
+export const inputChange = change => ({ type: INPUT_CHANGE, payload: change })
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING })
+  axios.get(`${ROOT_URL}/smurfs`)
+    .then(({ data }) => dispatch(sucessWithPayload(data)))
+    .catch(error => dispatch(sendErrorMessage(error)))
+}
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: CREATING })
+  axios.post(`${ROOT_URL}/smurfs`, smurf)
+    .then(({ data }) => dispatch(sucessWithPayload(data)))
+    .catch(error => dispatch(sendErrorMessage(error)))
+}
+
+export const editSmurf = id => ({ type: EDITING, payload: id })
+export const cancelEdit = () => ({ type: CANCEL_EDIT })
+
+export const updateSmurf = smurf => dispatch => {
+  const { id, name, age, height } = smurf
+  dispatch({ type: UPDATING })
+  axios.put(`${ROOT_URL}/smurfs/${id}`, { name, age, height })
+    .then(_ => dispatch(getSmurfs()))
+    .catch(error => dispatch(sendErrorMessage(error)))
+}
+
+export const deleteSmurf = id => dispatch => {
+  dispatch({ type: DELETING })
+  axios.delete(`${ROOT_URL}/smurfs/${id}`)
+    .then(_ => dispatch(getSmurfs()))
+    .catch(error => dispatch(sendErrorMessage(error)))
+}
