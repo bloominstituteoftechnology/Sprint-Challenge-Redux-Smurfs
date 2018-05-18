8a1efcf709c3b3d7439cc0cc4a3821be81706e90
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-05-18 11:36:48 -0500

    Smurff name mounted to browser

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -17,8 +17,10 @@ class App extends Component {
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        {this.props.smurfs.map(smurfs => {
+          return <div key={smurfs}>{smurfs.name}</div>;
+        })}
       </div>
     );
   }
