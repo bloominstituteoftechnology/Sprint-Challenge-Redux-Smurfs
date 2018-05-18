2069b5395885d6ca324f465da84ceaa0360d88f3
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-18 10:44:00 -0500

    created SmurfsList, passed smurfs down as props, & added boilerplate

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,14 +3,9 @@ import { connect } from 'react-redux';
 
 
 import './App.css';
+import SmurfsList from './SmurfsList';
 
 
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
 class App extends Component {
   render() {
     return (
@@ -19,6 +14,7 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfsList smurfs={this.props.smurfs} />
       </div>
     );
   }
