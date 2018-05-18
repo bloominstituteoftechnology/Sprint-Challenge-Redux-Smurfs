9f43b2117f76aaec5f21552c1efe5bccc9ac336f
kammagik <kammagik@yahoo.com>
2018-05-18 09:18:09 -0700

    initial commit, added actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,42 @@
    U - updateSmurf
    D - deleteSmurf
 */
+import axios from 'axios';
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const SMURFS_FETCHED = 'SMURFS_FETCHED';
+export const ERROR = 'ERROR';
+export const CREATING_SMURF = 'CREATING_SMURF';
+export const SMURF_CREATED = 'SMURF_CREATED';
+
+export const fetchSmurfs = () => {
+  const getSmurfs = axios.get('http://localhost:3333/smurfs');
+  return dispatch {
+    dispatch({ type: FETCHING_SMURFS });
+    getSmurfs
+      .then(response => {
+        console.log(response);
+        dispatch({ type: SMURFS_FETCHED, smurfs:data [] });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
+
+export const addSmurf = smurf => {
+  const createSmurf = axios.post(
+    `http://localhost:3333/smurfs/${smurf.id}`,
+    { newSmurf: smurf.name }
+  );
+  return function(dispatch) {
+    dispatch({ type: CREATING_SMURF });
+    createSmurf
+      .then(response => {
+        dispatch({ type: SMURF_CREATED, smurfs:data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
+
