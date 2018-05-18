c3fd2074225d5b878276df2599cdfc9d025b4a33
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-18 15:14:14 -0400

    Styled header

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header/Header';
 import SmurfList from './SmurfList/SmurfList';
-import './App.css';
+import smurfLogo from '../assets/the-smurfs-logo.svg.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title="Welcome to Smurf Village"/>
+        <Header title="Welcome to Smurf Village" image={smurfLogo} />
         <SmurfList />
       </div>
     );
--- smurfs/src/components/Header/Header.js
+++ smurfs/src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AddSmurfButton from '../AddSmurfButton/AddSmurfButton';
+import './Header.css';
 
 const Header = props => {
   return (
     <header className="header">
-      <h1>{props.title}</h1>
+      <h1>
+        {(props.image !== null) ? <img src={props.image} className="img-fluid" alt={props.title}/> : props.title}
+      </h1>
       <AddSmurfButton />
     </header>
   );
