ceaafe7e4a66be3994ca51922e879e74bd64b1e2
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-18 14:02:43 -0400

    Built Smurf Card component

---
+++ smurfs/src/components/SmurfCard/SmurfCard.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Card, CardBody, CardTitle } from 'reactstrap';
+import AddEditModal from '../AddEditModal/AddEditModal';
+import { updateSmurf, deleteSmurf } from '../../actions';
+
+class SmurfCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      btnProps: {
+        class: 'edit',
+        text: 'Edit',
+        hasIcon: 'false',
+        icon: ''
+      }
+    }
+  }
+  updateThisSmurf = (smurfObj, id) => {
+    if (smurfObj) {
+      this.props.updateSmurf(id, smurfObj);
+    }
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <Card className="smurf" data-type="card">
+          <CardBody>
+            <button className="remove" onClick={() => { this.props.removesmurf(this.props.smurf.id) }}>
+              <i className="fas fa-trash-alt"></i>
+            </button>
+            <CardTitle className="smurf-name">{this.props.smurf.name}</CardTitle>
+            <ul className="smurf-stats">
+              <li>
+                <p>{this.props.smurf.age} years old</p>
+              </li>
+              <li>
+                <p>{this.props.smurf.height} tall</p>
+              </li>
+            </ul>
+            <smurfModal
+              smurf={this.props.smurf}
+              modalBtn={this.state.btnProps}
+              modalTitle="Update smurf"
+              actionTitle="Update"
+              action={this.updateThisSmurf}
+            />
+          </CardBody>
+        </Card>
+      </React.Fragment>
+    );
+  }
+}
+
+export default connect(null, { updateSmurf, deleteSmurf })(SmurfCard);
