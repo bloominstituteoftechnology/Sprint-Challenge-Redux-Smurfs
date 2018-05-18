4d1576a7c12cf031f34c6331b5a055014eb842ac
Zach Campbell <beazlebubbles@gmail.com>
2018-05-18 12:19:27 -0600

    Installed dev tools

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import smurfReducer from './reducers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(
-  smurfReducer,
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const quatloo = createStore(smurfReducer, composeEnhancers(
   applyMiddleware(thunk, logger)
-);
+));
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={quatloo}>
     <App />
   </Provider>,
   document.getElementById('root')
