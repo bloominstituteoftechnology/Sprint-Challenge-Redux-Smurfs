2fbc184174e238bb9766858a3d2e2a8a198e14b6
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-18 11:22:57 -0500

    added reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,60 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+import axios from 'axios';
+import { Agent } from 'https';
+
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const SMURFS_FETCHED = 'SMURFS_FETCHED';
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const SMURF_ADDED = 'SMURF_ADDED';
+// export const UPDATING_SMURF = 'UPDATING_SMURF';
+// export const SMURF_UPDATED = 'SMURF_UPDATED';
+// export const DELETING_SMURF = 'DELETING_SMURF';
+// export const SMURF_DELETED = 'SMURF_DELETED';
+export const ERROR = 'ERROR';
+
+
+
+export const fetchSmurfs = () => {
+  const getSmurfs = axios.get('http://localhost:3333/smurfs')
+    return function(dispatch) {
+      dispatch({ type: FETCHING_SMURFS })
+      getSmurfs
+      .then(res => {
+        dispatch({
+          type: SMURFS_FETCHED,
+          payload: res.data
+        })
+      })
+      .catch(error => {
+        dispatch({
+          type: ERROR,
+          payload: error
+        })
+      })
+    }
+}
+
+export const addSmurf = (smurfName, smurfAge, smurfHeight) => {
+  const add = axios.post(`http://localhost:3333/smurfs`, {
+    name: smurfName,
+    age: smufAge, 
+    height: smurfHeight
+  })
+
+  return function(dispatch) {
+    dispatch({ ADDING_SMURF })
+    add 
+    .then(res => {
+      dispatch({
+        type: SMURF_ADDED,
+        payload: res.data
+      })
+    })
+    .catch(error => {
+      dispatch({
+        type: ERROR,
+        payload: error
+      })
+    })
+  }
+}
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -10,7 +10,7 @@ import /* You need some sort of reducer */ './reducers';
 
 const store = createStore(
   () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
