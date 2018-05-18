682a8ea72c16caf530ea1c9e55239774d33aef70
Chris Honda <christopher.honda92@gmail.com>
2018-05-18 11:31:56 -0700

    Components, actions, reducers in place, unresolved error

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,29 +2,47 @@ import * as actionTypes from "../actions";
 
 const initialState = {
   smurfs: [],
-  fetchingSmurfs: false,
-  addingSmurf: false,
+  gettingSmurfs: false,
+  creatingSmurf: false,
   updatingSmurf: false,
   deletingSmurf: false,
+  smurfSelected: {},
+  showUpdate: false,
   error: null
-}
+};
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
-
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.SINGLE_SMURF:
+      return { ...state, smurfSelected: action.payload, showUpdate: false };
+    case actionTypes.TOGGLE_UPDATE_FRIEND:
+      return { ...state, showUpdate: !state.showUpdate };
+    case actionTypes.GETTING_SMURFS:
+      return { ...state, gettingSmurfs: true };
+    case actionTypes.GET_SMURFS:
+      return { ...state, smurfs: action.payload, gettingSmurfs: false};
+    case actionTypes.UPDATING_SMURF:
+      return { ...state, updatingSmurf: true };
+    case actionTypes.UPDATE_SMURF:
+      return { ...state, smurfs: action.payload, updatingSmurf: false };
+    case actionTypes.DELETING_SMURF:
+      return { ...state, deletingSmurf: true };
+    case actionTypes.DELETE_SMURF:
+      return { ...state, smurfs: action.payload, deletingSmurf: false };
+    case actionTypes.CREATING_SMURF:
+      return { ...state, creatingSmurf: true };
+    case actionTypes.CREATE_SMURF:
+      return { ...state, smurfs: action.payload, creatingSmurf: false };
+    case actionTypes.ERROR:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        creatingsmurf: false,
+        deletingSmurf: false,
+        updatingSmurf: false,
+        error: action.payload
+      };
+    default: 
+      return state;
+  }
+};
