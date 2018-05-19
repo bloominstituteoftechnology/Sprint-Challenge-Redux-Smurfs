202a1fc5676324276a6ab4e3c84190b20281935d
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-05-18 23:17:22 -0400

    Added button control to prevent smurf cards from being updated, changed, or added at the same time

--- smurfs/src/components/SmurfsForm.js
+++ smurfs/src/components/SmurfsForm.js
@@ -23,13 +23,11 @@ class SmurfsForm extends Component {
     }
 
     createSmurf = () => {
-        console.log('This Props Length', this.props.smurfs.length + 1);
         this.props.addSmurf({ name: this.state.name, age: Number(this.state.age), height: this.state.height });
         this.setState({ name: '', age: 0, height: '' });
     }
 
     render() {
-        console.log('This Props Length Render', this.props.smurfs.length)
         return (
             <div className='form'>
                 <h3>{this.state.title}</h3>
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -12,8 +12,9 @@ class SmurfsList extends Component {
             name: '',
             age: '',
             height: '',
-            showForm: false,
         }
+        this.showForm = false;
+        this.click = false;
     }
 
     handleOnChange = (event) => {
@@ -26,12 +27,30 @@ class SmurfsList extends Component {
     }
 
     handleOnClick = (event) => {
-        this.state.showForm = !this.state.showForm;
+
+        const button = document.getElementsByTagName('button');
+
+        this.click = !this.click;
+
+        if (this.click) {
+            for (let i = 0; i < button.length; i++) {
+                if (button[i].getAttribute('id') !== `deleteButton${event.target.name}` && button[i].getAttribute('id') !== `changeButton${event.target.name}` && button[i].getAttribute('id') !== `updateButton${event.target.name}` && button[i].getAttribute('id') !== `cancelButton${event.target.name}`) {
+                    button[i].disabled = true;
+                }
+            }
+        }
+        else {
+            for (let i = 0; i < button.length; i++) {
+                button[i].disabled = false;
+            }
+        }
+
+        this.showForm = !this.showForm;
 
         const divone = document.getElementById(`${event.target.name}`);
-        const divtwo = document.getElementById(`${event.target.name + 'smurf'}`);
+        const divtwo = document.getElementById(`${'smurf' + event.target.name}`);
 
-        if (this.state.showForm === true) {
+        if (this.showForm === true) {
             divone.style.display = "none";
             divtwo.style.display = "initial"
         }
@@ -41,16 +60,26 @@ class SmurfsList extends Component {
         }
     }
 
+
     updateOnClick = (event) => {
+
+        const button = document.getElementsByTagName('button');
+
+        this.click = !this.click;
+
+        for (let i = 0; i < button.length; i++) {
+            button[i].disabled = false;
+        }
+
         this.props.updateSmurf(event.target.name, { name: this.state.name, age: Number(this.state.age), height: this.state.height, id: Number(event.target.name) });
         this.setState({ name: '', age: '', height: '' });
 
-        this.state.showForm = !this.state.showForm;
+        this.showForm = !this.showForm;
 
         const divone = document.getElementById(`${event.target.name}`);
-        const divtwo = document.getElementById(`${event.target.name + 'smurf'}`);
+        const divtwo = document.getElementById(`${'smurf' + event.target.name}`);
 
-        if (this.state.showForm === true) {
+        if (this.showForm === true) {
             divone.style.display = "none";
             divtwo.style.display = "initial"
         }
@@ -73,18 +102,18 @@ class SmurfsList extends Component {
                                             <CardText>Smurf Name: {smurf.name}</CardText>
                                             <CardText>Smurf Age: {smurf.age}</CardText>
                                             <CardText>Smurf Height: {smurf.height}</CardText>
-                                            <button onClick={() => this.props.deleteSmurf(smurf.id)}>Delete Smurf</button><br /><br /> 
-                                            <button name={smurf.id} onClick={this.handleOnClick}>Change Smurf Information</button><br />
+                                            <button id={'deleteButton' + smurf.id} onClick={() => this.props.deleteSmurf(smurf.id)}>Delete Smurf</button><br /><br />
+                                            <button id={'changeButton' + smurf.id} name={smurf.id} onClick={this.handleOnClick}>Change Smurf Information</button><br />
                                         </CardBody>
                                     </Card>
                                 </Col>
                             </Row>
-                            <div id={smurf.id + 'smurf'} style={{ display: 'none' }}>
+                            <div id={'smurf' + smurf.id} style={{ display: 'none' }}>
                                 <input type="text" name="name" value={this.state.name} placeholder={smurf.name} onChange={this.handleOnChange} /><br />
                                 <input type="type" name="age" value={this.state.age} placeholder={smurf.age} onChange={this.handleOnChange} /><br />
                                 <input type="height" name="height" value={this.state.height} placeholder={smurf.height} onChange={this.handleOnChange} /><br />
-                                <button name={smurf.id} onClick={this.updateOnClick}>Update Smurf Information</button>
-                                <button name={smurf.id} onClick={this.updateOnClick}>Cancel</button>
+                                <button id={'updateButton' + smurf.id} name={smurf.id} onClick={this.updateOnClick}>Update Smurf Information</button>
+                                <button id={'cancelButton' + smurf.id} name={smurf.id} onClick={this.handleOnClick}>Cancel</button>
                             </div>
                         </div>
                     )
