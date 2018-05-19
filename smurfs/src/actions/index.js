1f989a2087eaae693b3a59fb7b6197405c774018
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-05-19 00:20:34 -0400

    Actions and Reducers Created

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,134 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+
+export const SAVING = 'SAVING';
+export const SAVED = 'SAVED';
+
+export const UPDATING = 'UPDATING';
+export const UPDATED = 'UPDATED';
+
+export const DELETING = 'DELETING';
+export const DELETED = 'DELETED';
+
+export const ERROR = 'ERROR';
+
+const fetching = () => {
+    return {
+        type: FETCHING
+    }
+}
+
+const fetched = (data) => {
+    return {
+        type: FETCHED,
+        payload: data
+    }
+}
+
+const saving = () => {
+    return {
+        type: SAVING
+    }
+}
+
+const saved = (data) => {
+    return {
+        type: SAVED,
+        payload: data
+    }
+}
+
+const updating = () => {
+    return {
+        type: UPDATING
+    }
+}
+
+const updated = () => {
+    return {
+        type: UPDATED
+    }
+}
+
+const deleting = () => {
+    return {
+        type: DELETING
+    }
+}
+
+const deleted = () => {
+    return {
+        type: DELETED
+    }
+}
+
+const error = (err) => {
+    return {
+        type: ERROR,
+        payload: err
+    }
+}
+
+export const getSmurfs = () => {
+    const promise = axios.get('http://localhost:3333/smurfs');
+    console.log("fetching smurfs... ")
+    return function(dispatch) {
+        dispatch(fetching());
+        promise
+            .then( res => {
+                console.log("result of get request: ", res.data)
+                dispatch(fetched(res.data));
+            })
+            .catch( err => {
+                dispatch(error(err));
+            })
+    }
+}
+
+export const addSmurf = (smurf) => {
+    const promise = axios.post('http://localhost:3333/smurfs', smurf);
+    return function(dispatch) {
+        dispatch(saving());
+        promise
+            .then( res => {
+                dispatch(saved(res.data))
+            })
+            .catch( err => {
+                dispatch(error(err));
+            })
+    }
+}
+
+export const updateSmurf = (smurf) => {
+    const promise = axios.put(`http://localhost:3333/smurfs${smurf.id}`, smurf)
+    return function(dispatch) {
+        dispatch(updating())
+        promise
+            .then( res  => {
+                dispatch(updated(res));
+            })
+            .catch( err => {
+                dispatch(error(err));
+            })
+    }
+}
+
+export const deleteSmurf = (smurf) => {
+    const promise = axios.delete(`http://localhost:3333/smurfs${smurf.id}`)
+    return function(dispatch) {
+        dispatch(deleting())
+        promise 
+            .then( res => {
+                dispatch(deleted(res));
+            })
+            .catch( err => {
+                dispatch(error(err));
+            })
+    }
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { FETCHED, FETCHING, SAVING, SAVED, UPDATING, UPDATED, DELETING, DELETED, ERROR } from '../actions';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -14,6 +15,24 @@
  }
 */
 
+const initialState = {
+  smurfs: [],
+
+  fetching: false,
+  fetched: false,
+
+  saving: false,
+  saved: false,
+
+  updating: false,
+  updated: false,
+
+  deleting: false,
+  deleted: false,
+
+  error: null
+}
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +40,60 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+
+export const reducer = (state=initialState, action) => {
+    switch (action.type) {
+        case FETCHING:
+            return Object.assign({}, state, {
+                fetching: true,
+                fetched: false
+            })
+        case FETCHED:
+            return Object.assign({}, state, {
+                fetched: true, 
+                fetching: false, 
+                smurfs: action.payload
+            })
+        case SAVING:
+            return Object.assign({}, state, {
+                saving: true,
+                saved: false
+            })
+        case SAVED: 
+            return Object.assign({}, state, {
+                saving: false,
+                saved: true,
+                smurfs: state.smurfs.concat(action.payload)
+            })
+        case UPDATING:
+            return Object.assign({}, state, {
+                updating: true,
+                updated: false
+            })
+        case UPDATED:
+            return Object.assign({}, state, {
+                updated: true,
+                updating: false
+            })
+        case DELETING:
+            return Object.assign({}, state, {
+                deleted: false,
+                deleting: true
+            })
+        case DELETED:
+            return Object.assign({}, state, {
+                deleted: true,
+                deleting: false
+            })
+        case ERROR:
+            return Object.assign({}, state, {
+                fetching: false,
+                editing: false,
+                deleting: false,
+                error: action.payload
+            })
+        default:
+            return state;
+    }
+}
