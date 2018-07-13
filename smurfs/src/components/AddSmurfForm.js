c38cf9e7630a8fb09f07d0a4886c084d6b7c66f2
DirupT <iamcooled@gmail.com>
2018-07-13 10:46:19 -0500

    imported / added getSmurf to AddSmurfForm

--- smurfs/src/components/AddSmurfForm.js
+++ smurfs/src/components/AddSmurfForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addSmurf } from '../actions';
 
 class AddSmurfForm extends React.Component {
     constructor() {
@@ -15,16 +17,32 @@ class AddSmurfForm extends React.Component {
         this.setState({ [event.target.name]: event.target.value });
     }
 
+    addSmurf = () => {
+        const { name, height, age } = this.state;
+        const smurf = { name, height, age };
+
+        if (name === '' || height === '' || age === '') return;
+
+        this.props.addSmurf(smurf);
+        this.setState({ name: '', height: '', age: '' });
+    }
+
     render() {
         return (
             <form onSubmit={event => event.preventDefault()}>
                 <input onChange={this.handleInput} value={this.state.name} type='text' name='name' placeholder='name' />
                 <input onChange={this.handleInput} value={this.state.height} type='text' name='height' placeholder='height' />
                 <input onChange={this.handleInput} value={this.state.age} type='text' name='age' placeholder='age' />
-                <button>Add Smurf</button>
+                <button onClick={this.addSmurf}>Add Smurf</button>
             </form>
         );
     }
 }
 
-export default AddSmurfForm;
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs
+    }
+}
+
+export default connect(mapStateToProps, { addSmurf })(AddSmurfForm);
