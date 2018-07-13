71f154d05a5b169c2eb73bcc9a39ab9def97015b
Vu Cao <cdhvu@yahoo.com>
2018-07-13 09:09:12 -0700

    Completed Welcome Page

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import { Route } from 'react-router-dom';
+
+import Header from './Header';
+import styled from 'styled-components';
+import background from '../images/background.jpg';
+
+const APP = styled.div`
+  height: 100vh;
+  background: url(${background});
+  background-size: cover;
+  text-align: center;
+  color: white;
+  text-shadow: 2px 2px 5px #0a1f42;
+  font-family: "Comic Sans MS", "Comic Sans", cursive;
+  text-shadow: 3px 3px 5px #0a1f42;
+`
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -9,12 +26,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
+      <APP>
+        <Route exact path="/" component={Header} />
+        {/* <Route exact path="/smurfs" render={props => <Smurfs {...props} smurfs={this.state.smurfs}/>} />
+        <Route exact path="/smurfs" component={SmurfForm} />
+        <Route path="/smurfs/:id" render ={props => <Smurf {...props} updateState={this.updateState}/>} /> */}
+      </APP>
     );
   }
 }
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -7,6 +7,7 @@ import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 const store = createStore(
   rootReducer,
@@ -14,8 +15,10 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router>
+  	<Provider store={store}>
+  	  <App />
+  	</Provider>
+  </Router>,
   document.getElementById('root')
 );
