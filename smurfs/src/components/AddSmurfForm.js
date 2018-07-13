aa7eeebaf52dde9912d982621947c69847221209
DirupT <iamcooled@gmail.com>
2018-07-13 10:41:24 -0500

    added form/state/handleInput function to AddSmurfForm

--- smurfs/src/components/AddSmurfForm.js
+++ smurfs/src/components/AddSmurfForm.js
@@ -3,13 +3,25 @@ import React from 'react';
 class AddSmurfForm extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            name: '',
+            height: '',
+            age: ''
+        }
+    }
+
+    handleInput = event => {
+        this.setState({ [event.target.name]: event.target.value });
     }
 
     render() {
         return (
-            <div>
-                Smurfform
-            </div>
+            <form onSubmit={event => event.preventDefault()}>
+                <input onChange={this.handleInput} value={this.state.name} type='text' name='name' placeholder='name' />
+                <input onChange={this.handleInput} value={this.state.height} type='text' name='height' placeholder='height' />
+                <input onChange={this.handleInput} value={this.state.age} type='text' name='age' placeholder='age' />
+            </form>
         );
     }
 }
