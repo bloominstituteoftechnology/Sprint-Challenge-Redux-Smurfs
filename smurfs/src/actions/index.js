66b011c32983d1b15d9c7e779109ee36a8da4c58
Libby Hart <libbjo@gmail.com>
2018-07-13 10:30:42 -0600

    reducers and actions set up

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,77 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCHED_SMURFS = 'FETCHED_SMURFS';
+
+
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADDED_SMURF = 'ADDED_SMURF';
+
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const UPDATED_SMURF = 'UPDATED_SMURF';
+
+export const DELETING_SMURF = 'DELETING_SMURF';
+export const DELETED_SMURF = 'DELETED_SMURF';
+
+
+export const ERROR = 'ERROR';
+
+
+export const fetchSmurfs = () => {
+  const smurfs = axios.get(`http://localhost:3333/smurfs`);
+  return dispatch => {
+    dispatch({ type: FETCHING_SMURFS });
+    smurfs
+    .then(response => {
+    dispatch({ type: FETCHED_SMURFS, payload: response.data })
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err });
+    });
+  };
+};
+
+export const addSmurf = smurf => {
+  const newSmurf = axios.post(`http://localhost:3333/smurfs`, smurf);
+  return dispatch => {
+    dispatch({ type: ADDING_SMURF });
+    newSmurf
+    .then(response => {
+    dispatch({ type: ADDED_SMURF, payload: response.data })
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err });
+    });
+  };
+};
+
+// export const updateSmurfs = () => {
+//   const smurfs = axios.put(`http://localhost:3333/smurfs`);
+//   return dispatch => {
+//     dispatch({ type: FETCHING_SMURFS });
+//     smurfs
+//     .then(response => {
+//     dispatch({ type: FETCH_SMURFS, payload: response.data })
+//     })
+//     .catch(err => {
+//       dispatch({ type: ERROR, payload: err });
+//     });
+//   };
+// };
+
+// export const deleteSmurfs = () => {
+//   const smurfs = axios.delete(`http://localhost:3333/smurfs`);
+//   return dispatch => {
+//     dispatch({ type: FETCHING_SMURFS });
+//     smurfs
+//     .then(response => {
+//     dispatch({ type: FETCH_SMURFS, payload: response.data })
+//     })
+//     .catch(err => {
+//       dispatch({ type: ERROR, payload: err });
+//     });
+//   };
+// };
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,19 +1,46 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { FETCHING_SMURFS, FETCHED_SMURFS, ADDING_SMURF, ADDED_SMURF, UPDATING_SMURF, UPDATED_SMURF, DELETING_SMURF, DELETED_SMURF, ERROR } from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
- }
-*/
+ };
 
+ export const rootReducer = (state = initialState, action) => {
+   switch (action.type) {
+    case FETCHING_SMURFS:
+      return {...state, fetchingSmurfs: true};
+    case FETCHED_SMURFS:
+      return {...state, smurfs: action.payload, fetchingSmurfs: false};
+    
+    case ADDING_SMURF:
+      return {...state, addingSmurf:true};
+    case ADDED_SMURF:
+      return {...state, addingSmurf:true};
+    
+    case UPDATING_SMURF:
+      return {...state, updatingSmurf: true};
+    case UPDATED_SMURF:
+      return {...state, updatingSmurf: true};
+    
+    case DELETING_SMURF:
+      return {...state, deletingSmurf: true};
+    case DELETED_SMURF:
+      return {...state, deletingSmurf: true};
+    
+    case ERROR:
+      return{...state, error: action.payload};
+    
+    default:
+      return state;
+   }
+ };
+
+ export default rootReducer;
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
