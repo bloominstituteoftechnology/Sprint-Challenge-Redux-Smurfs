3e9a2db578f0c9675e5e30d11bbf41b9c197fb17
Erin Meredith <ekmeredith@att.net>
2018-07-13 14:53:35 -0400

    .

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -4,12 +4,12 @@ import { connect } from 'react-redux';
 import { fetchSmurfs, toggleForm } from './../actions';
 import Smurf from './Smurf';
 import AddSmurf from './AddSmurf';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import Styled from 'styled-components';
+
+const Body = Styled.div`
+  background: lightblue;
+`;
+
 class App extends Component {
 
   componentDidMount() {
@@ -19,7 +19,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <Body className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>
           <AddSmurf />
@@ -29,7 +29,7 @@ class App extends Component {
           })}
           </ul>
         </div>
-      </div>
+      </Body>
     );
   }
 }
