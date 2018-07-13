a9ed75a3e3f1ba14b28f63ae2449f9af2d9cbc31
chainchompa <chainchompa@gmail.com>
2018-07-13 11:32:59 -0400

    filled out blanks in index.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
+ I'll let you do this part on your own.
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
@@ -11,9 +11,6 @@ class App extends Component {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
       </div>
     );
   }
