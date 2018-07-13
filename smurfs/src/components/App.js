779bf83ff8809b58ac8e243ad70ff24e8c7c4ea5
snowcoding <vimalshah77@gmail.com>
2018-07-13 10:39:55 -0700

    ## Sprint Challenge: Redux
    ### Main Challenge
    * [x] Decide how to tackle the problem and create the necessary files

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
+const SmurfDiv = styled.div`
+  background:#fff;
+  position:absolute;
+  opacity:.6; /* manipulate to desired opacity */
+  height:20%;
+  width:10%;
+  opacity:0;
+  &:hover{
+    opacity:.6;
+  }
+  
+  /* Custom for each smurf*/
+  top: ${props => props.top};
+  left: ${props => props.left};
+`
+
 class App extends Component {
   render() {
+    const arr = [{"name":"Pappa Smurf","age":200,"height":"5cm", "top":"43%", "left":"28%"}, {"name":"Vanity Smurf","age":200,"height":"5cm", "top":"27%", "left":"39.7%"}]
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>Smurf Redux Village</h1>
+        { arr.map(smurf => <SmurfDiv top={smurf.top} left={smurf.left}>{smurf.name}</SmurfDiv>)}
       </div>
     );
   }
