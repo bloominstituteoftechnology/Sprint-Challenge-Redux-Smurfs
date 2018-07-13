8c5e4b7f0f2e23d08319f1b3bea26493f98b3f0b
raymondgrc <raymond_grc@yahoo.com>
2018-07-13 10:27:11 -0500

    Create simple actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,20 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from "axios";
+export const FETCHING_SMURF = "FETCHING_SMURF";
+export const FETCHED_SMURF = "FETCHED_SMURF";
+export const FETCHING_ERROR = "FETCHING_ERROR";
 
+export const getSmurfs = () => {
+  return function(dispatch) {
+        dispatch({type: FETCHING_SMURF});
+        axios.get(`http://localhost:3333/smurfs`)
+          .then((smurfs) => {
+              dispatch({ type: FETCHED_SMURF, payload: smurfs});
+            })
+          .catch(error => {
+              dispatch({type: FETCHED_ERROR, payload: console.log(error)});
+            })
+    };
+}
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
