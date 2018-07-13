9d41364f88b8df430fd8ef4f19076d3154bee298
DirupT <iamcooled@gmail.com>
2018-07-13 10:50:27 -0500

    added UpdateSmurfForm component

---
+++ smurfs/src/components/UpdateSmurfForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { editSmurf } from '../actions';
+
+class UpdateSmurfForm extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: '',
+            height: '',
+            age: ''
+        }
+    }
+
+    handleInput = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    editSmurf = () => {
+        const { name, height, age } = this.state;
+        const smurf = { name, height, age };
+
+        if (name === '' || height === '' || age === '') return;
+
+        this.props.editSmurf(smurf);
+        this.setState({ name: '', height: '', age: '' });
+    }
+
+    render() {
+        return (
+            <form onSubmit={event => event.preventDefault()}>
+                <input onChange={this.handleInput} value={this.state.name} type='text' name='name' placeholder='name' />
+                <input onChange={this.handleInput} value={this.state.height} type='text' name='height' placeholder='height' />
+                <input onChange={this.handleInput} value={this.state.age} type='text' name='age' placeholder='age' />
+                <button onClick={this.addSmurf}>Add Smurf</button>
+            </form>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs
+    }
+}
+
+export default connect(mapStateToProps, { editSmurf })(UpdateSmurfForm);
