0d086c785087d5524c5c0ef1401dc274c6e36294
Jasper <joshtietz11@gmail.com>
2018-07-13 10:38:53 -0500

    Josh Tietz -- Redux-sprint

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,4 +1,4 @@
-/* 
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,22 +1,41 @@
+import { combineReducers } from 'redux';
+
 /*
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+ /*Your initial/default state for this project could *Although does not have to* look a lot like this*/
+
+ const initialState =  {
    smurfs: [],
    fetchingSmurfs: false
+   fetched: false
    addingSmurf: false
    updatingSmurf: false
    deletingSmurf: false
    error: null
  }
-*/
+
+const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+  
+
+    default:
+        return state;
+  }
+}
+
+const reducer = combineReducers({
+    smurfReducer
+})
+
+export default smurfReducer;
+
 
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
