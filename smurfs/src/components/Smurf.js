9e06836f1ae621375fd42f0678d8f7f9da02b312
DirupT <iamcooled@gmail.com>
2018-07-13 11:09:47 -0500

    update form only shows when clicked

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -6,6 +6,10 @@ import { deleteSmurf } from '../actions';
 class Smurf extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            toggleEdit: false
+        }
     }
 
     render() {
@@ -15,12 +19,16 @@ class Smurf extends React.Component {
                 <p>Height: {this.props.smurf.height}</p>
                 <p>Age: {this.props.smurf.age}</p>
                 <button onClick={() => this.props.deleteSmurf(this.props.smurf.id)}>Delete</button>
-                <UpdateSmurfForm
-                    name={this.props.smurf.name}
-                    height={this.props.smurf.height}
-                    age={this.props.smurf.age}
-                    id={this.props.smurf.id}
-                />
+                <button onClick={() => this.setState({ toggleEdit: !this.state.toggleEdit })}>Edit</button>
+                {
+                    this.state.toggleEdit ? <UpdateSmurfForm
+                        name={this.props.smurf.name}
+                        height={this.props.smurf.height}
+                        age={this.props.smurf.age}
+                        id={this.props.smurf.id}
+                    /> : null
+                }
+
             </div>
         );
     }
