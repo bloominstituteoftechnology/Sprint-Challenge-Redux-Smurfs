82ad0f9f40422af3b4814265fd85de1524411cef
Kevin Tena <kevten09@gmail.com>
2018-07-13 12:25:51 -0500

    Set up component to input smurf info. Smurf adding now possible

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import { fetchSmurfs } from '../actions';
+import { fetchSmurfs, addSmurf } from '../actions';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -9,9 +9,26 @@ import { fetchSmurfs } from '../actions';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  state ={
+    name: '',
+    age: '',
+    email: ''
+  };
+
   componentDidMount() {
     this.props.fetchSmurfs();
   }
+
+  inputHandler = event => {
+    this.setState({ [event.target.name]: event.target.value});
+  }
+
+  addHandler = () => {
+    const {name, age, height} = this.state;
+    this.props.addSmurf({name, age, height});
+    this.setState({ name: '', age: '', height: ''});
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,9 +36,34 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <input
+          className="input"
+          value={this.state.name}
+          onChange={this.inputHandler}
+          name="name"
+          type="text"
+          placeholder="name"
+        />
+        <input
+          className="input"
+          value={this.state.age}
+          onChange={this.inputHandler}
+          name="age"
+          type="text"
+          placeholder="age"
+        />
+        <input
+          className="input"
+          value={this.state.height}
+          onChange={this.inputHandler}
+          name="height"
+          type="text"
+          placeholder="height"
+        />
+        <button onClick={() => this.addHandler()} type="button"> Add Smurf </button>
         <div> Smurfs: </div>
         {this.props.smurfs.map(smurf => {
-              return <div key={smurf.id}>{smurf.name} Age:{smurf.age} Height:{smurf.height} </div>;
+              return <div key={smurf.id}>{smurf.name} Age: {smurf.age} Height: {smurf.height} </div>;
             })}
       </div>
     );
@@ -34,4 +76,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { fetchSmurfs }) (App);
+export default connect(mapStateToProps, { fetchSmurfs, addSmurf }) (App);
