ef03e2fd88c57442df0b51460e86c5a927c6ecb1
CLN <cln@Sags-MacBook-Pro.local>
2018-07-13 12:15:25 -0400

    added exports to actions, .

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,18 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const ERROR = 'ERROR';
+export const FETCH_SMURFS = 'FETCH_SMURFS';
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADD_SMURF = 'ADD_SMURF';
+export const UPDATE_SMURF = 'UPDATE_SMURF';
+export const DELETE_SMURF = 'DELETE_SMURF';
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const DELETING_SMURF = 'DELETING_SMURF';
+
+
+
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
