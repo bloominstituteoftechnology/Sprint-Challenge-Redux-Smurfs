bfeea983a601b9f2b85850021a6b9e986ac761ef
Tyson Gushiken <tysonism@gmail.com>
2018-07-13 10:29:22 -0700

    Create delete functionality

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -10,10 +10,12 @@ export const DELETING_SMURF = 'DELETING_SMURF';
 export const SMURF_DELETED = 'SMURF_DELETED';
 export const DATA_ERROR = 'DATA_ERROR';
 
+const API_URL = 'http://localhost:3333/smurfs';
+
 export const fetchData = () => {
   return function(dispatch) {
     dispatch({ type: FETCHING_DATA });
-    axios.get('http://localhost:3333/smurfs')
+    axios.get(API_URL)
     .then(response => {
       dispatch({ type: DATA_FETCHED, payload: response.data });
     })
@@ -21,12 +23,34 @@ export const fetchData = () => {
   };
 };
 
-export const addSmurf = (smurf) => {
+export const addSmurf = smurf => {
   return function(dispatch) {
     dispatch({ type: ADDING_SMURF });
-    axios.post('http://localhost:3333/smurfs', smurf)
+    axios.post(API_URL, smurf)
     .then(response => {
       dispatch({ type: SMURF_ADDED, payload: response.data });
+    })
+    .catch(e => dispatch({ type: DATA_ERROR, payload: e }));
+  };
+};
+
+export const updateSmurf = smurf => {
+  return function(dispatch) {
+    dispatch({ type: UPDATING_SMURF });
+    axios.put(`${API_URL}/${smurf.id}`, smurf)
+    .then(response => {
+      dispatch({ type: SMURF_UPDATED, payload: response.data });
+    })
+    .catch(e => dispatch({ type: DATA_ERROR, payload: e }));
+  };
+}
+
+export const deleteSmurf = id => {
+  return function(dispatch) {
+    dispatch({ type: DELETING_SMURF });
+    axios.delete(`${API_URL}/${id}`)
+    .then(response => {
+      dispatch({ type: SMURF_DELETED, payload: response.data });
     })
     .catch(e => dispatch({ type: DATA_ERROR, payload: e }));
   };
--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { updateSmurf, deleteSmurf } from '../actions';
 
 const Smurf = props => {
   return (
-    <li className="smurf">{props.smurf.name}</li>
+    <li className="smurf">
+      <span className="smurf__name">{props.smurf.name}</span>
+      <span className="smurf__age">{props.smurf.age}</span>
+      <span className="smurf__height">{props.smurf.height}</span>
+      <button onClick={() => {props.updateSmurf(props.smurf)}}>Update</button>
+      <button onClick={() => {props.deleteSmurf(props.smurf.id)}}>Delete</button>
+    </li>
   );
 }
 
-export default Smurf;
+export default connect(null, { updateSmurf, deleteSmurf })(Smurf);
