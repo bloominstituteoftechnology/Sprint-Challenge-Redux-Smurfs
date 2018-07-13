239823abecdfc77de815d2efc7f53397c66f8a3d
CLN <cln@Sags-MacBook-Pro.local>
2018-07-13 11:56:40 -0400

    Working on reducer, .

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,19 +1,44 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import * as actionTypes from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+//  Your initial/default state for this project could *Although does not have to* look a lot like this
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
- }
-*/
+ };
 
+ export const smurfsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.FETCHING_SMURFS:
+      return { ...state, fetchingSmurfs: true };
+    case actionTypes.ADDING_SMURF:
+      return { ...state, addingSmurf: false };
+    case actionTypes.UPDATING_SMURF:
+      return { ...state, updatingSmurf: true };
+    
+    case actionTypes.DELETING_SMURF:
+      return { ...state, deletingSmurf: true };
+    
+    case actionTypes.ERROR:
+      return {
+        ...state,
+        gettingFriends: false,
+        creatingFriend: false,
+        deletingFriend: false,
+        updatingFriend: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
