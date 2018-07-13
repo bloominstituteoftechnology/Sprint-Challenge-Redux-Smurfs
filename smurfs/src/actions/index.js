9c0d87fbf77cb46eac27784161019ee2eb65e00a
DirupT <iamcooled@gmail.com>
2018-07-13 10:26:53 -0500

    added action exports / imported action exports into reducer / added cases for reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,17 @@
+import axios from 'axios';
+
 export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const SMURF_FETCHED = 'SMURF_DELETED';
+
 export const ADDING_SMURF = 'ADDING_SMURF';
+export const SMURF_ADDED = 'SMURF_ADDED';
+
 export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const SMURF_UPDATED = 'SMURF_UPDATED';
+
 export const DELETING_SMURF = 'DELETING_SMURF';
+export const SMURF_DELETED = 'SMURF_DELETED';
+
 export const ERROR = 'ERROR';
 
 /*
@@ -14,3 +24,11 @@ export const ERROR = 'ERROR';
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+  const promise = axios.get('http://localhost:3333/smurfs/');
+  return dispatch => {
+    dispatch({ type: FETCHING_SMURFS })
+    promise.then(response => dispatch)
+  }
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,12 +1,16 @@
-import { FETCHING_SMURFS, ADDING_SMURF, UPDATING_SMURF, DELETING_SMURF, ERROR } from '../actions';
+import { FETCHING_SMURFS, SMURF_FETCHED, ADDING_SMURF, SMURF_ADDED, UPDATING_SMURF, SMURF_UPDATED, DELETING_SMURF, SMURF_DELETED, ERROR } from '../actions';
 
 
 const initialState = {
   smurfs: [],
   fetchingSmurfs: false,
+  smurfFetched: false,
   addingSmurf: false,
+  smurfAdded: false,
   updatingSmurf: false,
+  smurfUpdated: false,
   deletingSmurf: false,
+  smurfDeleted: false,
   error: null
 }
 
@@ -15,17 +19,26 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case FETCHING_SMURFS:
       return;
-      
-    case FETCHING_SMURFS:
+
+    case SMURF_FETCHED:
       return;
 
     case ADDING_SMURF:
       return;
 
+    case SMURF_ADDED:
+      return;
+
     case UPDATING_SMURF:
       return;
 
+    case SMURF_UPDATED:
+      return;
+
     case DELETING_SMURF:
+      return;
+
+    case SMURF_DELETED:
       return;
 
     case ERROR:
