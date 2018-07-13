63be59e79f2ec71fd26cd4d7563c4069b7482db2
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-07-13 12:26:37 -0400

    data shows

---
+++ smurfs/src/components/smurfs/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Smurf from './Smurf';
+
+export default class smurfs extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.smurfs.map(smurf => {
+                    return (
+                        <div className="list_item" key={smurf.id}>
+                            <Smurf
+                                {...this.props}
+                                id={smurf.id}
+                                name={smurf.name}
+                                age={smurf.age}
+                                height={smurf.height}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+};
