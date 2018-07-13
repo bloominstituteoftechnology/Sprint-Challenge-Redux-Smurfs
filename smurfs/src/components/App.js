68334dbb9e691f9411366eb93fba4ea4da872103
Sukhada Gholba <sukhadagholba@gmail.com>
2018-07-13 09:08:36 -0700

    added input fields to add new data (smurfs) to the list

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -8,7 +8,9 @@ class App extends Component {
 constructor(props){
         super(props);
         this.state= {
-     
+    		name: "",
+		age: "",
+		height: ""
         };
 
 }
@@ -18,7 +20,12 @@ constructor(props){
     this.props.fetchingSmurfsAction(); 
   }	
 	
-	
+changeHandler = event => {
+        this.setState({[event.target.name]: event.target.value});
+
+};
+
+
 	
 render() {
     return (
@@ -26,11 +33,20 @@ render() {
 	  <div>{this.props.fetching ? (
           <h1>Fetching Smurfs...</h1>
         ) :( 
-        <ul>
+      <div>  <ul>
             {this.props.smurfs.map(smurf => {
               return <li key={smurf.id}>{smurf.name}, {smurf.age}, {smurf.height}</li>;
             })}
           </ul>
+
+ 
+		<input onChange={this.changeHandler} type="text" name="name" placeholder="Name" value={this.state.name} />
+                <input onChange={this.changeHandler} type="number" name="age" placeholder="Age" value={this.state.age} />
+                <input onChange={this.changeHandler} type="text" name="height" placeholder="Height" value={this.state.height} />
+
+                <button onClick={()=> this.addSmurfHandler()}>Add New Smurf</button>
+
+</div>
 	)}</div>
       </div>
 	    
