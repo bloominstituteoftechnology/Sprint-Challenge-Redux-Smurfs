5aaae35e241623af05118936ae27552df89e260c
Jameson <jamesonbrown@neolivity.com>
2018-07-13 10:18:00 -0600

    Created reducer for fetching, fetched, adding, and error

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,38 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { FETCHING, FETCHED, ADDING, ERROR } from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  fetchingSmurfs: false,
+  fetchedSmurfs: false,
+  addingSmurf: false,
+  smurfs: [],
+  error: null
+}
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+export const smurfsReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case FETCHING:
+      return Object.assign({}, state, {
+        fetchingSmurfs: true,
+        fetchedSmurfs: false,
+        error: null
+      });
+    case FETCHED:
+      return Object.assign({}, state, {
+        fetchingSmurfs: false,
+        fetchedSmurfs: true,
+        addingSmurf: false,
+        smurfs: action.payload,
+      });
+    case ADDING:
+      return Object.assign({}, state, {
+        addingSmurf: true
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        fetchingSmurfs: false,
+        error: action.payload
+      });
+    default:
+      return state;
+  }
+}
