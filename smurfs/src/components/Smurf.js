466f5979b64792c34615233d4575c7e366dc7dd6
DirupT <iamcooled@gmail.com>
2018-07-13 10:37:09 -0500

    display smurf data to screen

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 class Smurf extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
 
     render() {
         return (
             <div>
-                smurfs
+                <p>Name: {this.props.smurf.name}</p>
+                <p>Height: {this.props.smurf.height}</p>
+                <p>Age: {this.props.smurf.age}</p>
             </div>
         );
     }
