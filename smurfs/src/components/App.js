dd877e753902cc1f697df2f44137d5a60abd3d8a
Alec <afroenygam@gmail.com>
2018-07-13 12:36:08 -0400

    App.js connected to store

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux';
+import {fetchData} from '../actions';
+
+
 /*
  to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
+ I'll let you do this part on your own.
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -19,4 +24,15 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    friends: state.friends,
+    fetching: state.fetchingSmurfs,
+    saving: state.addingSmurfs,
+    updating: state.updatingSmurf,
+    deleting: state.deletingSmurf,
+    test: state
+  }
+}
+
+export default connect(mapStateToProps, {fetchData})(App);
