0038431640224f144d260d7e0bb71046cf6c2a49
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-07-13 11:20:24 -0400

    Blake Fletcher - Sprint-Challenge-Redux
    First commit
    Boilerplate complete
    store and imports added to index.js
    Smurf, Smurfs, and SmurfForm - blank files created
    Action creators added to actions/index.js
    very basic fetchingSmurfs action added as well
    reducers/index and /smurfsReducers created

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,15 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const SMURFS_FETCHED = 'SMURFS_FETCHED';
+export const SMURFS_SAVED = 'SMURFS_SAVED';
+export const SAVING_SMURFS = 'SAVING_SMURFS';
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const SMURF_UPDATED = 'SMURF_UPDATED';
+export const DELETING_SMURF = 'DELETING_SMURF';
+export const SMURF_DELETED = 'SMURF_DELETED';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +22,9 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const fetchingSmurfs = () => {
+    return {
+        type: FETCHING_SMURFS,
+    };
+}; 
