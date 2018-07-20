b922967fa34bacdf00467f3fc84192b67163cc6b
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-20 09:37:28 -0700

    Create rootReducer Implement action creators for GETing and POSTing smurfs Create SmurfsList component Wire things up

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,26 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { ERROR,  FETCHING_SMURFS, FETCH_SMURFS, ADDING_SMURF, ADD_SMURF } from '../actions/index';
+
+const initialState = {
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  smurfs: [],
+  error: null
+};
+
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_SMURFS: 
+      return {...state, fetchingFriends: true}
+    case FETCH_SMURFS: 
+      return {...state, friends: action.payload, fetchingFriends: false};
+    case ADDING_SMURF:
+      return {...state, addingFriend: true};
+    case ADD_SMURF:
+      return {...state, addingFriend: false, friends: action.payload};
+    default:
+      return state;
+  }
+}
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
