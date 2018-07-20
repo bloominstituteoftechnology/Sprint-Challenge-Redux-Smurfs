867b14fd404d9cc0655e14c3b7e4408aba142175
Eric B <timberwolf.eric@gmail.com>
2018-07-20 14:27:36 -0500

    Building Actions File with fetchSmurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,28 @@
-/* 
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
 
+export const GET_SMURFS = 'GET_SMURFS';
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADD_SMURF = 'ADD_SMURF'
+
+export const fetchSmurfs = () => {
+  return dispatch => {
+  dispatch({type: FETCHING_SMURFS});
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      console.log(response);
+      dispatch({type: FETCH_SMURFS, payload: response.data});
+    })
+    .catch(err => {
+      dispatch({type: ERROR, payload: err});
+    });
+  };
+}
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
