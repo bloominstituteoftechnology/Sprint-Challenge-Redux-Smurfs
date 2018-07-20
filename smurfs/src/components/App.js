d70dea5338bb390c177496c180eca720e2ea4c64
Katie Gorbell <katiegorbell@gmail.com>
2018-07-20 10:19:06 -0500

    initial push

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -11,9 +11,6 @@ class App extends Component {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
       </div>
     );
   }
