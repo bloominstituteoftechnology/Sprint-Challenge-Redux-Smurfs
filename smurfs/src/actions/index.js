7c6d33227f8ebac9b54702e236a26c1d56aa5ce9
Kairi <kairijb@gmail.com>
2018-07-20 10:28:23 -0500

    added actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,7 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+import axios from 'axios'
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +13,24 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const FETCHING_SMURF = FETCHING_SMURF;
+export const ADDING_SMURF = ADDING_SMURF
+export const UPDAING_SMURF = UPDAING_SMURF;
+export const DELETING_SMURF = DELETING_SMURF;
+export const ERROR = ERROR;
+
+export const fetchSmurf = () => {
+  const promise = axios.get('http://localhost:3333');
+
+  return function(dispatch) {
+    dispatch({type: FETCHING_SMURF});
+    promise.then((response) => {
+      console.log(response);
+      dispatch({type: ADDING_SMURF, payload: response.data.results})
+    })
+    .catch(err => {
+      dispatch({ype: ERROR, msg: "Error fetching smurf"})
+    })
+  }
+}
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -8,10 +8,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import /* You need some sort of reducer */ './reducers';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
 
 ReactDOM.render(
   <Provider store={store}>
