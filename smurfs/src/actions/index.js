22f96dbf853d1cbc91aece80c184897fb796e96e
Katie Gorbell <katiegorbell@gmail.com>
2018-07-20 12:11:45 -0500

    changed URL to get rid of error in order to continue onto add new smurf form

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,34 +1,21 @@
 import axios from 'axios';
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+
 
 export const FETCHING_SMURFS = 'FETCHING_SMURFS';
 export const FETCHED_SMURFS = 'FETCHED_SMURFS';
 export const ERROR_FETCHING_SMURFS = 'ERROR_FETCHING_SMURFS';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
 
 export const fetchSmurfs = () => {
   return dispatch => {
     dispatch({ type: FETCHING_SMURFS });
     axios
-      .get('http://localhost:3333/smurfs')
+      .get('http://localhost:3333')
       .then(response => {
         dispatch({ type: FETCHED_SMURFS, payload: response.data.results });
       })
       .catch(error => 
-        dispatch({ type: ERROR_FETCHING_SMURFS, payload: 'Error retrieving Smurfss'})
+        dispatch({ type: ERROR_FETCHING_SMURFS, payload: 'Error retrieving Smurfs: ', error})
       );
   }
 }
