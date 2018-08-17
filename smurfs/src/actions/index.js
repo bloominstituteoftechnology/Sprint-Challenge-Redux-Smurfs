9477e21e888170173e510962403e42f84dfe5367
szincone <sawyerzincone@gmail.com>
2018-08-17 09:41:33 -0600

    created actions for apifetch, reducer for initial state & apifetch

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,9 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from "axios";
+
+// export const ADD_NEW_SMURF = "ADD_NEW_SMURF";
+export const FETCHING = "FETCHING";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +15,17 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+const url = "http://localhost:3333/smurfs";
+
+export const fetchSmurfData = () => {
+  return function(dispatch) {
+    dispatch({ type: FETCHING });
+    axios
+      .get(url)
+      .then(response =>
+        dispatch({ type: FETCH_SUCCESS, payload: response.data })
+      )
+      .catch(error => dispatch({ type: FETCH_FAILURE, payload: error }));
+  };
+};
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from "../actions";
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -13,7 +14,14 @@
    error: null
  }
 */
-
+const initialState = {
+  // define a few properties here.
+  // Array chars, Boolean fetching, Boolean fetched, null error.
+  smurfs: [],
+  isFetching: false,
+  isFetched: false,
+  hasError: false
+};
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +29,33 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    // Fill me in with the important reducers
+    // action types should be FETCHING, FETCHED, and ERROR
+    // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {
+        ...state,
+        isFetching: true,
+        isFetched: false
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        isFetched: true,
+        smurfs: action.payload
+      };
+    case FETCH_FAILURE:
+      return {
+        ...state,
+        isFetching: true,
+        isFetched: false,
+        hasError: action.payload
+      };
+    default:
+      return state;
+  }
+};
