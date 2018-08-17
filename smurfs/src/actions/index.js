b8ec8f26ccb51910abaa044ebf9d59edc149a5af
Munsu <dark_specineff2002@yahoo.com>
2018-08-17 10:52:13 -0700

    commit #2

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,11 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const ADD_SMURF = 'ADD_SMURF';
+export const GET_SMURFS = 'GET_SMURFS';
+export const GOT_SMURFS = 'GOT_SMURFS';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +18,22 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+  const promise = axios.get(`http://localhost:3333/smurfs`);
+  return dispatch => {
+    dispatch({ type: GET_SMURFS }); 
+    promise.then(response => {
+      dispatch({ type: GOT_SMURFS, payload: response.data });
+    });
+  };
+};
+
+export const addSmurf = (smurf) => {
+  const post = axios.post('http://localhost:3333/smurfs', smurf);
+  return dispatch => {
+    post.then(response => {
+      dispatch({ type: ADD_SMURF, payload: response.data });
+    });
+  };
+};
