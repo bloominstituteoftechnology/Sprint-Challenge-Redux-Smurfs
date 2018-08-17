59551274ff452d377254a2da6037d9a571f2f3e2
codejoncode <jonathanjholloway@gmail.com>
2018-08-17 10:34:28 -0500

    completed all of the neccessary actions for todays task and stretch goals.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,18 @@
+import axios from 'axios'; 
+
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCHED_SMURFS = 'FETCHED_SMURFS'; 
+export const ADDED_SMURF = 'ADDED_SMURF';
+export const POSTING_SMURF = 'POSTING_SMURF';
+export const POSTED_SMURF = 'POSTED_SMURF';
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const UPDATED_SMURF = 'UPDATED_SMURF'; 
+export const DELETING_SMURF = 'DELETING_SMURF';
+export const DELETED_SMURF = 'DELETED_SMURF';
+export const ERROR = 'ERROR'; 
+
+
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
@@ -13,3 +28,63 @@
    U - updateSmurf
    D - deleteSmurf
 */
+const url = 'http://localhost:3333/smurfs/';
+export const getSmufs = () => {
+  const promise = axios.get(url)
+  return dispatch => {
+    dispatch({type:FETCHING_SMURFS})
+    promise.then(response => {
+      console.log(response) // console.log for now... delete later... 
+      dispatch({type: FETCHED_SMURFS, payload: response.data})
+    })
+    .catch(error => {
+      console.log(error) // console.log for now... delete later.. 
+      dispatch({type: ERROR, error: error})
+    })
+  }
+}
+
+export const addSmurf = (smurf) => {
+  const promise = axios.post(url, smurf); 
+  return dispatch => {
+    dispatch({type:POSTING_SMURF})
+    promise.then(response => {
+      console.log(response) // console.log for now... delete later.. 
+      dispatch({type: POSTED_SMURF, payload: response.data})
+    })
+    .catch(error => {
+      console.log(error) // console.log for now... delete later.. 
+      dispatch({type:ERROR, error: error}) 
+    })
+  }
+}
+
+export const updateSmuf = (id, smurf) => {
+  const promise = axios.put(`${url}${id}`, smurf);
+  return dispatch => {
+    dispatch({type:UPDATING_SMURF})
+    promise.then(response => {
+      console.log(response) // console.log for now... delete later... 
+      dispatch({type: UPDATED_SMURF, payload: response.data})
+    })
+    .catch(error => {
+      console.log(error) // console.log for now.. delete later.. 
+      dispatch({type: ERROR, error: error})
+    })
+  }
+}
+
+export const deleteSmurf = (id) => {
+  const promise = axios.delete(`${url}${id}`)
+  return dispatch => {
+    dispatch({type: DELETING_SMURF})
+    promise.then(response => {
+      console.log(response)
+      dispatch({type:DELETED_SMURF, payload: response.data})
+    })
+    .catch(error => {
+      console.log(error) // console.log for now... delete later... 
+      dispatch({type:ERROR, error: error})
+    })
+  }
+}
