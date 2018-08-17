cf1194a3126959e58574762ad45382bb5d3f34b9
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-17 08:41:31 -0700

    Major changes - Index.js
    Finished setting up the store in my index.js file.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import ***** from './reducers/index';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+const store = createStore(**** ,applyMiddleware(logger, thunk));
 
 ReactDOM.render(
   <Provider store={store}>
