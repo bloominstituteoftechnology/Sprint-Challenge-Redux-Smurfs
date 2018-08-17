cb775abd1c846283d89dbf44573c987bd2d2d640
Lauren <llwor94@gmail.com>
2018-08-17 10:54:22 -0700

    all functionality working

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <h1>Welcome to SmurfTown</h1>
         {this.props.fetchingSmurfs ? (
           <p> the smurfs b comin</p>
         ) : (
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,13 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Smurf from "./Smurf";
 
 const FriendsList = props => (
   <div>
     {props.smurfs.map((smurf, index) => (
-      <Fragment>
-        <Smurf key={index} smurf={smurf} />
+      <div key={index} >
+        <Smurf smurf={smurf} />
         <button onClick={() => props.handleDelete(smurf.id)}>{`delete ${smurf.name}?`}</button>
-      </Fragment>
+      </div>
     ))}
   </div>
 );
