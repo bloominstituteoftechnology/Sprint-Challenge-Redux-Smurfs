e4fc018941012f6b71c0f9763284994c1879e328
jordan-massingill <jordan_massingill@hotmail.com>
2018-08-17 12:29:58 -0500

    Added some styling

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Form } from 'reactstrap';
+
+const StyledForm = styled(Form) `
+  display: flex,
+  flex-direction: column
+`;
 
 export default function SmurfForm(props) {
   return (
-    <form onSubmit={props.submitSmurf}>
+    <StyledForm onSubmit={props.submitSmurf}>
       <input type='text' placeholder='name...' value={props.newName} name='newName' onChange={props.changeHandler} />
       <input type='text' placeholder='age...' value={props.newAge} name='newAge' onChange={props.changeHandler} />
       <input type='text' placeholder='height...' value={props.newHeight} name='newHeight' onChange={props.changeHandler} />
       <button type='submit'>Add New Smurf!</button>
-    </form>
+    </StyledForm>
   )
 }
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledLi = styled.li `
+  margin: 20px auto;
+`;
+
+const P = styled.p `
+  margin: 0 auto;
+  padding: 0;
+  font-weight: bold;
+`;
+
+const SmurfsDiv = styled.div `
+  max-width: 980px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const StyledUl = styled.ul `
+  width: 300px;
+  margin: 40px auto;
+`;
 
 export default function SmurfsList(props) {
   return (
-    <div>
-      <ul>{props.smurfs.map((smurf, index) =>
-        <li key={index}>{smurf.name}<br/>
+    <SmurfsDiv>
+      <h1>SMURFS!</h1>
+      <StyledUl>{props.smurfs.map((smurf, index) =>
+        <StyledLi key={index}><P>{smurf.name.toUpperCase()}</P><br/>
         {smurf.age} <br/>
-        {smurf.height}</li>)}
-        
-      </ul>
-    </div>
+        {smurf.height}</StyledLi>)}
+      </StyledUl>
+    </SmurfsDiv>
   )
 }
