e598695d35ececa490122debe60f85696856312c
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-17 10:38:11 -0600

    Added a form and hooked it up to the addSmurf post request

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,20 +3,49 @@ import './App.css';
 import { connect } from 'react-redux'; 
 import {getSmurfs, addSmurf} from '../actions'; 
 import Smurf from './Smurf'; 
+import NewSmurfForm from './NewSmurfForm'; 
 
 class App extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      name: '', 
+      age: '', 
+      height: '', 
+    }
   }
 
   componentDidMount(){
     this.props.getSmurfs(); 
   }
 
+  changeInputHandler = (event)  => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  submitDataHandler = (event) => {
+    const newSmurf = {
+      name: this.state.name, 
+      age: this.state.age, 
+      height: this.state.height
+    }
+
+    this.props.addSmurf(newSmurf); 
+
+    this.setState({
+      name: '', 
+      age: '', 
+      height: ''
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
+        <NewSmurfForm  change = {this.changeInputHandler} submit = {this.submitDataHandler} />
         {this.props.loadingSmurfs ? (
           <p> Loading Smurfs.... </p>
         ): this.props.smurfs.map(smurf => {
@@ -34,4 +63,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, {getSmurfs})(App);
+export default connect(mapStateToProps, {getSmurfs, addSmurf})(App);
