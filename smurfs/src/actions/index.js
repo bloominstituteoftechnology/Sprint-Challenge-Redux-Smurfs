09a0b82fbfbd3caa17c2d56bd16a344521ceaeb3
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-08-17 13:45:45 -0400

    MVP completed - minor bug with importing reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,58 +1,75 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from "axios";
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const GETTING_SMURFS = "GETTING_SMURFS";
+export const GET_SMURFS_SUCCESS = "GET_SMURFS_SUCCESS";
+export const GET_SMURFS_FAILURE = "GET_SMURFS_FAILURE";
 
-import axios from 'axios';
+export const ADDING_SMURF = "ADDING_SMURF";
+export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESS";
+export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE";
 
-export const ERROR = 'ERROR';
+export const MURDERING_SMURF = "MURDERING_SMURF";
+export const MURDER_SMURF_SUCCESS = "MURDER_SMURF_SUCCESS";
+export const MURDER_SMURF_FAILURE = "MURDER_SMURF_FAILURE";
 
-export const GETTING_SMURFS = 'GETTING_SMURFS';
-export const GET_SMURFS = 'GET_SMURFS';
-
-export const ADDING_SMURFS = 'ADDING_SMURFS';
-export const ADD_SMURFS = 'ADD_SMURFS';
-
-const URL = 'http://localhost:3333/smurfs';
-
+const URL = "http://localhost:3333/smurfs";
 
 export const getSmurfs = () => {
-    const smurfs = axios.get(URL);
-    return dispatch => {
-      dispatch({ type: GETTING_SMURFS });
-      smurfs
-        .then(response => {
-          dispatch({ type: GET_SMURFS, payload: response.data})
-        })
-        .catch(error => {
-          dispatch({ type: ERROR, payload: error});
-        }) 
-    }
-}
-
+	const promise = axios.get(URL);
+	return dispatch => {
+		dispatch({ type: GETTING_SMURFS });
+		promise
+			.then(response => {
+				dispatch({
+					type: GET_SMURFS_SUCCESS,
+					payload: response.data,
+				});
+			})
+			.catch(err => {
+				dispatch({
+					type: GET_SMURFS_FAILURE,
+					payload: err,
+				});
+			});
+	};
+};
 
 export const addSmurf = data => {
-  const smurfs = axios.post(URL, data);
-  return dispatch => {
-    dispatch({ type: ADDING_SMURFS });
-    smurfs
-      .then(response => {
-        dispatch({ type: ADD_SMURFS, payload: response.data })
-      })
-      .catch(error => {
-        dispatch({ type: ERROR, payload: error });
-      })
-  }
-}
- 
+	const promise = axios.post(URL, data);
+	return dispatch => {
+		dispatch({ type: ADDING_SMURF });
+		promise
+			.then(response => {
+				dispatch({
+					type: ADD_SMURF_SUCCESS,
+					payload: response.data,
+				});
+			})
+			.catch(err => {
+				dispatch({
+					type: ADD_SMURF_FAILURE,
+					payload: err,
+				});
+			});
+	};
+};
+
+export const murderSmurf = id => {
+	const promise = axios.delete(`${URL}/${id}`);
+	return dispatch => {
+		dispatch({ type: MURDERING_SMURF });
+		promise
+			.then(response => {
+				dispatch({
+					type: MURDER_SMURF_SUCCESS,
+					payload: response.data,
+				});
+			})
+			.catch(err => {
+				dispatch({
+					type: MURDER_SMURF_FAILURE,
+					payload: err,
+				});
+			});
+	};
+};
