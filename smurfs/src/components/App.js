90611211b545db9af2426ed73ee3c303b3e26243
szincone <sawyerzincone@gmail.com>
2018-08-17 10:02:23 -0600

    smurfs loading on page, initial data got"

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSmurfData } from "../actions/index";
+import "./App.css";
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,6 +9,15 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newSmurf: ""
+    };
+  }
+  componentDidMount() {
+    this.props.fetchSmurfData();
+  }
   render() {
     return (
       <div className="App">
@@ -14,9 +25,25 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <ul style={{ listStyle: "none" }}>
+          {this.props.smurfs.map(smurf => {
+            return <li key={smurf.name}>{smurf.name}</li>;
+          })}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    fetchSmurfData
+  }
+)(App);
