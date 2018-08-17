ea25e0e58850c3e2b5ae11245838dc78c1e3cfac
Trevor <trevorfehrman@gmail.com>
2018-08-17 09:48:35 -0700

    MVP complete

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -21,6 +21,10 @@ class App extends Component {
 		this.props.getSmurfs();
 	}
 
+	handleChange = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
 	render() {
 		return (
 			<div className="App">
@@ -33,6 +37,42 @@ class App extends Component {
 						</div>
 					);
 				})}
+				<form
+					onSubmit={event => {
+						event.preventDefault();
+						this.props.addSmurf(this.state);
+						this.setState(initState);
+					}}
+				>
+					<div>
+						<input
+							onChange={this.handleChange}
+							type="text"
+							placeholder="Name"
+							name="name"
+							value={this.state.name}
+						/>
+					</div>
+					<div>
+						<input
+							onChange={this.handleChange}
+							type="number"
+							placeholder="Age"
+							name="age"
+							value={this.state.age}
+						/>
+					</div>
+					<div>
+						<input
+							onChange={this.handleChange}
+							type="text"
+							placeholder="Height"
+							name="height"
+							value={this.state.height}
+						/>
+						<button>Add Smurf</button>
+					</div>
+				</form>
 			</div>
 		);
 	}
