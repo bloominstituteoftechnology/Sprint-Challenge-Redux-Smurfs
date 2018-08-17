a30b022168ca4e066b5bd62068bd4867178826c6
mkerbleski <mkerbles@gmail.com>
2018-08-17 09:29:13 -0600

    added actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,27 @@
-/* 
+//actions
+import axios from 'axios';
+
+export const GETTING_SMURFS from 'GETTING_SMURFS';
+export const GOT_SMURFS from 'GOT_SMURFS';
+export const ERROR from 'ERROR';
+export const ADDING_SMURF from 'ADDING_SMURF';
+export const UPDATING_SMURF from 'UPDATING_SMURF';
+export const DELETING_SMURF from 'DELETING_SMURF';
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+
+export const addSmurfs = () => {
+  return function(dispatch){
+    dispatch({type: GETTING_SMURFS})
+    axios.get(`http://localhost:3333/smurfs`).then(res => {
+      dispatch({type: GOT_SMURFS, payload: res.data}).catch(err => {
+        dispatch({type: ERROR, payload: err})
+      })
+    })
+  }
+}
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux';
+import styled from 'styled-components';
 /*
  to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
+ I'll let you do this part on your own.
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
+const AppDiv = styled.div`
+  border: 1px solid red;
+`;
+
 class App extends Component {
+  componentDidMount(){
+    console.log('CDM')
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
+      <AppDiv>
+        <h1>Smurfs</h1>
+      </AppDiv>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {state: state};
+}
+
+export default connect(mapStateToProps)(App);
