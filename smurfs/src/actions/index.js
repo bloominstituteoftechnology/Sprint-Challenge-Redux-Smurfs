5ca714fd833a414f8b93352dd64a7dcc8b0f9ae4
Kairi <kairijb@gmail.com>
2018-08-17 11:09:36 -0500

    created get smurf actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,7 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+import axios from 'axios'
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +13,26 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const GET_SMURF = 'GET_SMURF'
+export const GOT_SMURF = 'GOT_SMURF'
+
+export const CREATING_SMURF = 'CREATING_SMURF'
+export const CREATED_SMURF = 'CREATED_SMURF'
+
+export const ERROR = 'ERROR'
+
+export const getSmurf = () => {
+  const promise = axios.get('http://localhost:3333/smurfs')
+
+  return dispatch => {
+    dispatch({type: GET_SMURF});
+    getSmurf.then((response) => {
+      console.log(response.data)
+      dispatch({type: GOT_SMURF, payload: response.data})
+    })
+    .catch(err => {
+      dispatch({type: ERROR, payload: err})
+    })
+  }
+}
