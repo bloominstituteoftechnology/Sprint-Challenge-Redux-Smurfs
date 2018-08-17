5e1814c3119bb65d9ade43ac100019c7618b22ea
Carey <careycbaldwin@gmail.com>
2018-08-17 11:42:04 -0400

    created smurf and smurfs component, pulled in smurf data from server with .map and nested smurf component in App to display data

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+
+import Smurfs from './Smurfs';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
+const url = 'http://localhost:3333/smurfs'
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurfs: [],
+      name: '',
+      age:'',
+      height: ''
+    };
+  }
+
+componentDidMount() {
+  axios
+  .get(url)
+  .then((response)=>this.setState({smurfs: response.data}))
+  .catch(error => console.log(error))
+}
+
+
   render() {
     return (
       <div className="App">
@@ -14,6 +39,12 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+
+        <div>
+          <Smurfs 
+          smurfs = {this.state.smurfs}
+          />
+        </div>
       </div>
     );
   }
