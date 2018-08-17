e7b848816259fbc97dafbc8ffbb4ecfe7aab8166
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-17 08:37:49 -0700

    implement get method

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,17 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const RETRIEVING_SMURFS = 'RETRIEVING_SMURFS';
+export const RETRIEVED_SMURFS = 'RETRIEVED_SMURFS';
+export const ADDING_SMURFS = 'ADDING_SMURFS';
+export const ADDED_SMURFS = 'ADDED_SMURFS';
+export const UPDATING_SMURFS = 'UPDATING_SMURFS';
+export const UPDATED_SMURFS = 'UPDATED_SMURFS';
+export const DELETING_SMURFS = 'DELETING_SMURFS';
+export const DELETED_SMURFS = 'DELETED_SMURFS';
+export const ERROR = 'ERROR';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +23,16 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: RETRIEVING_SMURFS });
+    axios.get(`http://localhost:3333/smurfs`)
+      .then(res => {
+        dispatch({ type: RETRIEVED_SMURFS, payload: res.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err});
+      })
+  }
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,18 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import * as actionTypes from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+const initialState =  {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
+
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +21,27 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type){
+    case actionTypes.RETRIEVING_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      }
+    case actionTypes.RETRIEVED_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      }
+    case actionTypes.ERROR:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      }
+    default:
+      return state;
+  }
+}
