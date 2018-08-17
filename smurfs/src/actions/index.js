3db5b40112a52159648398a1e2fcbe143ff71f01
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-17 09:02:43 -0700

    Finished setting up 3 reducers (FETCHING, FETCHED, ERROR)
    Still need to add all the other reducers and actions but I want to achieved functionality first.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,22 @@
    U - updateSmurf
    D - deleteSmurf
 */
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+
+export const fetching_data = () => {
+    const endpoint = axios.get('http://localhost:3333/smurfs');
+    return (dispatch) => {
+        dispatch({ type: FETCHING});
+
+        endpoint.then((response) => {
+            dispatch({ type: FETCHED, payload: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: ERROR, payload: err })
+        });
+    }
+}
