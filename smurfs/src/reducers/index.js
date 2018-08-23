0e7974df2aafa6b72bfadfe76c6f761b53d8e921
odax <nr10@albion.edu>
2018-08-23 16:53:45 -0400

    added export RootReducer in reducer index.js and imported to src index.js

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -14,6 +14,10 @@
  }
 */
 
+const RootReducer = () => {
+
+}
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +25,5 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default RootReducer;
