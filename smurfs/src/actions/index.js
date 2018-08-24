88c486aab7e1ab176ed2aca4fbae4352e3cbeffa
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-24 13:30:43 -0400

    -added api calls

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -22,7 +22,34 @@ export const fetchSmurfs = () => {
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+  export const fetchSmurfs = () => {
+  const getSmurfs = axios.get(`http://localhost:3333/smurfs`);
+  return function(dispatch) {
+    dispatch({ type: FETCHING_SMURF });
+    getSmurfs
+      .then( response => {
+        console.log('GET repsonse', response)
+        dispatch({ type: SMURF_FETCHED, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  }
+}
+ export const addSmurf = data => {
+  const postSmurf = axios.post(`http://localhost:3333/smurfs`, data);
+  return function(dispatch) {
+    dispatch({ type: PENDING });
+    postSmurf
+      .then( response => {
+        console.log('POST repsonse', response)
+        dispatch({ type: SUCCESS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  }
+}
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
