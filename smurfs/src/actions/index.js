4d4f01a1c14149af6a58413e435b57c991f65006
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-08-24 11:50:18 -0500

    added actions content, added initial state in reducers, added some beginning statements in app

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,13 @@
+import axios from 'axios';
+
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const FETCH_SMURFS = 'FETCH_SMURFS';
+export const FETCHED_SMURFS = 'FETCHED_SMURFS';
+export const ERROR = 'ERROR';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +19,16 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+  return dispatch => {
+    dispatch({type: FETCH_SMURFS})
+    axios.get('http://localhost:333/smurfs')
+    .then(response => {
+      console.log(response)
+      dispatch({ typed: FETCHED_SMURFS, payload: response.data})
+    })
+    .catch (error => dispatch({type: ERROR, error: error}))
+  }
+}
+
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -19,4 +20,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {{
+
+}};
+
+export default connect(mapStateToProps, {})(App);
