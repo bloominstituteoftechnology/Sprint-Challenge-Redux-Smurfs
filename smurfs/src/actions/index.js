d5bf722999de16ed369aa3a90d6f4b74d7409441
rverdi642 <rverdi642@gmail.com>
2018-08-24 12:31:35 -0500

    complete except for stretch

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,45 @@
+import axios from 'axios';
+
+export const GET_SMURFS = 'GET_SMURFS';
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const ADDING_SMURF = 'ADDING_SMURF'
+export const ADD_SMURF = 'ADD_SMURF';
+export const ERROR = 'ERROR';
+
+
+const URL = 'http://localhost:3333/smurfs';
+
+
+export const getSmurfs = () => {
+  const smurfs = axios.get(`${URL}`);
+  return dispatch => {
+    dispatch({type: GETTING_SMURFS});
+    smurfs.then(response => {
+      dispatch({type: GET_SMURFS, payload: response.data});
+    })
+    .catch(err => {
+      dispatch({type: ERROR, payload: err});
+    });
+  };
+};
+
+
+export const addSmurf = smurf => {
+  const newSmurf = axios.post(`${URL}`,smurf);
+  console.log(newSmurf);
+  return dispatch => {
+    dispatch({type:ADDING_SMURF});
+    newSmurf
+    .then(response => {
+      dispatch({type: ADD_SMURF , payload: response.data});
+    })
+    .catch(err => {
+      dispatch({type: ERROR, payload: err});
+    });
+  };
+};
+
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
