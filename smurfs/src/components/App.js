3085ccf9c5d374bcf4b21614de3c3bc9bd16eecb
Synch3 <jonsimo@aol.com>
2018-08-24 12:25:37 -0400

    Don't know why its not working, going to refactor

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { fetchingSmurfs } from '../actions';
+import { Route } from 'react-router-dom';
+import SmurfForm from './SmurfForm';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +11,27 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchingSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <Route exact path = '/smurfs' component={SmurfForm} />
       </div>
     );
   }
 }
 
-export default App;
+let mapStateToProps = state => {
+  return {
+    name: state.name,
+    age: state.age,
+    height: state.height
+  };
+};
+
+
+export default connect(mapStateToProps, { fetchingSmurfs })(App);
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,16 +6,17 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import rootReducer './reducers';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
+  rootReducer,
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router><App /></Router>
   </Provider>,
   document.getElementById('root')
 );
