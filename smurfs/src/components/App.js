bc6ff0e5cd546f5e805a471ddf73fca176acfc77
bri79319 <bri79319@gmail.com>
2018-08-24 13:43:59 -0400

    trying to fix bug

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,23 +3,29 @@ import "./App.css";
 import { connect } from "react-redux";
 import { getSmurfs, postSmurfs } from "../actions";
 
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
-
 class App extends Component {
   state = {
     name: "",
     age: "",
-    height: "",
+    height: ""
   };
 
   componentDidMount() {
     this.props.getSmurfs();
   }
+
+  handleInputChanger = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = () => {
+    this.props.postSmurfs({
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    });
+    this.setState({ name: "", age: "", height: "" });
+  };
 
   render() {
     return (
@@ -28,6 +34,30 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <div>
+          <input
+          onChange={this.handleInputChanger}
+          name="name"
+          type="text"
+          value={this.state.name}
+          placeholder="Name" 
+          />
+          <input
+          onChange={this.handleInputChanger}
+          name="age"
+          type="text"
+          value={this.state.age}
+          placeholder="Age" 
+          />
+          <input
+          onChange={this.handleInputChanger}
+          name="height"
+          type="text"
+          value={this.state.height}
+          placeholder="Height" 
+          />
+          <button onClick={this.handleSubmit}>Create New Smurf</button>
+        </div>
         {this.props.smurfs.map(smurf => {
           return (
             <div>
@@ -49,8 +79,11 @@ const mapStateToProps = state => {
   return {
     smurfs: state.smurfs,
     fetchingSmurfs: state.fetchingSmurfs,
-    postingSmurfs: state.postingSmurfs,
+    postingSmurfs: state.postingSmurfs
   };
 };
 
-export default connect(mapStateToProps,{getSmurfs, postSmurfs})(App);
+export default connect(
+  mapStateToProps,
+  { getSmurfs, postSmurfs }
+)(App);
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,9 +6,9 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from "./reducers";
+// import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
