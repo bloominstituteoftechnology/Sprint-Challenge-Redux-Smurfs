052c8d6266a2ce1b832fd1acbb7e873bde5c240e
maribelcuales <maribelcuales@gmail.com>
2018-08-27 16:19:30 +0800

    added reducer and set store and middleware in src/index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import smurfsReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';  // this is the function that will take in reducer/s which will represent our state of application globally
+import { Provider } from 'react-redux';  // the provider component provides the application with the store that will contain the state data for the react application
+import smurfReducer from './reducers'; // reducer
 
-const store = createStore (smurfsReducer, applyMiddleware(thunk, logger));
+//add reducer and call middleware here
+const store = createStore(smurfReducer, applyMiddleware(thunk, logger));
 
+// wrap our <App /> with the <Provider> component and pass it a store property set to equal to the createStore function
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
+  <Provider store={store}> 
+    <App /> 
   </Provider>,
   document.getElementById('root')
 );
