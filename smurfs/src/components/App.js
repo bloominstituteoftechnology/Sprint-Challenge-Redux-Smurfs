cbf015b82aedcc9431ad6f5b4212ea3ce1f2c3f6
Luisan <luisangelsc@gmail.com>
2018-09-21 09:59:29 -0600

    boilerplate + get request

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import { fetchSmurfs } from '../actions'
+import { connect } from 'react-redux'
 /*
  to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
+ I'll let you do this part on your own.
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchSmurfs()
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,4 +27,12 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStatetoProps = state => {
+  return {
+    state,
+  }
+}
+
+export default connect(mapStatetoProps,
+  {fetchSmurfs})
+  (App)
