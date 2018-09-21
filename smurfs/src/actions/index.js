30f9142f4fa83c38ca699d90bcf2d3d72739fb5a
Max <maxkajiwara@gmail.com>
2018-09-21 09:45:28 -0700

    Build actions/index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,3 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
-
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +8,86 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+
+// getSmurfs
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const GOT_SMURFS = 'GOT_SMURFS';
+export const GET_SMURFS_ERROR = 'GET_SMURFS_ERROR';
+//addSmurf
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADDED_SMURF = 'ADDED_SMURF';
+export const ADD_SMURF_ERROR = 'ADD_SMURF_ERROR';
+// deleteSmurf
+export const DELETING_SMURF = 'DELETING_SMURF';
+export const DELETED_SMURF = 'DELETED_SMURF';
+export const DELETE_SMURF_ERROR = 'DELETE_SMURF_ERROR';
+// editSmurf
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const UPDATED_SMURF = 'UPDATED_SMURF';
+export const UPDATE_SMURF_ERROR = 'UPDATE_SMURF_ERROR';
+
+export const getSmurfs = () => {
+	return dispatch => {
+		dispatch({ type: GETTING_SMURFS });
+
+		axios
+			.get('http://localhost:3333/smurfs')
+
+			.then(response => {
+				dispatch({ type: GOT_SMURFS, payload: response.data });
+			})
+
+			.catch(error => dispatch({ type: GET_SMURFS_ERROR, payload: error }));
+	};
+};
+
+export const addSmurf = smurf => {
+	return dispatch => {
+		dispatch({ type: ADDING_SMURF });
+
+		axios
+			.post('http://localhost:3333/smurfs', smurf)
+
+			.then(response => {
+				dispatch({ type: ADDED_SMURF, payload: response.data });
+			})
+
+			.catch(error => dispatch({ type: ADD_SMURF_ERROR, payload: error }));
+	};
+};
+
+export const deleteSmurf = id => {
+	return dispatch => {
+		dispatch({ type: DELETING_SMURF });
+
+		axios
+			.delete(`http://localhost:3333/smurfs/${id}`)
+
+			.then(response => {
+				console.log(response.data);
+				dispatch({ type: DELETED_SMURF, payload: response.data });
+			})
+
+			.catch(error => dispatch({ type: DELETE_SMURF_ERROR, payload: error }));
+	};
+};
+
+export const editSmurf = updatedSmurf => {
+	return dispatch => {
+		dispatch({ type: UPDATING_SMURF });
+
+		axios
+			.put(`http://localhost:3333/smurfs/${updatedSmurf.id}`, {
+				updatedSmurf
+			})
+
+			.then(response => {
+				console.log(response);
+				dispatch({ type: UPDATED_SMURF, payload: response.data });
+			})
+
+			.catch(error => dispatch({ type: UPDATE_SMURF_ERROR, payload: error }));
+	};
+};
