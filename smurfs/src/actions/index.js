0192f69d5a14dc41790f1bbe06b8a4afea0ef785
lyang9 <lyang9@buffalo.edu>
2018-09-21 12:25:47 -0400

    Completed actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,13 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCHED_SMURFS = 'FETCHED_SMURFS';
+export const SAVING_SMURFS = 'SAVING_SMURFS';
+export const SAVED_SMURFS = 'SAVED_SMURFS';
+export const ERROR = 'ERROR';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +18,29 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: FETCHING_SMURFS });
+    const promise = axios.get('http://localhost:3333/smurfs')
+    promise 
+      .then(response => {
+        dispatch({ type: FETCHED_SMURFS, payload: response.data });
+      })
+      .catch(error => {
+        dispatch({ type: ERROR, payload: error });
+      });
+  };
+};
+
+export const saveSmurf = smurf => {
+  return dispatch => {
+    dispatch({ type: SAVING_SMURFS })
+    axios.post('http://localhost:3333/smurfs', smurf)
+      .then(response => {
+        dispatch({ tpye: SAVED_SMURFS, payload: response.data });
+      })
+      .catch(error => {
+        dispatch({ type: ERROR, payload: error });
+      });
+  };
+};
