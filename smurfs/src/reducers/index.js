9097ac226fad884a9ce3ff2f6ad9cbf36972dfaf
Clint Kunz <clintkunz@gmail.com>
2018-09-21 09:30:54 -0600

    reducers set up

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,42 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { FETCHING_SMURF, SMURF_FETCHED, FETCH_ERROR, ADDING_SMURF, SMURF_ADDED } from '../actions';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+ */
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
- }
-*/
+}
+
+export default rootReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case FETCHING_SMURF:
+      return Object.assign({}, state, {fetchingSmurfs:true});
+    case SMURF_FETCHED:
+      return Object.assign({}, state, {
+        smurfs: [...state.smurfs, ...action.payload],
+        fetchingSmurfs: false
+      });
+    case FETCH_ERROR:
+      return Object.assign({}, state, {
+        fetchingSmurfs: false,
+        error: "Error fetching smurfs"
+      });
+    case ADDING_SMURF:
+      return {...state, addingSmurf: true };
+    case SMURF_ADDED:
+      return {...state, addingSmurf: false, smurfs: action.payload };
+    default:
+      return state;
+  }
+}
 
 /*
   You'll only need one smurf reducer for this project.
