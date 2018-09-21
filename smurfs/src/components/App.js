568e3c318dc19b391b2e627ceb1170878f00acc7
Michael Agard <mdagard@gmail.com>
2018-09-21 12:30:58 -0400

    add mapStateToProps and componentDidMount with imports

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import SmurfList from './SmurfList';
+import SmurfForm from './SmurfForm';
+import { getSmurfs } from '../actions';
+
 import './App.css';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +14,26 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+    console.log(`Current State on App.js ${this.props.smurfs}`);
+  }
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfList smurfs={this.props.smurfs} />
+        <SmurfForm />
+        <p>Hello World!</p>
       </div>
     );
   }
 }
-
-export default App;
+// export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs,
+  fetching: state.fetching
+});
+export default connect(
+  mapStateToProps,
+  { getSmurfs }
+)(App);
