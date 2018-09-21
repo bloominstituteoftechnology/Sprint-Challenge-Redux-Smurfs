ca5411e9ffc4ee4baf6aee9c01074083e1420c36
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-09-21 10:42:23 -0500

    Built the actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,62 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
+export const FETCH = 'FETCH';
+export const FETCHED = 'FETCHED';
+export const ADDING = 'ADDING';
+export const ADDED = 'ADDED';
+export const ERROR = 'ERROR';
+export const REMOVED = 'REMOVED';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const get = () => {
+  return dispatch => {
+    dispatch({ type: FETCH });
+    axios
+      .get('http://localhost:3333/smurfs')
+
+      .then(response => {
+        dispatch({ type: FETCHED, payload: response.data });
+      })
+
+      .catch(error => {
+        console.log(error);
+        dispatch({ type: ERROR });
+      });
+  };
+};
+
+export const add = smurf => {
+  return dispatch => {
+    dispatch({ type: ADDING });
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+
+      .then(
+        response =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve(dispatch({ type: ADDED, payload: response.data }));
+            }, 1000);
+          })
+      )
+
+      .catch(error => {
+        console.log(error);
+        dispatch({ type: ERROR });
+      });
+  };
+};
+
+export const removeSmurf = id => {
+  return dispatch => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+
+      .then(response => {
+        dispatch({ type: REMOVED, payload: response.data });
+      })
+
+      .catch(error => {
+        console.log(error);
+        dispatch({ type: ERROR });
+      });
+  };
+};
