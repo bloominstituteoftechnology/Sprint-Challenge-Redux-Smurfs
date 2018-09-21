664c2a8d9e8bf51a3858e7aae4edb6d4edc44a38
David Layman <davidlayman85@gmail.com>
2018-09-21 12:02:58 -0400

    david layman sprint-redux-smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { get,post } from '../actions/index';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +9,65 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
-  render() {
+constructor(props){
+super(props);
+this.state={
+name:'',
+age:'',
+height:''
+}
+}
+componentDidMount()
+{
+this.props.get();
+}
+onChange=(e)=>{
+this.setState({
+	[e.target.placeholder]:e.target.value
+	})
+}
+
+postSmurf=(e)=>{
+e.preventDefault();
+if(this.state.name && this.state.age && this.state.height){
+this.props.post({name:this.state.name,age:this.state.age,height:this.state.height});
+}
+else alert('Fill out yer smurfin\' fields! That means all of them!');
+}
+
+render(props) {
     return (
       <div className="App">
+	  
+	  <form>
+        <input onChange={this.onChange} type="text" placeholder="name" value={this.state.name}/>
+        <input onChange={this.onChange} type="text" placeholder="age" value={this.state.age}/>
+        <input onChange={this.onChange} type="text" placeholder="height" value={this.state.height}/>
+        <button type="submit" onClick={this.postSmurf}>Submit</button>
+      </form>
+	  
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+		{this.props.fetchingSmurfs ? (
+		
+		<div>loading...</div>
+		) : (
+		<div>
+		{this.props.smurfs.map(smurf => {
+		return(<div key={smurf.name}>{smurf.name}: age {smurf.age}</div>)
+		})}
+		</div>
+		)
+		}
       </div>
     );
   }
 }
 
-export default App;
+const Map = (props) => {
+return({
+smurfs:props.smurfs,
+fetchingSmurfs:props.fetchingSmurfs
+});
+}
+
+export default connect(Map,{get,post})(App);
