399244a4d528cbd1574413b261a0ad3ff1c71d53
Keiran <keirankozlowski@gmail.com>
2018-09-21 12:35:43 -0400

    Set up Smurf and SmurfForm components.

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -3,3 +3,32 @@ import { connect } from 'react-redux';
 
 import { fetchSmurfs } from '../actions';
 
+class Smurfs extends Component {
+    render() {
+        return(
+            <div>
+            {this.props.smurfs.map(smurf => {
+                return(
+                    <div key={`${smurf.name} ${smurf.id}`}>
+                        {smurf.name}
+                        {smurf.age}
+                        {smurf.height}
+                    </div> 
+                )
+            })}
+        </div>
+        )
+    }
+
+    componentDidMount() {
+        this.props.fetchSmurfs();
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs
+    }
+}
+
+export default connect(mapStateToProps, fetchSmurfs)(Smurfs);
