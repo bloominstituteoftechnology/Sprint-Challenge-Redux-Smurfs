c13e8596d041ff642a1d1d5e4dc8199405f1ab24
Keiran <keirankozlowski@gmail.com>
2018-09-21 11:47:50 -0400

    Set up actions, fixed import statement in index.js and reducer/index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,13 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const FETCH_SMURFS = "FETCH_SMURFS";
+export const FETCHING_SMURFS = "FETCHING_SMURFS";
+export const ADD_SMURF = "ADD_SMURF";
+export const ADDING_SMURF = "ADDING_SMURF";
+export const ERROR = "ERROR";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +20,45 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: FETCHING_SMURFS });
+
+    axios
+      .get('http://localhost:3333')
+      .then(response => {
+        dispatch({
+          type: FETCH_SMURFS,
+          payload: response.data.results
+        });
+      })
+      .catch(error => {
+        dispatch({
+          type: ERROR,
+          payload: error
+        });
+      })
+  }
+}
+
+export const addSmurf = (smurf) => {
+  const newSmurf = axios.post('http://localhost:3333', smurf);
+
+  return dispatch => {
+    dispatch({ type: ADDING_SMURF });
+
+    newSmurf
+      .then(({data}) => {
+        dispatch({
+          type: ADD_SMURF,
+          payload: data
+        });
+      })
+      .catch(error => {
+        dispatch({
+          type: ERROR,
+          payload: error
+        });
+      })
+  }
+}
