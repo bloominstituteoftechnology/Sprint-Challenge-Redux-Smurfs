86f7771a8f0665d9ea9c0db46ef489a69adcf876
BrianARuff <thelonewolf198927@gmail.com>
2018-09-21 12:09:48 -0400

    MVP done

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,32 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+
+export const FETCHING_SMURFS = "FETCHING_SMURFS";
+export const FETCHING_SMURFS_SUCCESSFUL = "FETCHING_SMURFS_SUCCESSFUL";
+export const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE";
+export const POSTING_SMURF = "POSTING_SMURF";
+export const POSTING_SMURF_SUCCESSFUL = "POSTING_SMURF_SUCCESSFUL";
+export const POSTING_SMURF_FAILURE = "POSTING_SMURF_FAILURE";
+
+export const getSmurfs = () => {
+  return dispatch => {
+    dispatch({type: FETCHING_SMURFS})
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(resp => dispatch({type: FETCHING_SMURFS_SUCCESSFUL, payload: resp.data}))
+      .catch(err => dispatch({type: FETCHING_SMURFS_FAILURE, payload: err}));
+  }
+}
+
+export const addSmurf = (smurf) => {
+  return dispatch => {
+    dispatch({type: POSTING_SMURF});
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(resp => dispatch({type: POSTING_SMURF_SUCCESSFUL, payload: resp.data}))
+      .catch(err => dispatch({type: POSTING_SMURF_FAILURE, payload: err}));
+  }
+}
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import {getSmurfs} from '../actions';
+import {connect} from 'react-redux';
+import SmurfList from '../components/SmurfList';
+import AddSmurfForm from './AddSmurfForm';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +11,30 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <AddSmurfForm />
+        <SmurfList smurfs={this.props.smurfs} />
       </div>
     );
   }
 }
 
-export default App;
+
+
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs,
+    addingSmurf: state.addingSmurf,
+    updatingSmurf: state.updatingSmurf,
+    deletingSmurf: state.deletingSmurf,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps, {getSmurfs})(App);
