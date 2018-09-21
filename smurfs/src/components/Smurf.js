fab7649b0a44feadb916beae068756c2d1d30154
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-21 11:30:22 -0400

    Created Smurf List Component

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import { fetchSmurfs } from '../actions';
+import { fetchSmurfs } from "../actions";
 
 function Smurf(props) {
-    if (!props.smurfs || !props.smurfs.length) {
-        return <h1>Gargamel has stolen your smurfs! :( Go fetch them!</h1>
-    }
-    return <div className="smurfs-list-wrapper">
-            <div className="smurf-card" key={props.smurf.id}>
-                {props.smurf.name}
-                {props.smurf.age}
-                {props.smurf.email}
-                <div>X</div>
-            </div>
-        ))}
-    </div>;
+  return (
+    <div className="smurf-card" key={props.smurf.id}>
+      {props.smurf.name}
+      {props.smurf.age}
+      {props.smurf.email}
+      <div>X</div>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
-    smurfs: state.smurfs
+  smurfs: state.smurfs
 });
 
 export default connect(
-    mapStateToProps,
-    { fetchSmurfs }
+  mapStateToProps,
+  { fetchSmurfs }
 )(Smurf);
---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { fetchSmurfs } from "../actions";
+import Smurf from "./Smurf";
+
+function SmurfList(props) {
+  if (!props.smurfs || !props.smurfs.length) {
+    return <h1>>Gargamel has stolen your smurfs! :( Go fetch them!</h1>;
+  }
+  return (
+    <div className="smurfs-list-wrapper">
+      {props.smurfs.map(smurf => (
+        <Smurf smurf={smurf} key={smurf.id} />
+      ))}
+    </div>
+  );
+}
+
+const mapStateToProps = state => ({
+  smurfs: state.smurfs
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchSmurfs }
+)(Smurfs);
