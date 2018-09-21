39f4d578284655e78d35e4a201c79e4015d980e1
Keiran <keirankozlowski@gmail.com>
2018-09-21 11:29:33 -0400

    Set up middleware/imports in index.js, set up initial skeleton of reducer/index.js

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import * from '../actions';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -14,6 +15,15 @@
  }
 */
 
+const initialState =  {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  // updatingSmurf: false,
+  // deletingSmurf: false,
+  error: null
+}
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +31,40 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export const rootReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case "FETCH_SMURFS":
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false
+      }
+    case "FETCHING_SMURFS":
+      return {
+        ...state,
+        fetchingSmurfs: true
+      }
+    case "ADD_SMURF":
+      return {
+        ...state,
+        smurfs: action.payload,
+        addingSmurf: false
+      }
+    case "ADDING_SMURF":
+      return {
+        ...state,
+        addingSmurf: true
+      }
+    case "ERROR":
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        addingSmurf: false,
+        error: action.payload
+      }
+
+    default:
+      return state;
+  }
+}
