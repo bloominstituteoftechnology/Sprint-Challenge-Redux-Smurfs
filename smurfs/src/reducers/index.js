ba155823ccc5cedaed4cd59d8891ff4dca8f2972
decagondev <decagondevelopment@gmail.com>
2018-09-21 19:20:54 +0100

    (STRETCH) : added edit and delete cases in to the recuders

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -16,10 +16,17 @@
 
 const initialState = {
   smurfs: [],
+  smurf: null,
   fetchingSmurfs: false,
+  smurfsFetched: false,
+  fetchingSmurf: false,
+  smurfFetched: false,
   addingSmurf: false,
-  updatingSmurf: false,
+  smurfAdded: false,
+  editingSmurf: false,
+  smurfEdited: false,
   deletingSmurf: false,
+  smurfDeleted: false,
   error: null
 };
 
@@ -31,41 +38,43 @@ const initialState = {
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case "FETCHING_SMURFS":
-      return Object.assign({}, state, { fetchingSmurfs: true });
+  
+  switch(action.type) {
+    case 'FETCHING_SMURFS':
+       return Object.assign({}, state, { fetchingSmurfs: true });
 
-    case "SMURFS_FETCHED":
-      return Object.assign({}, state, {
-        smurfs: action.payload,
-        smurfsFetched: true,
-        fetchingSmurfs: false
-      });
+   case 'SMURFS_FETCHED':
+     return Object.assign({}, state, { smurfs: action.payload, smurfsFetched: true, fetchingSmurfs: false });
 
-    case "FETCHING_SMURF":
-      return Object.assign({}, state, { fetchingSmurf: true });
+   case 'ADDING_SMURF':
+     return Object.assign({}, state, { addingSmurf: true });
 
-    case "SMURF_FETCHED":
-      return Object.assign({}, state, {
-        smurf: action.payload,
-        smurfFetched: true,
-        fetchingSmurf: false
-      });
+   case 'SMURF_ADDED':
+     return Object.assign({}, state, { smurfs: action.payload, smurfAdded: true, addingSmurf: false });
 
-    case "ADDING_SMURF":
-      return Object.assign({}, state, { addingSmurf: true });
+   case 'FETCHING_SMURF':
+     return Object.assign({}, state, { fetchingSmurf: true });
 
-    case "SMURF_ADDED":
-      return Object.assign({}, state, {
-        smurfs: action.payload,
-        smurfAdded: true,
-        addingSmurf: false
-      });
+   case 'SMURF_FETCHED':
+     return Object.assign({}, state, { smurf: action.payload, smurfFetched: true, fetchingSmurf: false });
 
-    case "ERROR":
-      return Object.assign({}, state, { error: action.payload });
+   case 'EDITING_SMURF':
+     return Object.assign({}, state, { editingSmurf: true });
+
+   case 'SMURF_EDITED':
+     return Object.assign({}, state, { smurfs: action.payload, smurf: action.payload.find(smurf => smurf.id === state.smurf.id), smurfEdited: true, editingSmurf: false });
+
+   case 'DELETING_SMURF':
+     return Object.assign({}, state, { deletingSmurf: true });
+
+   case 'SMURF_DELETED':
+     return Object.assign({}, state, { smurfs: action.payload, smurf: null, smurfDeleted: true, deletingSmurf: false });
+
+
+   case 'ERROR':
+     return Object.assign({}, state, { error: action.payload });
 
     default:
-      return state;
+     return state;
   }
 };
