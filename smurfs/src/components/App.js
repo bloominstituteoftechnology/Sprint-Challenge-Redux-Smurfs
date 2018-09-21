1fd6b4c7015076830e2da5be3d07d56e4bda76df
decagondev <decagondevelopment@gmail.com>
2018-09-21 19:10:45 +0100

    (MVP) refactored app and other components got it reading smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,75 +1,98 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getSmurfs, addSmurf } from '../actions';
 import './App.css';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import Header from './Header';
 import Form from './Form';
 import SmurfsList from './SmurfsList';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import Smurf from './Smurf';
+import { getSmurfs, addSmurf } from '../actions';
+
 class App extends Component {
-    state = {
+  state = {
+    name: '',
+    age: '',
+    height: ''
+  }
+
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
+  handleSmurfSubmit = e => {
+    e.preventDefault();
+
+    const newSmurf = { name: this.state.name,
+                       age: this.state.age,
+                       height: this.state.height };
+
+    this.props.addSmurf(newSmurf);
+
+    this.setState({
       name: '',
       age: '',
       height: ''
-    }
+    });
 
-    //handler for input change
-    handleInputChange = e => {
-      this.setState({ [e.target.name]: e.target.value });
-    };
+    this.props.history.push('/');
+  }
 
-    // smufr submit handler
-    handleSmurfSubmit = e => {
-      e.preventDefault();
-  
-      const newSmurf = { name: this.state.name,
-                         age: this.state.age,
-                         height: this.state.height };
-  
-      this.props.addSmurf(newSmurf);
-  
-      this.setState({
-        name: '',
-        age: '',
-        height: ''
-      });
-  
-      this.props.history.push('/');
-    }
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    // the cancel handler
-    handleCancel = e => {
-      e.preventDefault();
-  
-      this.setState({
-        name: '',
-        age: '',
-        height: ''
-      });
-  
-      this.props.history.push('/');
-    }
+  handleCancel = e => {
+    e.preventDefault();
+
+    this.setState({
+      name: '',
+      age: '',
+      height: ''
+    });
+
+    this.props.history.push('/');
+  }
+
   render() {
+    if (this.props.fetchingSmurfs) {
+      return (
+        <div>Loading smurfs...</div>
+      )
+    }
+
     return (
-      <div className="App">
+      <div className="container">
         <Header />
-        <Form />
+
         <Route exact path="/" render={ props =>
             <SmurfsList smurfs={this.props.smurfs} />
           }
         />
+
+        <Switch>
+
+          <Route path="/smurfs/add" render={ props =>
+            <Form name={this.state.name}
+                         age={this.state.age}
+                         height={this.state.height}
+                         handleSmurfSubmit={this.handleSmurfSubmit}
+                         handleInputChange={this.handleInputChange}
+                         handleCancel={this.handleCancel} />
+            }
+          />
+
+          <Route path="/smurfs/:id" render={ props =>
+             <Smurf {...props} />
+            }
+          />
+
+        </Switch>
+
       </div>
     );
   }
 }
 
-// created the mapStateToProps function
 const mapStateToProps = (state) => {
   return {
     smurfs: state.smurfs,
@@ -77,5 +100,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-// exported withRouter and connect using the mapStateToProps function
 export default withRouter(connect(mapStateToProps, { getSmurfs, addSmurf })(App));
