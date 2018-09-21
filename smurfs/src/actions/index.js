67f2bf42b3f084555e9e0005a6d3a950e1d62dbd
David Benavidez <benavidez_david13@yahoo.com>
2018-09-21 10:51:25 -0700

    finished with mvp

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,15 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+export const FETCH = 'FETCH';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_ERROR = 'FETCH_ERROR';
+export const ADD = 'ADD';
+export const ADD_SUCCESS = 'ADD_SUCCESS';
+export const ADD_ERROR = 'ADD_ERROR';
+
+
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +22,34 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurf = () => {
+  return dispatch => {
+
+      dispatch({ type: FETCH});
+      axios
+          .get('http://localhost:3333/smurfs')
+          .then(response => {
+              dispatch({ type: FETCH_SUCCESS, payload: response.data});
+          })
+          .catch( err => {
+              console.log(err);
+              dispatch({ type: FETCH_ERROR });
+          });
+  };
+};
+
+
+export const addSmurf = (smurf) => {
+  return dispatch => {
+      dispatch({ type: ADD});
+      axios.post('http://localhost:3333/smurfs',smurf)
+          .then(response => {
+              dispatch({ type: ADD_SUCCESS, payload: response.data});
+          })
+          .catch( err => {
+              console.log(err);
+              dispatch({ type: ADD_ERROR });
+          });
+  };
+};
+
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import { connect } from 'react-redux';
+import '../styles/App.css';
+import { fetchSmurf } from '../actions';
+import SmurfForm from './SmurfForm';
+
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchSmurf();
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {this.props.fetching ? (
+          <h1>Loading...</h1>
+        ) : (
+            <div>
+              {this.props.smurfs.map(smurfs => {
+                return (
+
+                  <div key={smurfs.name}>
+                    <h3>Name: {smurfs.name} </h3>
+                    <h3>Age: {smurfs.age} </h3>
+                    <h3>Height: {smurfs.height} </h3>
+                    <SmurfForm  />
+
+                  </div>
+
+                )
+
+              })}
+            </div>
+          )}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    error: state.error,
+    fetching: state.fetching,
+    adding: state.adding,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    fetchSmurf,
+  }
+)(App);
