f5daab0d8c8d21d0f50aa8597b443ab8af68951f
JeanetteFernandez <jeanfern555@gmail.com>
2018-09-21 12:40:08 -0500

    Get and Post Completed - there's access to the list and smurfs are able to be added using react/redux - MVP complete.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,13 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const SMURFS_FETCHED = 'SMURFS_FETCHED';
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const SMURF_ADDED = 'SMURF_ADDED';
+export const ERROR = 'ERROR';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +20,34 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurfs = () => dispatch => {
+  dispatch ({type: FETCHING_SMURFS});
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      // console.log('axios fetch response:', response);
+      dispatch({type: SMURFS_FETCHED, payload: response.data })
+    })
+    .catch(error => {
+      // console.log('axios fetch error:', error);
+      dispatch({type: ERROR, payload: error})
+    })
+};
+
+export const addSmurf = smurf => dispatch => {
+  dispatch ({type: ADDING_SMURF});
+  axios 
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(response => {
+      // console.log('axios adding response:', response);
+      dispatch({type: SMURF_ADDED, payload: response.data})
+    })
+    .catch(error => {
+      // console.log('axios adding error:', error);
+      dispatch({type: ERROR, payload: error})
+    })
+};
+
+
+
+
