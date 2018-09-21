0c43ff8b78a83a0a8c505572f74f9caf19e22010
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-21 11:35:27 -0400

    Created Actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,45 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+export const ADDING_SMURF = "ADDING_SMURF";
+export const SMURF_ADDED = "SMURF_ADDED";
+export const ADD_SMURF_ERROR = "ADD_SMURF_ERROR";
+
+
+
+
+
+export const fetchSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: FETCHING });
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        console.log(response);
+        dispatch({ type: FETCHED, payload: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
+
+export const addNewFriend = smurf => dispatch => {
+  dispatch({ type: ADDING_SMURF });
+
+  axios
+    .post("http://localhost:3333/smurfs", smurf)
+    .then(response => {
+      dispatch({ type: SMURF_ADDED, payload: response.data });
+    })
+    .catch(err => {
+      dispatch({ type: ADD_SMURF_ERROR, payload: err });
+    });
+};
