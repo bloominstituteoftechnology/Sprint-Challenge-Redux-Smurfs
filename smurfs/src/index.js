4d0a9b8c35305da5132d8fc6628fa78eea0cd8b8
Anthony Greb <agreb255@gmail.com>
2018-09-21 13:28:05 -0400

    getting more errors

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,19 +4,18 @@ import "./index.css";
 import App from "./components/App";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
 
-const reduxDevTools =
-  window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_();
+// const reduxDevTools =
+//   window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_();
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk, logger),
-    reduxDevTools
-  ) // this is the most basic reducer. A function that returns and object. Replace it.
+  applyMiddleware(thunk, logger)
+  // reduxDevTools
+  // this is the most basic reducer. A function that returns and object. Replace it.
   // applyMiddleware( be sure to throw in the proper middlewares here )
 );
 
