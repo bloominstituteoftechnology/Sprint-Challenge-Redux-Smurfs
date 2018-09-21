9af15181383be4859dc9c85692093102035eda38
decagondev <decagondevelopment@gmail.com>
2018-09-21 18:55:52 +0100

    (MVP) created added the Header component to the app component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getSmurfs, addSmurf } from '../actions';
 import './App.css';
+import Header from './Header';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -20,7 +21,26 @@ class App extends Component {
     handleInputChange = e => {
       this.setState({ [e.target.name]: e.target.value });
     };
-    
+
+    // smufr submit handler
+    handleSmurfSubmit = e => {
+      e.preventDefault();
+  
+      const newSmurf = { name: this.state.name,
+                         age: this.state.age,
+                         height: this.state.height };
+  
+      this.props.addSmurf(newSmurf);
+  
+      this.setState({
+        name: '',
+        age: '',
+        height: ''
+      });
+  
+      this.props.history.push('/');
+    }
+
     // the cancel handler
     handleCancel = e => {
       e.preventDefault();
@@ -36,7 +56,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        REDUX WORKING
+        <Header />
       </div>
     );
   }
