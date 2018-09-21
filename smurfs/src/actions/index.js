3185316026e44aa0db20b63fa5fba8cdcfdea5c5
unknown <cornielleandres@gmail.com>
2018-09-21 11:37:53 -0400

    Get redux hooked up and working.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,32 @@
+// Dependencies
+import axios from 'axios';
 /* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
+	Action Types Go Here!
+	Be sure to export each action type so you can pull it into your reducer
 */
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const GETTING_SMURFS_COMPLETE = 'GETTING_SMURFS_COMPLETE';
+export const GETTING_SMURFS_ERROR = 'GETTING_SMURFS_ERROR';
 
 /*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
+	For this project you'll need at least 2 action creators for the main portion,
+	and 2 more for the stretch problem.
+	Be sure to include action types for each type of action creator. Also, be sure to mind
+		the "pending" states like, fetching, creating, updating and deleting.
+	C - addSmurf
+	R - getSmurfs
+	U - updateSmurf
+	D - deleteSmurf
 */
+
+export const getSmurfs = () => dispatch => {
+	dispatch({ type: GETTING_SMURFS });
+
+	axios
+		.get('http://localhost:3333/smurfs')
+		.then(res => console.log('res', res))
+		.catch(err => {
+			console.log(err);
+			dispatch({ type: GETTING_SMURFS_ERROR });
+		})
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,49 @@
 /*
-  Be sure to import in all of the action types from `../actions`
+	Be sure to import in all of the action types from `../actions`
 */
-
+// Actions
+import {
+	GETTING_SMURFS,
+	GETTING_SMURFS_COMPLETE,
+	GETTING_SMURFS_ERROR,
+} from '../actions';
 /*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
+	Your initial/default state for this project could *Although does not have to* look a lot like this
+	{
+	smurfs: [],
+	fetchingSmurfs: false
+	addingSmurf: false
+	updatingSmurf: false
+	deletingSmurf: false
+	error: null
+	}
 */
+const initialState = {
+	smurfs: [],
+};
 
+const smurfReducer = (state = initialState, action) => {
+	switch(action.type) {
+		case GETTING_SMURFS: {
+			return state;
+		}
+		case GETTING_SMURFS_COMPLETE: {
+			return state;
+		}
+		case GETTING_SMURFS_ERROR: {
+			return state;
+		}
+		default: {
+			return state;
+		}
+	}
+};
+
+export default smurfReducer;
 /*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
+	You'll only need one smurf reducer for this project.
+	Feel free to export it as a default and import as rootReducer. 
+	This will guard your namespacing issues.
+	There is no need for 'combineReducers' in this project.
+	Components can then read your store as, `state` and not `state.fooReducer`.
 */
