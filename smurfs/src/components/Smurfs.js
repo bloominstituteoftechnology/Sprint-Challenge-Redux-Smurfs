060751556606c1bd377f4dbf9bb024391d82e669
Anthony Greb <agreb255@gmail.com>
2018-09-21 14:58:04 -0400

    almost

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,8 +1,29 @@
-// import React, { Component } from 'react';
+import React, { Component } from "react";
 // import './App.css';
-// import SelectedSmurf from './SelectedSmurf';
+import SelectedSmurf from "./SelectedSmurf";
 // import { connect } from 'react-redux';
 
-// class Smurfs extends Component {
+class Smurfs extends Component {
+  handleShowSmurf = smurf => {
+    this.props.updateSingleSmurf(smurf);
+  };
 
-// }
+  render() {
+    return (
+      <div className="Smurf-List">
+        {/* <h1>My Smurfs</h1> */}
+        {this.props.smurfs.map(smurf => {
+          return (
+            <div>
+              <div onClick={() => this.handleShowSmurf(smurf)} key={smurf.id}>
+                {smurf.name}, {smurf.age}, {smurf.height};
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Smurfs;
