59d748629a1f92c60d19b542869e553da3bae5ed
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-09-21 11:00:06 -0700

    mvp

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,15 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+import axios from 'axios';
+
+export const ERROR = 'ERROR';
+//export const GET_SMURFS = 'GET_SMURFS';
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const SMURFS_RECEIVED = 'SMURFS_RECEIVED';
+export const CREATE_SMURF = 'CREATE_SMURF';
+export const SMURF_CREATED = 'SMURF_CREATED';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +22,36 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+//export const getSmurfs = () => {
+
+export function getSmurfs() {
+    //return dispatch => {
+    return function(dispatch) {
+	dispatch({ type: GETTING_SMURFS });	
+	axios.get(`http://localhost:3333/smurfs`)
+	    .then(response => {
+		console.log(response.data);
+		dispatch({ type: SMURFS_RECEIVED, payload: response.data });
+	    })
+	    .catch(err => {
+		console.log('error in getSmurfs action');
+		dispatch({ type: ERROR, payload: err });
+	    });
+    };
+};
+
+export function addSmurf(smurf) {
+    //const newSmurf = axios.post(`http://localhost:3333/smurfs`, smurf);
+    return function(dispatch) {
+	dispatch({ type: CREATE_SMURF });
+	axios.post(`http://localhost:3333/smurfs`, smurf)
+	    .then(response => {
+		dispatch({type: SMURF_CREATED, payload: response.data});
+	    })
+	    .catch(err => {
+		console.log('error in addSmurf action');
+		dispatch({type: ERROR, payload: err});
+	    });
+    };
+};
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,67 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSmurfs, addSmurf } from '../actions/index.js';
 import './App.css';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
- */
+*/
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+    
+    state = {
+	age: 0,
+	name: 'Smurf',
+	height: '10cm'
+    }
+
+    componentDidMount() {
+	this.props.getSmurfs();
+    };
+    
+    handleChange = event => {
+	this.setState ({
+	    [event.target.name]: event.target.value
+	});
+    }
+    
+    render() {
+	const { name, age, height } = this.state;
+	const { addSmurf, fetchingSmurfs, smurfs } = this.props; 
+	
+	return (
+	    <div className="App">
+              <h1>Village</h1>
+	      <div>
+		{smurfs.map(smurf => (
+		    <section>
+		      <p>{smurf.name}</p>
+		      <p>{smurf.age}</p>
+		      <p>{smurf.height}</p>
+		    </section>
+		))}
+	    </div>
+                <section>
+		<input name="name" type="text" placeholder="name" value={name} onChange={this.handleChange}/>
+		<input name="age" type="number" placeholder="age" value={age} onChange={this.handleChange}/>
+		<input name="height" type="text" placeholder="height" value={height} onChange={this.handleChange}/>
+                <button onClick = {() => addSmurf({name, age, height})}>New</button>
+		</section>
+		</div>
+	);
+    }
 }
 
-export default App;
+//export default App;
+
+const mapStateToProps = state => ({
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs,
+    addingSmurf: state.addingSmurf,
+    error: state.error
+});
+
+ export default connect(mapStateToProps, {getSmurfs, addSmurf})(App); 
