af9e48c8d38ff706cc22113e1b74bf3bf01ebd4c
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-21 12:34:45 -0400

    Actions/Reducers Created actions and reducers for full crud Simple smurf component also created

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+ 
+
+
+class Smurfs extends Component {   
+   render() {
+    return (
+      <div className="Smurfs">
+        <h1>Smurf Village</h1>
+        <ul>
+          {this.props.smurfs.map(smurf => {
+            return (
+                <li className="Smurf">
+                    <h3>{smurf.name}</h3>
+                    <strong>{smurf.height} tall</strong>
+                    <p>{smurf.age} smurf years old</p>                
+                </li>  
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Smurfs; 
