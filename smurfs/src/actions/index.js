d25cbc2538cac016b852a85df2608c09817d98d9
verydecent <wnjhwng@gmail.com>
2018-09-28 08:53:43 -0700

    created action types

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,11 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const GETTING_SMURF = 'GETTING_SMURF';
+export const SMURFS_RECEIVED = 'SMURFS_RECEIVED';
+export const ERROR = 'ERROR';
+export const CREATING_SMURF = 'CREATING_SMURF';
+export const CREATED_SMURF
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
