4a6b3389bcf59671eb2eb60297e8976cbc95bb6d
Matthew Wright <matthewbwright@gmail.com>
2018-09-28 11:06:50 -0700

    adds styling

--- smurfs/src/views/Smurf.js
+++ smurfs/src/views/Smurf.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import '../components/App.css';
 
 function Smurf(props) {
   return (
-    <div>
+    <div className="smurf">
       <h2>{props.smurf.name}</h2>
-      <p>{props.smurf.age}</p>
+      <p>{props.smurf.age} years old</p>
       <p>{props.smurf.height}</p>
-      <button onClick={() => props.editSmurf(props.smurf)}>Edit</button>
-      <button onClick={event => props.deleteClick(event, props.smurf.id)}>
+      <button className="button" onClick={() => props.editSmurf(props.smurf)}>
+        Edit
+      </button>
+      <button
+        className="button"
+        onClick={event => props.deleteClick(event, props.smurf.id)}
+      >
         Delete
       </button>
     </div>
