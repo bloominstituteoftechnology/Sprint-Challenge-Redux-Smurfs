cfd2038765d41b4af3c090a39e15244151c3b74d
lyang9 <lyang9@buffalo.edu>
2018-09-28 13:23:59 -0400

    Created onChange handler

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -9,18 +9,21 @@ const SmurfForm = props => {
           text='text'
           placeholder='name'
           value={props.state.name}
+          onChange={props.handleInput}
         />
         <input
           name='age'
           text='text'
           placeholder='age'
           value={props.state.age}
+          onChange={props.handleInput}
         />
         <input
           name='height'
           text='text'
           placeholder='height'
           value={props.state.height}
+          onChange={props.handleInput}
         />
         <button type='submit'>Add Smurf</button>
       </form>
