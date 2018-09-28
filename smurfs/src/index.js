332f0e33afc23784e55db252304677fe0c773f13
Marious Davis <mlancelot88@gmail.com>
2018-09-28 16:23:25 -0500

    reimported my smurfsReducer into index.js. Smurf Card reappears

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,8 +6,10 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { smurfsReducer } from './reducers';
 
 const store = createStore(
+  smurfsReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk, logger)
 );
