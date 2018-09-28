d4d80730ce43c4b19a39500081728af9c9d95a92
Matthew Wright <matthewbwright@gmail.com>
2018-09-28 10:24:27 -0700

    sets up app.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { getSmurf, addSmurf, deleteSmurf, updateSmurf } from '../actions';
+import Smurf from '../views';
+import SmurfForm from '../views';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +11,89 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  state = {
+    name: '',
+    age: '',
+    height: '',
+    editId: null
+  };
+
+  componentDidMount() {
+    this.props.getSmurf();
+  }
+
+  handleChange = event => {
+    if (event.target.name === 'age') {
+      const number = parseInt(event.target.value, 10);
+      this.setState({ [event.target.name]: number });
+    } else {
+      this.setState({ [event.target.name]: event.target.value });
+    }
+  };
+
+  handleClick = event => {
+    event.preventDefault();
+    this.state.editId !== null
+      ? this.props.updateSmurf(this.state)
+      : this.props.addSmurf(this.state);
+    this.setState({ name: '', age: '', height: '', editId: null });
+  };
+
+  deleteClick = event => {
+    event.preventDefault();
+    this.props.deleteSmurf(id);
+  };
+
+  editSmurf = smurf => {
+    this.setState(
+      {
+        name: smurf.name,
+        age: smurf.age,
+        height: smurf.height,
+        editId: smurf.id
+      },
+      () => console.log(this.state)
+    );
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>Smurfs Village</h1>
+        <div>
+          {this.props.fetching ? (
+            <h2>La la la-la la la</h2>
+          ) : (
+            <Fragment>
+              {this.props.smurfs.map(smurf => (
+                <Smurf
+                  key={smurf.id}
+                  smurf={smurf}
+                  deleteClick={this.deleteClick}
+                  editSmurf={this.editSmurf}
+                />
+              ))}
+            </Fragment>
+          )}
+        </div>
+        <SmurfForm
+          input={this.state}
+          handleChange={this.handleChange}
+          handleClick={this.handleClick}
+        />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    fetching: state.fetchingSmurfs
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSmurf, addSmurf, deleteSmurf, updateSmurf }
+)(App);
---
+++ smurfs/src/views/SmurfForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function SmurfForm(props) {
+  console.log(props.input.editId);
+  return (
+    <form>
+      <h2>{props.input.editId ? 'Edit Smurf' : 'AddSmurf'}</h2>
+      <div>
+        <label>Name</label>
+        <input
+          name="name"
+          type="text"
+          placeholder="Smurf name"
+          value={props.input.name}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div>
+        <label>Age</label>
+        <input
+          name="age"
+          type="text"
+          placeholder="Smurf age"
+          value={props.input.age}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div>
+        <label>Height</label>
+        <input
+          name="height"
+          type="text"
+          placeholder="Smurf height"
+          value={props.input.height}
+          onChange={props.handleChange}
+        />
+      </div>
+      <button onClick={props.handleClick}>
+        {props.input.editId ? 'Edit Smurf' : 'Add Smurf'}
+      </button>
+    </form>
+  );
+}
+
+export default SmurfForm;
