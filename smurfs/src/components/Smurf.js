19bcf0c81bd63be2ee90c06cee4e3fc479a07bd3
lyang9 <lyang9@buffalo.edu>
2018-09-28 12:57:07 -0400

    Wrapped in div tag

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Smurf = props => {
   return (
-    <h3>{props.name}</h3>
-    <p>{props.age} years old</p>
-    <p>{props.height} cm tall</p>
+    <div>
+      <h3>{props.name}</h3>
+      <p>{props.age} years old</p>
+      <p>{props.height} cm tall</p>
+    </div>
   );
 }
 
