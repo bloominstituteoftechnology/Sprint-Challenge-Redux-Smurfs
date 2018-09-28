8523be9238d89fddff2a7877684a36dfdacde121
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-09-28 11:05:18 -0500

    reducers at level of displaying response

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {getSmurf} from '../actions/index';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -7,6 +9,9 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount(){
+    this.props.getSmurf();
+  }
   render() {
     return (
       <div className="App">
