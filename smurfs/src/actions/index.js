19c9c8c4dc9a6fe3e8aee1d1afe1250d66ec69db
IlanL75 <anatoly.leytman@gmail.com>
2018-09-28 08:56:00 -0700

    Adding index.js, actions/index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,37 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+
+export const SMURFS_FETCH_START = 'SMURFS_FETCH_START';
+export const SMURFS_FETCH_COMPLETE = 'SMURFS_FETCH_COMPLETE';
+export const SMURFS_FETCH_FAILURE = 'SMURFS_FETCH_FAILURE';
+
+export const ADD_SMURF_START = 'ADD_SMURF_START';
+export const ADD_SMURF_COMPLETE = 'ADD_SMURF_COMPLETE';
+export const ADD_SMURF_FAILURE = 'ADD_SMURF_FAILURE';
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: SMURFS_FETCH_START });
+    const promise = axios.get('http://localhost:3333/smurfs');
+
+    promise
+        .then(response => {
+            dispatch({ type: SMURFS_FETCH_COMPLETE, payload: response.data });
+        })
+        .catch(err => {
+            dispatch({ type: SMURFS_FETCH_FAILURE, payload: err });
+        });
+};
+
+export const addSmurf = smurf => dispatch => {
+    dispatch({ type: ADD_SMURF_START });
+
+    axios.post('http://localhost:3333/smurfs', smurf)
+        .then(response => {
+            dispatch({ type: ADD_SMURF_COMPLETE, payload: response.data });
+        }).catch(err => {
+            dispatch({ type: ADD_SMURF_FAILURE, payload: err });
+        });
+}
