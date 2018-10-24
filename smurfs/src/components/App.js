6ddefaa64ef7be5018afd34626ef470cdb68a6ed
Rahul Desai <rd825@georgetown.edu>
2018-10-24 14:41:38 -0400

    Built very basic GET functionality

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux';
+import {getSmurfs, addSmurf, deleteSmurf} from '../actions'
+ 
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +10,32 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
+    console.log(this.props.smurfs)
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {this.props.smurfs.map(smurf => <p>{smurf.name}</p>)}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    got: state.got
+  }
+}
+
+export default connect(mapStateToProps, {getSmurfs, addSmurf, deleteSmurf})(App);
