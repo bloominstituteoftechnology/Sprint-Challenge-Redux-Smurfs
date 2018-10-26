f294af788088979df732cbee46e700cb9a3ee35e
Stublink <chrisrey95@gmail.com>
2018-10-26 11:41:01 -0400

    Reducers for ADD, UPDATE, DELETE

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,57 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  ADD_SMURF,
+  ADD_SMURF_FAILURE,
+  ADD_SMURF_SUCCESS,
+  UPDATE_SMURF,
+  UPDATE_SMURF_FAILURE,
+  UPDATE_SMURF_SUCCESS,
+  DELETE_SMURF,
+  DELETE_SMURF_FAILURE,
+  DELETE_SMURF_SUCCESS,
+} from '../actions'
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null,
+}
+
+export default (state = initialState, action) => {
+  switch(action.case) {
+    case ADD_SMURF : return {...state, addingSmurf: true}
+    case ADD_SMURF_FAILURE : return {...state, addingSmurf: false, error: action.payload}
+    case ADD_SMURF_SUCCESS : 
+      return {
+        ...state, 
+        addingSmurf: false,
+        smurfs: action.payload
+      }
+
+    case UPDATE_SMURF : return {...state, updatingSmurf: true}
+    case UPDATE_SMURF_FAILURE : return {...state, updatingSmurf: false, error: action.payload}
+    case UPDATE_SMURF_SUCCESS : 
+      return {
+        ...state, 
+        updatingSmurf: false,
+        smurfs: action.payload
+      }
+
+    case DELETE_SMURF : return {...state, deletingSmurf: true}
+    case DELETE_SMURF_FAILURE : return {...state, deletingSmurf: false, error: action.payload}
+    case DELETE_SMURF_SUCCESS : 
+      return {
+        ...state, 
+        deletingSmurf: false,
+        smurfs: action.payload
+      }
+
+    default: return {...state};
+  }
+}
 
 /*
   You'll only need one smurf reducer for this project.
