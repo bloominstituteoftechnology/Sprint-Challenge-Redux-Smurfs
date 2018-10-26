1b7cc77b4a93cbcbe75b523dfcd4f590579f8ab5
ryanboris <ryan.boris@gmail.com>
2018-10-26 11:34:34 -0400

    Completes App Component and Renders Smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,62 @@
-import React, { Component } from 'react';
-import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import React, { Component } from 'react'
+import './App.css'
+import { connect } from 'react-redux'
+import { getSmurfs } from '../actions/index'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+    state = {
+        name: '',
+        age: null,
+        height: '',
+    }
+    componentDidMount() {
+        this.props.getSmurfs()
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.props.gettingSmurfs ? (
+                    <p>Getting smurfs, yo!</p>
+                ) : (
+                    <div>
+                        <ul>
+                            {this.props.smurfs.map(smurf => {
+                                return (
+                                    <div className="list">
+                                        <li key={smurf.name}>
+                                            Name:
+                                            {smurf.name}
+                                        </li>
+                                        <li key={smurf.age}>
+                                            Age:
+                                            {smurf.age}
+                                        </li>
+                                        <li key={smurf.height}>
+                                            Height:
+                                            {smurf.height}
+                                        </li>
+                                    </div>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                )}
+                {this.props.error !== '' ? <p>{this.props.error}</p> : null}
+            </div>
+        )
+    }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs,
+        error: state.error,
+        gettingSmurfs: state.gettingSmurfs,
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    { getSmurfs }
+)(App)
