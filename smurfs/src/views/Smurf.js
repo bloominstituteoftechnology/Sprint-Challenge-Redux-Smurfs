2bd052b5f457c9a74c7ef47ce9d104a1ca2b6a68
Matthew Wright <matthewbwright@gmail.com>
2018-10-26 10:04:51 -0700

    adds smurf view

--- smurfs/src/views/Smurf.js
+++ smurfs/src/views/Smurf.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function Smurf() {
-  return <div />;
+function Smurf(props) {
+  return (
+    <div>
+      <h2>{props.smurf.name}</h2>
+      <p>{props.smurf.age} years old</p>
+      <p>{props.smurf.height}</p>
+    </div>
+  );
 }
 
 export default Smurf;
