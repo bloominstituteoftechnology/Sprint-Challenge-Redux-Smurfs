c5c72a3818587c3bd3aef5560ed915f2215a7528
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-26 09:14:21 -0700

    actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,12 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from "axios";
+export const ADD_SMURF = "ADD_SMURF";
+export const GET_SMURFS = "GET_SMURFS";
+export const FETCHING = "FETCHING";
+export const CREATING = "CREATING";
+export const FAILURE = "FAILURE";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +19,22 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => {
+  const promise = axios.get("http://localhost:3333/smurfs");
+  return function(dispatch) {
+    dispatch({type: FETCHING});
+    promise
+      .then(response => dispatch({type: GET_SMURFS, payload: response.data}))
+      .catch(err => dispatch({type: FAILURE, payload: err}));
+  };
+};
+
+export const addSmurf = newSmurf => {
+  const promise = axios.post("http://localhost:3333/smurfs", newSmurf);
+  return function(dispatch) {
+    dispatch({type: CREATING});
+    promise
+      .then(response => dispatch({type: ADD_SMURF, payload: response.data}))
+      .catch(err => dispatch({type: FAILURE, payload: err}));
+  };
+};
