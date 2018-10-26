2e277dc6b819b6d592c0ae4e3e7f9aa2ff6e3ec2
ryanboris <ryan.boris@gmail.com>
2018-10-26 11:29:15 -0400

    Creates GET action

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,20 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios'
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const GET_SMURFS = 'GET_SMURFS'
+export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS'
+export const GET_SMURFS_ERROR = 'GET_SMURFS_ERROR'
+
+export const getSmurfs = () => {
+    return dispatch => {
+        dispatch({ type: GET_SMURFS })
+        axios
+            .get('http://localhost:3333/smurfs')
+            .then(res => {
+                dispatch({ type: GET_SMURFS_SUCCESS, payload: res.data })
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch({ type: GET_SMURFS_ERROR })
+            })
+    }
+}
