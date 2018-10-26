1f59b8cfd04356bc56e4784965a4cecabacbe1d0
unknown <brandongrych@gmail.com>
2018-10-26 12:52:16 -0500

    extra style added

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -5,8 +5,8 @@ import Smurf from './Smurf';
 export default function SmurfList(props) {
     return (
         <div className="smurfsContainer">
-        {props.smurfs.map(smurf => (
-            <Smurf smurf={smurf}/>
+        {props.smurfs.map((smurf, index) => (
+            <Smurf key={index} smurf={smurf}/>
         ))}
         </div>
     )
