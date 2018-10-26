6f3d2281ed024f017d8311bf581dca8aa3a8ebe9
MEBonham <programmer@mckaybonham.com>
2018-10-26 10:33:35 -0600

    Added nonfunctional edit/delete icons

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -3,7 +3,19 @@ import React from "react";
 const SmurfCard = props => {
     return (
         <section>
-            <h2>{props.smurf.name}</h2>
+            <div className="first-line">
+                <img
+                    className="delX"
+                    src="./images/Red_x.png"
+                    alt="Delete"
+                />
+                <img
+                    className="editBtn"
+                    src="./images/pencil.png"
+                    alt="Edit"
+                />
+                <h2>{props.smurf.name}</h2>
+            </div>
             <p><strong>Age:</strong> {props.smurf.age} smurf years</p>
             <p><strong>Height:</strong> {props.smurf.height}</p>
         </section>
