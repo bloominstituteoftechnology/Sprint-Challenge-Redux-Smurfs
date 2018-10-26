b94c724cfbd4e0160fd5963c2818b722939c11c2
ryanboris <ryan.boris@gmail.com>
2018-10-26 11:40:13 -0400

    Renders form and allows smurfs to be added

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import { connect } from 'react-redux'
 import { getSmurfs } from '../actions/index'
+import { addSmurf } from '../actions/index'
 
 class App extends Component {
     state = {
@@ -12,6 +13,24 @@ class App extends Component {
     componentDidMount() {
         this.props.getSmurfs()
     }
+    handleChange = e => {
+        const target = e.target
+        const name = target.name
+        const value =
+            //eslint-disable-next-line
+            target.name === 'age' ? parseInt(target.value) : target.value
+
+        this.setState({
+            [name]: value,
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.addSmurf(this.state)
+        window.location.reload()
+    }
+
 
     render() {
         return (
@@ -40,6 +59,30 @@ class App extends Component {
                                 )
                             })}
                         </ul>
+                        <form onSubmit={this.handleSubmit}>
+                            <label forHtml="name">Name:</label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                onChange={this.handleChange}
+                            />
+                            <label forHtml="age">Age:</label>
+                            <input
+                                type="number"
+                                id="age"
+                                name="age"
+                                onChange={this.handleChange}
+                            />
+                            <label forHtml="height">Height:</label>
+                            <input
+                                type="text"
+                                id="height"
+                                name="height"
+                                onChange={this.handleChange}
+                            />
+                            <input type="submit" value="submit" />
+                        </form>
                     </div>
                 )}
                 {this.props.error !== '' ? <p>{this.props.error}</p> : null}
@@ -53,10 +96,11 @@ const mapStateToProps = state => {
         smurfs: state.smurfs,
         error: state.error,
         gettingSmurfs: state.gettingSmurfs,
+        addingSmurf: state.addingSmurf,
     }
 }
 
 export default connect(
     mapStateToProps,
-    { getSmurfs }
+    { getSmurfs, addSmurf }
 )(App)
