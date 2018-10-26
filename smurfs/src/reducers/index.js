c7875ea921a20e674d21f1c86ab3e55cbfeff117
ryanboris <ryan.boris@gmail.com>
2018-10-26 11:31:10 -0400

    Adds GET reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,33 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+    GET_SMURFS,
+    GET_SMURFS_SUCCESS,
+    GET_SMURFS_ERROR,
+} from '../actions/index'
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+    smurfs: [],
+    gettingSmurfs: false,
+}
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+export const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_SMURFS:
+            return {
+                ...state,
+                gettingSmurfs: true,
+            }
+        case GET_SMURFS_SUCCESS:
+            return {
+                ...state,
+                gettingSmurfs: false,
+                smurfs: [...state, ...action.payload],
+            }
+        case GET_SMURFS_ERROR:
+            return {
+                ...state,
+                gettingSmurfs: false,
+            }
+        default:
+            return state
+    }
+}
