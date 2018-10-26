a1945665104bba9d685f2d01dfa440720a04ac30
ryanboris <ryan.boris@gmail.com>
2018-10-26 11:36:29 -0400

    Creates POST action

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,6 +4,10 @@ export const GET_SMURFS = 'GET_SMURFS'
 export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS'
 export const GET_SMURFS_ERROR = 'GET_SMURFS_ERROR'
 
+export const ADD_SMURF = 'ADD_SMURF'
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS'
+export const ADD_SMURF_ERROR = 'ADD_SMURF_ERROR'
+
 export const getSmurfs = () => {
     return dispatch => {
         dispatch({ type: GET_SMURFS })
@@ -15,6 +19,24 @@ export const getSmurfs = () => {
             .catch(err => {
                 console.log(err)
                 dispatch({ type: GET_SMURFS_ERROR })
+            })
+    }
+}
+
+export const addSmurf = smurf => {
+    return dispatch => {
+        dispatch({ type: ADD_SMURF })
+        axios
+            .post('http://localhost:3333/smurfs', smurf)
+            .then(res =>
+                dispatch({
+                    type: ADD_SMURF_SUCCESS,
+                    payload: res.data,
+                })
+            )
+            .catch(err => {
+                console.log(err)
+                dispatch({ type: ADD_SMURF_ERROR })
             })
     }
 }
