56c6febecfea5aeb21e3f12efb04c01a249c314e
Michael Checo <checomichael2@gmail.com>
2018-10-26 11:33:35 -0400

    created action to get smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,27 @@
+export const FETCHING = 'FETCHING';
+export const GET_SMURFS = 'GET_SMURFS';
+export const ERR = 'ERR';
+
+
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const getSmurfs = () => {
+  const promise = axios.get('http://localhost:3333/smurfs')
+  return dispatch => {
+    dispatch({type: FETCHING})
+    promise
+    .then(response => {
+      console.log('RESPONSE', response)
+      dispatch({type: GET_SMURFS, payload: response.data})
+    })
+    .catch(err => {
+      dispatch({type: ERR, payload: err})
+    })
+  }
+}
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -12,4 +32,19 @@
    R - getSmurfs
    U - updateSmurf
    D - deleteSmurf
+*/
+
+
+
+
+/*
+ Your initial/default state for this project could *Although does not have to* look a lot like this
+ {
+   smurfs: [],
+   fetchingSmurfs: false
+   addingSmurf: false
+   updatingSmurf: false
+   deletingSmurf: false
+   error: null
+ }
 */
