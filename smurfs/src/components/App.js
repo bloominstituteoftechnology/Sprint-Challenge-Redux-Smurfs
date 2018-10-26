39ff0c6f716e44a074a40753f8e95dec59858254
Brad Mortensen <b.mort86@gmail.com>
2018-10-26 09:38:32 -0600

    building components functionality

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { fetchSmurfs, addSmurf } from '../actions'
+import Smurf from './smurf';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +10,25 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchSmurfs();
+  }
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>SMURFS!</h1>
+        <Smurf />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    fetching: state.fetching 
+  };
+};
+// our mapStateToProps needs to have two properties inherited from state
+// the characters and the fetching boolean
+export default connect(mapStateToProps,{ fetchSmurfs, addSmurf })(App);
---
+++ smurfs/src/components/smurf.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Smurf = props => {
+  return (
+    <div className='contact-card'>
+      <h3>{props.friend.name}</h3>
+      <p>{props.friend.age}</p>
+      <p>{props.friend.email}</p>
+      <button 
+        onClick={() => props.deleteFriend()}>DELETE</button>
+    </div>
+  )       
+};
+
+export default Smurf;
