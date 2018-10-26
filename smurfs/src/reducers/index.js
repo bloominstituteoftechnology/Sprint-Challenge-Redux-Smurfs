13bd5a4e3b1b4896ab8c0b0d0a7b9fdf89f67ead
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-26 11:35:24 -0400

    filled in reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,7 +2,12 @@
   Be sure to import in all of the action types from `../actions`
 */
 import {
-
+  FETCHING, 
+  SUCCESS, 
+  FAILURE, 
+  ADDING, 
+  ADDED, 
+  FAILED_ADD
 } from '../actions/';
 
 
@@ -18,6 +23,13 @@ import {
  }
 */
 
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  error: null
+}
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -25,3 +37,47 @@ import {
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export const smurfReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case FETCHING:
+        return {
+              ...state,
+              fetchingSmurfs: true
+          }
+      case SUCCESS:
+          return {
+              ...state,
+              smurfs: [
+                  ...state.smurfs,
+                  ...action.payload
+              ],
+              fetchingSmurfs: false
+          }
+      case FAILURE:
+          return {
+              ...state,
+              fetchingSmurfs: false,
+              error: action.payload
+          }
+      case ADDING:
+          return {
+              ...state,
+              addingSmurf: true,
+          }
+      case ADDED:
+          return {
+              ...state,
+              addingSmurf: false,
+              smurfs: [
+                  ...action.payload
+              ]
+          }
+      case FAILED_ADD:
+          return {
+              ...state,
+              savingSmurfs: false,
+              error: action.payload
+          }
+  }
+}
