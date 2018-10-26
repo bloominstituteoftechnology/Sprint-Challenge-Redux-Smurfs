a97b72709fa6425f2d69fa046e44af85033b8a8f
Brandon Strellis <bstrellis@gmail.com>
2018-10-26 11:54:35 -0400

    Added getSmurfs action. Updated store with reducer.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,33 @@
+// ACTIONS -------------------- ACTIONS
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
+export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
 
+export const getSmurfs = () => dispatch => {
+  dispatch({type: FETCHING_CHARS});
+
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+        console.log('response', response);
+        dispatch({
+          type: FETCHING_SMURFS_SUCCESS,
+          payload: response
+        });
+      }
+    )
+    .catch(error => {
+        dispatch({
+          type: FETCHING_SMURFS_FAILURE,
+          payload: error
+        });
+    })
+}
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
