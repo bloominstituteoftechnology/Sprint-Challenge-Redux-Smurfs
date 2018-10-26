3272dfcab6c8a95eb1a5653e81e3f04025a9fd0e
Blake <koenneckerb@gmail.com>
2018-10-26 09:08:48 -0700

    Created the actions for CRUD operations.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from "./reducers";
 
