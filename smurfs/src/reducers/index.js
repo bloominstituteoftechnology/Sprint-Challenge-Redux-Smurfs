022ab75f98d36842414905fb6f29783fb0fef48d
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-26 11:30:32 -0400

    did actions.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,10 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+
+} from '../actions/';
+
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
