3e66658b61c2dcdd55468f109e5a2e92b616e6bd
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-26 12:27:51 -0400

    actions and reducers

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,40 @@
+import axios from "axios";
+
+export const ERROR = "ERROR";
+export const GET_SMURFS = "GET_SMURFS";
+export const GETTING_SMURFS = "GETTING_SMURFS";
+export const CREATING_SMURF = "CREATING_SMURF";
+export const CREATE_SMURF = "CREATE_SMURF";
+
+const URL = "http://localhost:3333/smurfs";
+
+export const getSmurfs = () => {
+  const smurfs = axios.get(`${URL}/get`);
+  return dispatch => {
+    dispatch({ type: GETTING_SMURFS });
+    smurfs
+      .then(response => {
+        dispatch({ type: GET_SMURFS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
+
+export const createSmurf = smurf => {
+  const newSmurf = axios.post(`${URL}/create`, smurf);
+  return dispatch => {
+    dispatch({ type: CREATING_SMURF });
+    newSmurf
+      .then(({ data }) => {
+        dispatch({ type: CREATE_SMURF, payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
