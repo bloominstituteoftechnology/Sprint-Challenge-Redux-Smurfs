f69e74446738e1b57f32abd285550c21ce13263a
Matthew Wright <matthewbwright@gmail.com>
2018-10-26 10:02:00 -0700

    adds finctionality for add Smurf

--- smurfs/src/views/SmurfForm.js
+++ smurfs/src/views/SmurfForm.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
-function SmurfForm() {
-  return <div />;
+import './ViewStyle.css';
+
+function SmurfForm(props) {
+  console.log(props.input.editId);
+  return (
+    <form>
+      <h2>{props.input.editId ? 'Edit Smurf' : 'Add Smurf'}</h2>
+      <div>
+        <label>Name</label>
+        <input
+          name="name"
+          type="text"
+          placeholder="Smurf name"
+          value={props.input.name}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div>
+        <label>Age</label>
+        <input
+          name="age"
+          type="text"
+          placeholder="Smurf age"
+          value={props.input.age}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div>
+        <label>Height</label>
+        <input
+          name="height"
+          type="text"
+          placeholder="Smurf height"
+          value={props.input.height}
+          onChange={props.handleChange}
+        />
+      </div>
+      <button onClick={props.handleClick}>
+        {props.input.editId ? 'Edit Smurf' : 'Add Smurf'}
+      </button>
+    </form>
+  );
 }
 
 export default SmurfForm;
