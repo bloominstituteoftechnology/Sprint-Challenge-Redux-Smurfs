078bc3abc7ae6acc78369d415ac6d0f0b7c355b5
bummings <eabummings@gmail.com>
2018-10-26 12:31:10 -0400

    setting up app.js to display smurfs, hooked up to store with connect method

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +7,18 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>testing</h1>
+        {/* map over smurfs with list component  */}
       </div>
     );
   }
 }
 
-export default App;
+export default Connect(mapStateToProps)(App);
