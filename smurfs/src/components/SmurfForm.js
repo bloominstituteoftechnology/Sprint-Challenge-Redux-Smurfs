82f7a1027f64892ca0f607c7ab0a544c9fd666d4
Katie Cleary <katiecleary@airmaster.lan>
2018-10-26 13:02:34 -0400

    created smurf form

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSmurfs, addSmurf } from "../actions";
+
+class SmurfForm extends Component {
+  smurfsHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+   submitSmurf = () => {
+    this.props.addSmurf({
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    });
+  };
+   render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="name"
+            onChange={this.smurfsHandler}
+            placeholder="Add Smurf Name Here.."
+          />
+          <br />
+          <input
+            type="number"
+            name="age"
+            onChange={this.smurfsHandler}
+            placeholder="Add Smurf Age Here.."
+          />
+          <br />
+          <input
+            type="text"
+            name="height"
+            onChange={this.smurfsHandler}
+            placeholder="Add Smurf Height Here.."
+          />
+          <br />
+          <button onClick={this.submitSmurf}> Add Smurf </button>
+        </form>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    addedSmurf: state.addedSmurf,
+    error: state.error
+  };
+};
+export default connect(
+  mapStateToProps,
+  {
+    addSmurf,
+    fetchSmurfs
+  }
+)(SmurfForm);
