6adc42cec9289eb990aa99881c088417ba81ba2d
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-26 10:52:17 -0500

    actions index js completed

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,15 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+import axios from 'axios';
+
+export const FETCHING_SMURFS= 'FETCHING_SMURFS';
+export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
+export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
+export const CREATING_SMURFS="CREATING_SMURFS"; 
+export const CREATED_SMURFS="CREATED_SMURFS"; 
+export const ERROR ="ERROR"; 
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +22,29 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: response.data });
+    })
+    .catch(error => {
+      dispatch({ type: FETCHING_SMURFS_FAILURE, payload: error });
+    });
+};
+
+export const createFriends = smurf =>{
+  const newFriend = axios.post(`http://localhost:3333/smurfs`, smurf); 
+  return dispatch => {
+      dispatch({ type: CREATING_SMURFS}); 
+      newFriend
+      .then(({ data }) => {
+          dispatch({ type: CREATED_SMURFS, payload: data}); 
+      })
+      .catch(err =>{
+          dispatch({type: ERROR, payload: err}); 
+      })
+  };
+}; 
