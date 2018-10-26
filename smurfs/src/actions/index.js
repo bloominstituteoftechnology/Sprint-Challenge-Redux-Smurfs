52c9ac4eb37a992e60deff2ffefa79d9c6828b1c
Axolotl <samuelmachat@gmail.com>
2018-10-26 12:21:43 -0400

    write action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,51 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios'
+
+export const ADDING = 'ADDING'
+export const ADDSMURF = 'ADDSMURF'
+export const GETTING = 'GETTING'
+export const GETSMURFS = 'GETSMURFS'
+export const UPDATING = 'UPDATING'
+export const UPDATESMURF = 'UPDATESMURF'
+export const DELETING = 'DELETING'
+export const DELETESMURF = 'DELETESMURF'
+export const ERROR = 'ERROR'
+
+export const addSmurf = ({ name, age, height }) => dispatch => {
+  dispatch({ type: ADDING })
+
+  axios
+    .post('http://localhost:3333/smurfs', { name, age, height })
+    .then(res => dispatch({ type: ADDSMURF, payload: res.data }))
+    .catch(err => dispatch({ type: ERROR, payload: err }))
+}
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: GETTING })
+
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => dispatch({ type: GETSMURFS, payload: res.data }))
+    .catch(err => dispatch({ type: ERROR, payload: err }))
+}
+
+export const updateSmurf = ({ id, name, age, height }) => dispatch => {
+  dispatch({ type: UPDATING })
+
+  axios
+    .put(`http://localhost:3333/smurfs/${id}`, { name, age, height })
+    .then(res => dispatch({ type: UPDATESMURF, payload: res.data }))
+    .catch(err => dispatch({ type: ERROR, payload: err }))
+}
+
+export const deleteSmurf = ({ id }) => dispatch => {
+  dispatch({ type: DELETING })
+
+  axios
+    .delete(`http://localhost:3333/smurfs/${id}`)
+    .then(res => dispatch({ type: DELETESMURF, payload: res.data }))
+    .catch(err => dispatch({ type: ERROR, payload: err }))
+}
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+)
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
