2ef985440b8f2e65fade8ac86a6349ba59cf2733
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-26 16:11:41 -0500

    Rework updates to App js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,94 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import { fetchSmurfs, postSmurf } from '../actions/';
+import { fetchSmurfs } from '../actions/';
 import { connect } from 'react-redux';
+import Smurfs from './Smurfs';
+import SmurfForm from './SmurfsForm';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      name: '',
-      age: '',
-      height: '',
-    }
-  }
-
   componentDidMount() {
     this.props.fetchSmurfs();
-  }
-
-  addSmurf = () => {
-    const newSmurf = {
-      name: this.state.name,
-      age: this.state.age,
-      height: this.state.height,
-    }
-    this.props.postSmurf(newSmurf);
-    this.setState({
-      name: '',
-      age: '',
-      height: '',
-    })
-  }
-
-  handleNewSmurf = e => {
-    this.setState({[e.target.name]: e.target.value})
   }
 
   render() {
     return (
       <div className="App">
-        <h1>SMURFS!</h1>
-        <div>
-          {this.props.fetching ? (
-            <p>LOADING SMURFS . . . </p>
-            ) : (
-            <div>
-              {this.props.smurfs.map(smurf => {
-                return (<div key={smurf.name}>
-                  <h2>Name: {smurf.name}</h2>
-                  <p>age: {smurf.age}</p>
-                  <p>height: {smurf.height}</p>
-                </div>)
-              })}
-            </div>
-          )}
-        </div>
-        <form>
-          Name:
-          <input
-            type="text"
-            onChange={this.handleNewSmurf}
-            value={this.state.name}
-            name='name'
-          /><br />
-          Age:
-          <input
-            type="text"
-            onChange={this.handleNewSmurf}
-            value={this.state.age}
-            name='age'
-          /><br />
-          Height:
-          <input
-            type="text"
-            onChange={this.handleNewSmurf}
-            value={this.state.height}
-            name='height'
-          />
-        </form>
-        <button onClick={ this.addSmurf }>Submit New Smurf</button>
+        <header className="App-header">
+        <h1>Smurfs</h1>
+        <SmurfForm />
+        </header>
+      <div>
+        {this.props.isFetching ? (
+          <h1>Loading</h1>
+        ): (
+          <Smurfs smurfs={this.props.smurfs} />
+        )}
+      </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
+  //const{ smurfsReducer } = state; 
   return {
-    smurfs: state.smurfs,
-    error: state.error,
-    fetching: state.fetching
+    smurfs: state.SmurfsReducer.smurfs,
+    isfetching: state.SmurfsReducer.isfetching
   }
 }
 
-export default connect(mapStateToProps, { fetchSmurfs, postSmurf })(App);
+export default connect(mapStateToProps, { fetchSmurfs})(App);
