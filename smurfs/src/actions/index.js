4576b0ea9ef1ffe8947dc0506f62273fc5bbea61
Angelo Deleon <rudy209@comcast.net>
2018-10-26 09:27:08 -0700

    actions done

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,14 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+import { FETCH_SMURFS } from './types';
+
+export const fetchSmurfs = () => async dispatch => {
+  const res = await axios.get('http://localhost:3333/smurfs');
+  dispatch({ type: FETCH_SMURFS, payload: res.data });
+};
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
