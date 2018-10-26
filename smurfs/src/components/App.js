28c958f3a04451373a831f247e5b270cd2dd52fc
Axolotl <samuelmachat@gmail.com>
2018-10-26 12:39:28 -0400

    map state and dispatch to props

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addSmurf, getSmurfs, updateSmurf, deleteSmurf } from '../actions'
+import './App.css'
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
+const mapStateToProps = ({
+  smurfs,
+  fetchingSmurfs,
+  addingSmurf,
+  updatingSmurf,
+  deletingSmurf,
+  error
+}) => ({
+  smurfs,
+  fetchingSmurfs,
+  addingSmurf,
+  updatingSmurf,
+  deletingSmurf,
+  error
+})
+
 class App extends Component {
   render() {
     return (
@@ -15,8 +34,11 @@ class App extends Component {
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  { addSmurf, getSmurfs, updateSmurf, deleteSmurf }
+)(App)
