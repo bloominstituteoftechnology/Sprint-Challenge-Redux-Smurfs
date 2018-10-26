c3cec5c8976e2091c9a0da277b1427098f2325c0
Trevor Lewis <tenzingtitan@gmail.com>
2018-10-26 10:52:25 -0700

    MVP complete with some styling

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,13 @@
+import axios from 'axios';
 /* 
-  Action Types Go Here!
+  Action types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const GETTING_SMURF = 'GETTING_SMURF';
+export const SMURFS_RECEIVED = 'SMURFS_RECEIVED';
+export const ERROR = 'ERROR';
+export const CREATING_SMURF = 'CREATING_SMURF';
+export const CREATED_SMURF = 'CREATED_SMURF';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +18,33 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export function getSmurfs() {
+  return function(dispatch) {
+    dispatch({ type: GETTING_SMURF });
+    axios
+      .get(`http://localhost:3333/smurfs`)
+        .then(res => {
+          dispatch({ type: SMURFS_RECEIVED, payload: res.data })
+        })
+          .catch(err => {
+            console.log(err, 'Failure in getting smurfs');
+            dispatch({ type: ERROR, payload: err })
+          })
+  }
+}
+
+export function addSmurf(smurf) {
+  return function(dispatch) {
+    dispatch({ type: CREATING_SMURF });
+    axios
+      .post(`http://localhost:3333/smurfs`, smurf)
+        .then(res => {
+          dispatch({ type: CREATED_SMURF, payload: res.data })
+        })
+        .catch(err => {
+          console.log(err, 'Failure in adding smurf')
+          dispatch({ type: ERROR, payload: err })
+        })
+  }
+}
