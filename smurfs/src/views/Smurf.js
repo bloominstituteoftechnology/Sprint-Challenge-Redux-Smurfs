15fe1e87b030d921f17fbe808d5441bcf76aecf9
Matthew Wright <matthewbwright@gmail.com>
2018-10-26 10:25:41 -0700

    adds edit and delete buttons

--- smurfs/src/views/Smurf.js
+++ smurfs/src/views/Smurf.js
@@ -8,6 +8,15 @@ function Smurf(props) {
       <h2>{props.smurf.name}</h2>
       <p>{props.smurf.age} years old</p>
       <p>{props.smurf.height}</p>
+      <button className="button" onClick={() => props.editSmurf(props.smurf)}>
+        Edit
+      </button>
+      <button
+        className="button"
+        onClick={event => props.deleteClick(event, props.smurf.id)}
+      >
+        Delete
+      </button>
     </div>
   );
 }
