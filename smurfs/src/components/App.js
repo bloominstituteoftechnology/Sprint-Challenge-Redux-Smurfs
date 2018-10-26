e42455ec21143b925a95764c222e7700698c4e11
Lee Len Formento <leebatef@gmail.com>
2018-10-26 00:21:33 -0400

    More comments deleted

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -5,12 +5,6 @@ import Smurfs from './Smurfs';
 import SmurfForm from './SmurfForm';
 import {getSmurfs} from '../actions';
 
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
 class App extends Component {
   componentDidMount() {
     this.props.getSmurfs();
