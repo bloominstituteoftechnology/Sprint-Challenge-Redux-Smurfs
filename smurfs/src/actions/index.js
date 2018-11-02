59286a8175b0bbf4ff7569f9a9f8afb5a8757a3d
jaklundun <govi.escudero@gmail.com>
2018-11-02 09:53:43 -0700

    Set up Actions with GET related action creators.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,13 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCHED_SMURFS = 'FETCHED_SMURFS';
+export const ERROR = 'ERROR';
+// export const ADDING_SMURF = 'ADDING_SMURF';
+// export const ADDED_SMURF = 'ADDED_SMURF';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +20,20 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const fetchSmurfs = () => {
+  const getSmurfs = axios.get('http://localhost:3333/smurfs');
+  return function (dispatch) {
+    dispatch({ type: FETCHING_SMURFS, payload: true });
+    getSmurfs
+      .then(data => {
+        console.log(data);
+        dispatch({ type: FETCHED_SMURFS, payload: data.data });
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch({ type: ERROR, payload: true });
+      })
+  }
+}
+
