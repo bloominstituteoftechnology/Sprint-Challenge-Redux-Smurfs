c8fbea4760ac317e37a69169ab2e0e825bd875d5
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-11-02 12:26:00 -0400

    actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,59 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ADDED = 'ADDED';
+export const UPDATING = 'UPDATING';
+export const UPDATED = 'UPDATED';
+export const DELETED = 'DELETED';
+export const ERROR = 'ERROR';
+const url = 'http://localhost:3333/smurfs'
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING })
+  axios
+    .get(url)
+    .then(res => {
+      dispatch({ type: FETCHED, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err })
+    })
+}
+
+export const addSmurf = (newSmurf) => dispatch => {
+  axios
+    .post(url, newSmurf)
+    .then(res => {
+      dispatch({ type: ADDED, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err })
+    })
+}
+
+export const updating = (id) => dispatch => {
+  dispatch({ type: UPDATING, payload: id })
+}
+
+export const updateSmurf = (editedSmurf) => dispatch => {
+  axios
+    .put(`${url}/${editedSmurf.id}`, editedSmurf)
+    .then(res => {
+      dispatch({ type: UPDATED, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err })
+    })
+}
+
+export const deleteSmurf = (id) => dispatch => {
+  axios
+    .delete(`${url}/${id}`)
+    .then(res => {
+      dispatch({ type: DELETED, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err })
+    })
+}
