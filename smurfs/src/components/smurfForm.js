3f364068c9198ff49b70b98307960e002e2bceec
Wanda <doubleuevans@gmail.com>
2018-11-02 12:18:36 -0500

    addSmurfForm, addSmurf action

---
+++ smurfs/src/components/smurfForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addSmurf } from "../actions";
+
+class SmurfForm extends Component {
+  state = {
+    name: "",
+    age: "",
+    height: ""
+  };
+
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleAddSmurf = () => {
+    const { name, age, height } = this.state;
+    this.props.createFriend({ name, age, height });
+    this.setState({ name: "", age: "", height: "" });
+  };
+
+  render() {
+    return (
+      <form className="Column-Layout">
+        <input
+          className="input"
+          value={this.state.name}
+          name="name"
+          type="text"
+          placeholder="Name"
+          onChange={this.handleInputChange}
+        />
+        <input
+          className="input"
+          value={this.state.age}
+          name="age"
+          type="text"
+          placeholder="Age"
+          onChange={this.handleInputChange}
+        />
+        <input
+          className="input"
+          value={this.state.email}
+          name="height"
+          type="text"
+          placeholder="height"
+          onChange={this.handleInputChange}
+        />
+        <button onClick={() => this.handleAddSmurf()} type="button">
+          Add New Friend
+        </button>
+      </form>
+    );
+  }
+}
+
+const mapStateToProps = state => {};
+
+export default connect(
+  mapStateToProps,
+  { addSmurf }
+)(FriendForm);
