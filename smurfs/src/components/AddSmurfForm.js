fa1f0b1a31a8941459ca87ac1691009789cb69ce
jaklundun <govi.escudero@gmail.com>
2018-11-02 11:07:49 -0700

    AddSmurfForm handlers have functional. GET and POST now fully functional and displaying corresponing contents on the DOM.

--- smurfs/src/components/AddSmurfForm.js
+++ smurfs/src/components/AddSmurfForm.js
@@ -12,14 +12,24 @@ class AddSmurfForm extends Component {
         }
     }
 
+    handleInputChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleAddSmurfBtn = () => {
+        const newSmurf = this.state;
+        this.props.addSmurf(newSmurf);
+        this.setState({ name: '', age: '', height: '' })
+    }
+
     render() {
         return (
             <div>
                 <form>
-                    <input name='name' value={this.state.name} placeholder='Add name...' />
-                    <input name='age' value={this.state.age} placeholder='Add age...' />
-                    <input name='height' value={this.state.height} placeholder='Add height...' />
-                    <button>Register Smurf</button>
+                    <input onChange={this.handleInputChange} name='name' value={this.state.name} placeholder='Add name...' />
+                    <input onChange={this.handleInputChange} name='age' value={this.state.age} placeholder='Add age...' />
+                    <input onChange={this.handleInputChange} name='height' value={this.state.height} placeholder='Add height...' />
+                    <button onClick={this.handleAddSmurfBtn} >Register Smurf</button>
                 </form>
             </div>
         )
