5add7246ef1ed5087bc9289dbfebd28ce034f643
Robin U Familara <robin.familara@cgius.com>
2018-11-10 12:41:49 -0600

    Modified the Component App -Import the components and actions -map state to props -this is to assure state manipulation before GET -Modified the header on SmurfForm and Smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import './App.css';
+import Smurfs from './Smurfs';
+import SmurfForm from './SmurfForm';
+import {getSmurfs} from '../actions';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +11,29 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+       <header>
+        <h1 className="Title">SMURFS! 2.0 W/ Redux</h1>
+        <SmurfForm />
+        <Smurfs />
+       </header>
+       {this.props.error ? <h3>Error Getting Smurfs</h3> : null}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+  smurfs: state.smurfs,
+  fetching: state.gettingSmurfs,
+  error: state.error
+  }
+};
+ export default connect(mapStateToProps,{getSmurfs})(App);
