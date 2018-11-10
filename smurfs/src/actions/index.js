4375d05e347b1ff0198b52f9af05c66601631ac7
Griffin <griffinshuth@gmail.com>
2018-11-10 11:44:43 -0700

    got my MVP done

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+FETCHING_SMURFS, ADDING_SMURF, UPDATING_SMURF, DELETING_SMURF, ERROR, FETCHED_SMURFS
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const DELETING_SMURF = 'DELETING_SMURF';
+export const ERROR = 'ERROR';
+export const FETCHED_SMURFS = 'FETCHED_SMURFS';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +20,32 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+const URL = 'http://localhost:3333/smurfs';
+
+export const getSmurfs = () => {
+	const smurf = axios.get(`${URL}`);
+	return (dispatch) => {
+		dispatch({ type: FETCHING_SMURFS });
+		smurf
+			.then(({ data }) => {
+				dispatch({ type: FETCHED_SMURFS, payload: data });
+			})
+			.catch((err) => {
+				dispatch({ type: ERROR, payload: err });
+			});
+	};
+};
+
+export const addSmurf = (smurf) => {
+	const promise = axios.post(`${URL}`, smurf);
+	return (dispatch) => {
+		promise
+			.then(({ data }) => {
+				dispatch({ type: ADDING_SMURF, payload: data });
+			})
+			.catch((err) => {
+				dispatch({ type: ERROR, payload: err });
+			});
+	};
+};
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,102 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSmurfs, addSmurf } from '../actions';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
+ I'll let you do this part on your own.
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			age: '',
+			height: ''
+		};
+	}
+	componentDidMount() {
+		this.props.getSmurfs();
+	}
+
+	formHandler = (e) => {
+		e.preventDefault();
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	addSmurf = (e) => {
+		e.preventDefault();
+		const { name, age, height } = this.state;
+		this.props.addSmurf({
+			name,
+			age,
+			height
+		});
+		this.setState({ name: '', age: '', height: '' });
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<div>
+					<form onSubmit={this.addSmurf}>
+						<input
+							type="text"
+							onChange={this.formHandler}
+							name="name"
+							value={this.props.name}
+							placeholder="Name..."
+						/>
+
+						<input
+							type="text"
+							onChange={this.formHandler}
+							name="age"
+							value={this.props.age}
+							placeholder="Age..."
+						/>
+
+						<input
+							type="text"
+							onChange={this.formHandler}
+							name="height"
+							value={this.props.height}
+							placeholder="Height..."
+						/>
+						<button onClick={this.addSmurf} name="Submit">
+							Submit
+						</button>
+					</form>
+				</div>
+				{this.props.smurfs.map((smurf) => {
+					return (
+						<div key={smurf.id}>
+							<h3>Name: {smurf.name}</h3>
+							<p>Age: {smurf.age}</p>
+							<p>Height: {smurf.height}</p>
+              <button>Delete</button>
+              <button>Edit</button>
+						</div>
+            
+					);
+				})}
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	const { smurfReducer } = state;
+	return {
+		smurfs: smurfReducer.smurfs
+	};
+};
+
+export default connect(mapStateToProps, {
+	getSmurfs,
+	addSmurf,
+})(App);
