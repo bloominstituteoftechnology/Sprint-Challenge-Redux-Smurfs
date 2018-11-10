4e74b140b14119b797a799453e5aa143b9c983f0
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-11-10 12:29:13 -0500

    progress commit

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,12 +6,13 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import rootReducer from './reducers';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+
+const middleware = applyMiddleware(thunk,logger)
+
+const store = createStore(rootReducer, middleware)
+
 
 ReactDOM.render(
   <Provider store={store}>
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,20 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+const initialState = {
+smurfs : [],
+error : '',
+loading: false
+
+};
+
+
+export default (state = initialState, action) =>{
+  switch(action.type){
+    default:
+      return state;
+  }
+}
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -14,10 +28,6 @@
  }
 */
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+
+
+
