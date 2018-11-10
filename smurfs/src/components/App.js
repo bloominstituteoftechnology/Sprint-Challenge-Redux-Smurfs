374e01f0b2502524dd5d71777a3c243b990790d6
stevekutz <stkutz@gmail.com>
2018-11-10 12:37:48 -0600

    partially built up Apps

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux';
+import {addSmurf, getSmurfs} from '../actions';
+
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,8 +11,32 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  state = {
+    name: '',
+    age: null,
+    height: ''
+  };
+
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
+
+
+
+
+
+
+
   render() {
     return (
+
+      const {name, age, height} = this.state;
+      const {addSmurf, fetchingSmurfs, smurfs} = this.props;
+
+
+
+
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your Redux version of Smurfs!</div>
