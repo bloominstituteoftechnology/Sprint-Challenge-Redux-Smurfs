5a391aa0e3cf003d162219686cfb5bb006e09090
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-10 15:59:27 -0500

    added SmurfsContent component added import for connect and getSmurfs action creator added connect method added mapStateToProps method

---
+++ smurfs/src/components/SmurfsContent.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getSmurfs } from '../actions';
+
+import SmurfsList from './SmurfsList';
+
+class SmurfsContent extends React {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    // call action here
+    this.props.getSmurfs();
+  }
+
+  render() {
+    if (this.props.fetching) {
+      <h2>Smurfs are loading...</h2>
+    }
+    return (
+      <div>
+        <SmurfsList smurfs={this.props.smurfs} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    fetching: state.fetchingSmurfs
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs }
+)(SmurfsContent);
