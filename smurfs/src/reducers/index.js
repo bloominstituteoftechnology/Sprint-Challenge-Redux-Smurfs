53ad8af48450f20b40795c78308a92f035867a5c
Jason Hedrick <jasonleehedrick@gmail.com>
2018-11-10 09:51:39 -0800

    Reducer functionality setup in reducer\index.js and connected, with middleware, to the store in the root index.js

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,19 +1,44 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {FETCHING, SUCCESS, FAILURE, ADD_SMURF, DELETE_SMURF, UPDATE_SMURF} from '../actions/index';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null,
+}
 
+export default (previousState = initialState, action) => {
+  switch(action.type) {
+    case FETCHING: 
+      return Object.assign({}, previousState, 
+        {fetchingSmurfs: true, addingSmurf: false, updatingSmurf: false, deletingSmurf: false, error: null});
+
+    case SUCCESS:
+      return Object.assign({}, previousState, 
+        {smurfs: action.payload, fetchingSmurfs: false, addingSmurf: false, updatingSmurf: false, deletingSmurf: false, error: null});
+
+    case FAILURE: 
+      return Object.assign({}, previousState, 
+        {fetchingSmurfs: false, addingSmurf: false, updatingSmurf: false, deletingSmurf: false, error: action.payload});
+
+    case ADD_SMURF:
+      return [...previousState, 
+        {fetchingSmurfs: false, addingSmurf: true, updatingSmurf: false, deletingSmurf: false, error: null}];
+    
+    case DELETE_SMURF:
+      return [...previousState,
+        {fetchingSmurfs: false, addingSmurf: false, updatingSmurf: false, deletingSmurf: true, error: null}];
+
+    case UPDATE_SMURF:
+      return [...previousState,
+        {fetchingSmurfs: false, addingSmurf: false, updatingSmurf: true, deletingSmurf: false, error: null}];
+
+    default: 
+      return previousState;
+  }
+}
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
