8056a46540021782802532059240ebae902b48c9
Matthew Meitl <darksamus6@gmail.com>
2018-11-10 09:58:45 -0600

    finished index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,59 +1,24 @@
-//-- Dependencies --------------------------------
-import * as actions from '../actions';
 
-//-- Initial Store State -------------------------
-const initialState = {
-    smurfs: [],
-    fetching: false,
-    error: null,
-    focus: null,
-};
-  
-//-- Route Actions -------------------------------
-export default function (state = initialState, action){
-    switch (action.type) {
-        case actions.NOT_READY      : return handleNotReady  (state, action);
-        case actions.FETCH_ERROR    : return handleFetchError(state, action);
-        case actions.FETCHING       : return handleFetching  (state, action);
-        case actions.SMURFS_RESPONSE: return handleSmurfsList(state, action);
-        case actions.FOCUS_CANCEL   : /* Fall Through */
-        case actions.FOCUS_SMURF    : return handleFocus     (state, action);
-        default                     : return                  state         ;
-    }
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Smurfs from './components/smurfs';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 
-//-- Action Handling -----------------------------
-function handleFetching(state, action) {
-    return {
-        ...state,
-        fetching: true,
-    };
-}
-function handleSmurfsList(state, action) {
-    return {
-        ...state,
-        fetching: false,
-        smurfs: action.smurfs,
-        error: null,
-        focus: null,
-    };
-}
-function handleFetchError(state, action) {
-    return {
-        ...state,
-        fetching: false,
-        error: action.error,
-    };
-}
-function handleNotReady(state, action) {
-    return {
-        ...state,
-        error: action.error,
-    };
-}
-function handleFocus(state, action) {
-    return {
-        ...state,
-        focus: action.id,
-    };
-}
+//-- Generate Data store for Applicaton ----------
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, logger),
+);
+
+//-- Mount Applicaton ----------------------------
+ReactDOM.render(
+  <Provider store={store}>
+    <Smurfs />
+  </Provider>,
+  document.getElementById('root')
+);
