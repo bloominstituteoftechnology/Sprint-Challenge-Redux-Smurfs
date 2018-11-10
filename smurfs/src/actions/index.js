6c4faf65d97d9e1ca9a9c5a1f8276265284b14bc
Jennifer King <jenbellpgh@gmail.com>
2018-11-10 12:52:58 -0500

    Added skeleton for actions. Action types: FETCHING, UPDATING, CREATING, DELETING, ERROR. Empty functions for: getSmurfs, addSmurf, editSmurf, deleteSmurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,15 @@
+/* We need axios to get data from the api */
+import axios from 'axios';
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const FETCHING = 'FETCHING';
+export const UPDATING = 'UPDATING';
+export const CREATING = 'CREATING';
+export const DELETING = 'DELETING';
+export const ERROR    = 'ERROR';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +21,17 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+// Assigning all of these as empty functions for now, just to have them defined.
+
+// GET the smurfs from the API data:
+export const getSmurfs = () => {};
+
+// POST to add a new smurf to the API:
+export const addSmurf = () => {};
+
+// Stretch - PUT to edit a smurf:
+export const editSmurf = () => {};
+
+// Stretch - DELETE to remove:
+export const deleteSmurf = () => {};
