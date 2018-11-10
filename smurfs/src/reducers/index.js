c5bf477fc1fc5f972f9fc3c24f5d7732d0cc3752
Jennifer King <jenbellpgh@gmail.com>
2018-11-10 12:58:58 -0500

    Basic skeleton for reducers is complete. initialState setup, and all action types return state.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { FETCHING, UPDATING, ERROR, CREATING, DELETING } from '../actions';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -13,6 +14,14 @@
    error: null
  }
 */
+const initialState = {
+  smurfs: [],
+  fetching: false,
+  adding: false,
+  updating: false,
+  deleting: false,
+  error: null
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +30,14 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export default ( state = initialState, action ) => {
+  switch( action.type ) {
+    case FETCHING:
+    case UPDATING:
+    case CREATING:
+    case DELETING:
+    case ERROR:
+    default:
+      return state;
+  }
+};
