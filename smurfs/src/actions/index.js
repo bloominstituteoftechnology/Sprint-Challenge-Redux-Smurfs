6cc92f6daefd12f2b6f05251158c3224c12f0d10
Robert Billington <chillingtonbillington@gmail.com>
2018-11-10 11:43:38 -0600

    Updated action. Started reducer.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,49 +1,48 @@
-export const SMURF_LOADING = 'SMURF_LOADING';
-export const SMURF_SUCCESS = 'SMURF_SUCCESS';
-export const SMURF_FAILURE = 'SMURF_FAILURE';
+
+import axios from 'axios';
+
+export const GET_SMURF_LOADING = 'GET_SMURF_LOADING';
+export const GET_SMURF_SUCCESS = 'GET_SMURF_SUCCESS';
+export const GET_SMURF_FAILURE = 'GET_SMURF_FAILURE';
+export const ADD_SMURF_LOADING = 'ADD_SMURF_LOADING';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAILURE = 'ADD_SMURF_FAILURE';
+export const DELETE_SMURF_LOADING = 'DELETE_SMURF_LOADING';
+export const DELETE_SMURF_SUCCESS = 'DELETE_SMURF_SUCCESS';
+export const DELETE_SMURF_FAILURE = 'DELETE_SMURF_FAILURE';
 
 export const getSmurf = () => (dispatch) =>{
-  dispatch({type:SMURF_LOADING})
+  dispatch({type:GET_SMURF_LOADING})
     axios
       .get('http://localhost:3333/smurfs')
         .then(response =>{
-          dispatch({type:SMURF_SUCCESS, payload: response.data})
+          dispatch({type:GET_SMURF_SUCCESS, payload: response.data})
         })
         .catch(error =>{
-          dispatch({type:SMURF_FAILURE, payload : 'Sorry, no Smurfs to be found'})
+          dispatch({type:GET_SMURF_FAILURE, payload : 'Sorry, no Smurfs to be found'})
         })
 }
-export const getSmurf = () => (dispatch) =>{
-  dispatch({type:SMURF_LOADING})
-    axios
-      .get('http://localhost:3333/smurfs')
-        .then(response =>{
-          dispatch({type:SMURF_SUCCESS, payload: response.data})
-        })
-        .catch(error =>{
-          dispatch({type:SMURF_FAILURE, payload : 'Sorry, no Smurfs to be found'})
-        })
-}
+
 export const addSmurf = (newSmurf) => (dispatch) =>{
-  dispatch({type:SMURF_LOADING})
+  dispatch({type:ADD_SMURF_LOADING})
     axios
       .get('http://localhost:3333/smurfs', newSmurf)
         .then(response =>{
-          dispatch({type:SMURF_SUCCESS, payload: response.data})
+          dispatch({type:ADD_SMURF_SUCCESS, payload: response.data})
         })
         .catch(error =>{
-          dispatch({type:SMURF_FAILURE, payload : 'Sorry, no Smurfs will be added'})
+          dispatch({type:ADD_SMURF_FAILURE, payload : 'Sorry, no Smurfs will be added'})
         })
 }
-export const Smurf = (id) => (dispatch) =>{
-   dispatch({type:SMURF_LOADING}) 
+export const deleteSmurf = (id) => (dispatch) =>{
+   dispatch({type:DELETE_SMURF_LOADING}) 
     axios
       .delete(`http://localhost:3333/smurfs/${id}`)
         .then(response =>{
-          dispatch({type:SMURF_SUCCESS, payload : response.data})
+          dispatch({type:DELETE_SMURF_SUCCESS, payload : response.data})
         })
         .catch(error =>{
-          dispatch({type:SMURF_FAILURE, payload : 'Sorry, no Smurfs will be deleted'})  
+          dispatch({type:DELETE_SMURF_FAILURE, payload : 'Sorry, no Smurfs will be deleted'})  
         })
       
 }
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,31 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  GET_SMURF_LOADING,
+  GET_SMURF_SUCCESS ,
+  GET_SMURF_FAILURE,
+  ADD_SMURF_LOADING ,
+  ADD_SMURF_SUCCESS ,
+  ADD_SMURF_FAILURE ,
+  DELETE_SMURF_LOADING,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_FAILURE ,
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+} from '../actions/index';
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const initialState = {
+  smurfs : [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null,
+}
+
+export const smurfReducer = (state = initialState, action) =>{
+  switch(action.type){
+    case SMURF_LOADING:
+      return Object.assign({}, state, {
+        ...state,
+        fetchingSmurfs:true,
+      })
+  }
+}
