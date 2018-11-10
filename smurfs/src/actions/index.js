08f43ce20caea7574d45bc3c512bdeea236879ac
Mitchell Robles <mitchellr92@live.com>
2018-11-10 09:26:38 -0800

    created actions for loading and adding smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,12 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+import axios from 'axios';
+
+export const LOADING = 'LOADING';
+export const SUCCESS = 'SUCCESS';
+export const ERROR = 'ERROR';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +19,43 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const loadSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: LOADING })
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then(response => {
+        dispatch({
+          type: SUCCESS,
+          payload: response.data
+        })
+      })
+      .catch(err => {
+        dispatch({
+          type: ERROR,
+          payload: err
+        })
+      })
+  }
+}
+
+export const addSmurf = (newSmurf) => {
+  return dispatch => {
+    dispatch({ type: LOADING })
+    axios
+      .get(`http://localhost:3333/smurfs`, newSmurf)
+      .then(response => {
+        dispatch({
+          type: SUCCESS,
+          payload: response.data
+        })
+      })
+      .catch(err => {
+        dispatch({
+          type: ERROR,
+          payload: err
+        })
+      })
+  }
+}
