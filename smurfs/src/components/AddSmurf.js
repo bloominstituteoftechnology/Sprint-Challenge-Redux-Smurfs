9f1f300c5955d36bdacd5b6943e9229318e83766
iepoch <staclem@live.com>
2018-11-10 11:35:52 -0700

    Clearing the input fields after submission

--- smurfs/src/components/AddSmurf.js
+++ smurfs/src/components/AddSmurf.js
@@ -22,6 +22,11 @@ class AddSmurf extends Component {
     submitHandler = e => {
         e.preventDefault()
         this.props.addSmurf(this.state)
+        this.setState({
+            age: '',
+            name: '',
+            height: '',
+        })
     }
 
     render() {
@@ -29,7 +34,7 @@ class AddSmurf extends Component {
             <form onSubmit={this.submitHandler}>
                 <input type="text" name='name' value={this.state.name} onChange={this.inputHandler} placeholder='Smurf Name' />
                 <input type="number" name='age' value={this.state.age} onChange={this.inputHandler} placeholder='Smurf Age' />
-                <input type="text" name='height' value={this.state.size} onChange={this.inputHandler} placeholder='Smurf Height' />
+                <input type="text" name='height' value={this.state.height} onChange={this.inputHandler} placeholder='Smurf Height' />
                 <button type='submit'>Add Smurf</button>
             </form>
         )
