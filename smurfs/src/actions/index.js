ecd119de9db0736edce1734ee2a1b5706d65dd42
Matthew Meitl <darksamus6@gmail.com>
2018-11-10 09:57:54 -0600

    initial commit and created action

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,59 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+//-- Dependencies --------------------------------
+import * as actions from '../actions';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+//-- Initial Store State -------------------------
+const initialState = {
+    smurfs: [],
+    fetching: false,
+    error: null,
+    focus: null,
+};
+  
+//-- Route Actions -------------------------------
+export default function (state = initialState, action){
+    switch (action.type) {
+        case actions.NOT_READY      : return handleNotReady  (state, action);
+        case actions.FETCH_ERROR    : return handleFetchError(state, action);
+        case actions.FETCHING       : return handleFetching  (state, action);
+        case actions.SMURFS_RESPONSE: return handleSmurfsList(state, action);
+        case actions.FOCUS_CANCEL   : /* Fall Through */
+        case actions.FOCUS_SMURF    : return handleFocus     (state, action);
+        default                     : return                  state         ;
+    }
+};
+
+//-- Action Handling -----------------------------
+function handleFetching(state, action) {
+    return {
+        ...state,
+        fetching: true,
+    };
+}
+function handleSmurfsList(state, action) {
+    return {
+        ...state,
+        fetching: false,
+        smurfs: action.smurfs,
+        error: null,
+        focus: null,
+    };
+}
+function handleFetchError(state, action) {
+    return {
+        ...state,
+        fetching: false,
+        error: action.error,
+    };
+}
+function handleNotReady(state, action) {
+    return {
+        ...state,
+        error: action.error,
+    };
+}
+function handleFocus(state, action) {
+    return {
+        ...state,
+        focus: action.id,
+    };
+}
