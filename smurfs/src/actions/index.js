2adb81db3393cd408042cf923ad7b0aa27832011
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-11-10 10:49:23 -0700

    Built out action to get Smurf data from server

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,13 @@
+import axios from 'axios';
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+
+export const FETCHING = 'FETCHING';
+export const GET_SMURFS = 'GET_SMURFS';
+export const ERROR = 'ERROR';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +19,19 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+// R - getSmurfs
+
+export const getSmurfs = () => {
+  return (dispatch) => {
+    dispatch({type: FETCHING});
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then(response => {
+        dispatch({type: GET_SMURFS, smurfs: response.data})
+      })
+      .catch(err => {
+        dispatch({type: ERROR, error: 'Oh no! We had some trouble loading the Smurfs!'})
+      });
+  }
+}
