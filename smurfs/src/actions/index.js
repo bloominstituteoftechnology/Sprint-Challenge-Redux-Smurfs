bbb1370f4c2b6b8524cbf658330111cce97589a3
Emmanium <pradoe.ep@gmail.com>
2018-11-10 11:00:01 -0700

    corrected rootReducer import created CRUD actions created reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,8 +3,63 @@ export const LOADING = 'LOADING';
 export const SUCCESS = 'SUCCESS';
 export const FAILURE = 'FAILURE';
 
-//actions
-export const addSmurf = ''
+//actions below
+// R - read - axios get
+export const getSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: LOADING })
+    axios.get('http://localhost:3333')
+      .then(response => {
+        console.log(response)
+        dispatch({ type: SUCCESS, smurfs: response.data })
+      })
+      .catch(err => {
+        dispatch({ type: FAILURE, error: 'Failed to load smurfs'})
+      })
+  }
+}
+
+// C - create - axios post
+export const addSmurf = (newSmurf) => {
+  return dispatch => {
+    dispatch({ type: LOADING })
+    axios.post('http://localhost:3333/smurfs', newSmurf)
+      .then(response => {
+        dispatch({ type: SUCCESS, smurfs: response.data })
+      })
+      .catch(err => {
+        dispatch({ type: FAILURE, error: 'Failed to add smurf'})
+      })
+  }
+}
+
+// U - Update - axious put
+export const updateSmurf = (updatedSmurf) => {
+  return dispatch => {
+    dispatch({ type: LOADING })
+    axios.put(`http://localhost:3333/smurfs/${updatedSmurf.id}`, updatedSmurf)
+      .then(response => {
+        dispatch({ type: SUCCESS, smurfs: response.data })
+      })
+      .catch(err => {
+        dispatch({ type: FAILURE, error: 'Failed to update smurf'})
+      })
+  }
+}
+
+// D - Delete - axios delete
+export const deleteSmurf = (id) => {
+  return dispatch => {
+    dispatch({ type: LOADING })
+    axios.delete(`http://localhost:3333/smurfs/${id}`)
+      .then(response => {
+        dispatch({ type: SUCCESS, smurfs: response.data })
+      })
+      .catch(err => {
+        dispatch({ type: FAILURE, error: 'Failed to delete smurf'})
+      })
+  }
+}
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
