57697cc16e58669b785103dc0998ca3b9c647990
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-11-10 11:40:38 -0700

    getting smurf in console

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
@@ -19,8 +20,15 @@ export const FETCHING_SMURFS = 'FETCHING_SMURFS';
 export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
 export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
 
+const URL = 'http://localhost:3333'
+
 export function getSmurfs() {
   return function(dispatch) {
     dispatch({ type: FETCHING_SMURFS })
+    axios
+      .get(`${URL}/smurfs`)
+      .then(response => {
+        dispatch(response.data);
+      })
   }
 }
