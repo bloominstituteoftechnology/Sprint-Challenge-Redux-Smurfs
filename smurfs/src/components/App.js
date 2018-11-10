f56f0d64fc9028ebaccb9ca015d908a9c5ca7df3
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-10 14:00:17 -0500

    added loading and error message to the app.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -18,6 +18,8 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        { this.props.fetchingSmurfs ? <h2>Loading</h2> : null }
+        { this.props.error !== '' ? <h1>{this.props.error}</h1> : null }
         { this.props.smurfs.map( (smurf, i) => <Smurf smurf={smurf} key={i} /> ) }
       </div>
     );
