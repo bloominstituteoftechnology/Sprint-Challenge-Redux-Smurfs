9a75ddf40bd996f89c87dc1f009e0e19cb687019
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-11-10 12:12:29 -0700

    First Part of Reducer is Complete

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,6 @@
 
 
-import {LOADING, SUCCESS, ERROR} from 'react-redux'
+import {GATHERING, SUCCESS, ERROR} from 'react-redux'
 /*
   Be sure to import in all of the action types from `../actions`
 */
@@ -15,10 +15,20 @@ const initialState = {
   error: null,
 }
 
-export default (state = initialState, actions) {
+export default (state = initialState, actions) => {
 
-  
+    switch(actions.type) {
+        case GATHERING:
+        return Object.assign({}, state, {gatheringSmurfs: true})
+        case SUCCESS:
+        return Object.assign({}, state, {smurfs: action.smurfs, gatheringSmurfs:false, error:null})
+        case ERROR:
+        return Object.assign({}, state, {error: action.errorMessage, gatheringSmurfs:false})
+        default:
+        return state
+    }
 }
+
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
