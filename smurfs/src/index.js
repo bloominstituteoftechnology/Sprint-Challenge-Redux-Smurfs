5708fc323ed205779d4134dc98040036f589bc10
Robin U Familara <robin.familara@cgius.com>
2018-11-10 12:22:54 -0600

    Modified main index under src -store and import smurfReducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -8,7 +8,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk,logger));
+import { smurfsReducer } from './reducers';
+ const store = createStore(smurfsReducer, applyMiddleware(thunk,logger));
 
 ReactDOM.render(
   <Provider store={store}>
