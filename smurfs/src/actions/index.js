108f6025b6ce6c3010e93790eb96ec7e76273f1b
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-10 14:25:17 -0500

    added axios import added FETCHTING, SUCCESS and FAILURE actions added getSmurfs action creator

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,11 @@
-/* 
+import axios from 'axios';
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +16,23 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: FETCHING });
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log(response);
+        dispatch({
+          type: SUCCESS,
+          payload: response.data
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: FAILURE,
+          payload: 'No smurfs found'
+        });
+      });
+  }
+}
