e9ab7fafc2c6b89cc26de83202e7ca839b4d973b
Robert Billington <chillingtonbillington@gmail.com>
2018-11-10 11:33:30 -0600

    Completed actions.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,49 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+export const SMURF_LOADING = 'SMURF_LOADING';
+export const SMURF_SUCCESS = 'SMURF_SUCCESS';
+export const SMURF_FAILURE = 'SMURF_FAILURE';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const getSmurf = () => (dispatch) =>{
+  dispatch({type:SMURF_LOADING})
+    axios
+      .get('http://localhost:3333/smurfs')
+        .then(response =>{
+          dispatch({type:SMURF_SUCCESS, payload: response.data})
+        })
+        .catch(error =>{
+          dispatch({type:SMURF_FAILURE, payload : 'Sorry, no Smurfs to be found'})
+        })
+}
+export const getSmurf = () => (dispatch) =>{
+  dispatch({type:SMURF_LOADING})
+    axios
+      .get('http://localhost:3333/smurfs')
+        .then(response =>{
+          dispatch({type:SMURF_SUCCESS, payload: response.data})
+        })
+        .catch(error =>{
+          dispatch({type:SMURF_FAILURE, payload : 'Sorry, no Smurfs to be found'})
+        })
+}
+export const addSmurf = (newSmurf) => (dispatch) =>{
+  dispatch({type:SMURF_LOADING})
+    axios
+      .get('http://localhost:3333/smurfs', newSmurf)
+        .then(response =>{
+          dispatch({type:SMURF_SUCCESS, payload: response.data})
+        })
+        .catch(error =>{
+          dispatch({type:SMURF_FAILURE, payload : 'Sorry, no Smurfs will be added'})
+        })
+}
+export const Smurf = (id) => (dispatch) =>{
+   dispatch({type:SMURF_LOADING}) 
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+        .then(response =>{
+          dispatch({type:SMURF_SUCCESS, payload : response.data})
+        })
+        .catch(error =>{
+          dispatch({type:SMURF_FAILURE, payload : 'Sorry, no Smurfs will be deleted'})  
+        })
+      
+}
