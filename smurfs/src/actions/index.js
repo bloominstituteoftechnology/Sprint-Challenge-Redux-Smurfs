1ae9402c26e0fe3e6c8d927890bdce3745dbe33a
paulapivat <paul.apivat@gmail.com>
2018-11-11 01:20:39 +0700

    render smurf array on page through Get request

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,11 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+import axios from 'axios'
+
+export const LOADING = "LOADING"
+export const SUCCESS = "SUCCESS"
+export const ERROR = "ERROR"
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +18,19 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+//GET
+export const getSmurfs = () => {
+  return (dispatch) => {
+    dispatch({ type: LOADING })
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then(response => {
+        dispatch({ type: SUCCESS, smurfs: response.data })
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, errorMessage: "Trouble Getting Smurfs"})
+      })
+  }
+}
+
---
+++ smurfs/src/components/SmurfPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SmurfPage = props => {
+
+    return (
+        <div className="smurf-card" key={props.smurf.id}>
+            <h3>Name: {props.smurf.name} </h3>
+            <p> Age: {props.smurf.age} </p>
+            <p> Height: {props.smurf.height} </p>
+        </div>
+    )
+}
+
+export default SmurfPage
