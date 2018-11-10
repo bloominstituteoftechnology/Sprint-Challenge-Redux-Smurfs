806957952c95bc9faf4dd68b1bb4558f714dca23
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-10 14:25:56 -0500

    added import for actions

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-import { } from '../actions/index.js';
+import { FETCHING, SUCCESS, FAILURE } from '../actions/index.js';
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
