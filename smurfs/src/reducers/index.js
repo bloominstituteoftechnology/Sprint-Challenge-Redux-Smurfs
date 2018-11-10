5d849582d500b77641499483922dc3cb7dce96ec
Matthew Meitl <darksamus6@gmail.com>
2018-11-10 10:01:31 -0600

    added reducers

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,58 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import * as actions from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+//-- Initial Store State -------------------------
+const initialState = {
+    smurfs: [],
+    fetching: false,
+    error: null,
+    focus: null,
+};
+  
+//-- Route Actions -------------------------------
+export default function (state = initialState, action){
+    switch (action.type) {
+        case actions.NOT_READY      : return handleNotReady  (state, action);
+        case actions.FETCH_ERROR    : return handleFetchError(state, action);
+        case actions.FETCHING       : return handleFetching  (state, action);
+        case actions.SMURFS_RESPONSE: return handleSmurfsList(state, action);
+        case actions.FOCUS_CANCEL   : 
+        case actions.FOCUS_SMURF    : return handleFocus     (state, action);
+        default                     : return                  state         ;
+    }
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+//-- Action Handling -----------------------------
+function handleFetching(state, action) {
+    return {
+        ...state,
+        fetching: true,
+    };
+}
+function handleSmurfsList(state, action) {
+    return {
+        ...state,
+        fetching: false,
+        smurfs: action.smurfs,
+        error: null,
+        focus: null,
+    };
+}
+function handleFetchError(state, action) {
+    return {
+        ...state,
+        fetching: false,
+        error: action.error,
+    };
+}
+function handleNotReady(state, action) {
+    return {
+        ...state,
+        error: action.error,
+    };
+}
+function handleFocus(state, action) {
+    return {
+        ...state,
+        focus: action.id,
+    };
+}
