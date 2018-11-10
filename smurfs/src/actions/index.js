691f2dd16c6b2b29067c4ef6bc1f50706f1fb259
JeffJohnson <jeff.jj.9494@gmail.com>
2018-11-10 12:51:51 -0700

    i dont know why my other commits arent working but i think this one will

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,53 @@
-/* 
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const GETTING_SMURF = 'GETTING_SMURF';
+export const GETTING_SMURF_SUCCESS = 'GETTING_SMURF_SUCCESS';
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const UPDATE_SMURF_SUCCESS = 'UPDATE_SMURF_SUCCESS';
+export const DELETEING_SMURF = 'DELETEING_SMURF';
+export const DELETE_SMURF = 'DELETE_SMURF';
+export const CREATEING_SMURF = 'CREATEING_SMURF';
+export const CREATE_SMURF = 'CREATE_SMURF';
+export const ERROR = 'ERROR';
+
+
+const URL = 'http://localhost:3333/smurfs'
+
+export const getSmurfs = () => {
+const smurfs = axios.get(`${URL}`);
+return dispatch => {
+  dispatch({type:GETTING_SMURF});
+  smurfs
+  .then(response=>{
+    dispatch({type:GETTING_SMURF_SUCCESS, payload: response.data});
+  })
+  .catch(err=>{
+    dispatch({type:ERROR, payload: err})
+  })
+  };
+};
+
+export const createSmurf = smurf =>{
+const newSmurf = axios.post(`${URL}`,smurf)
+return dispatch => {
+  dispatch({ type: CREATEING_SMURF });
+  newSmurf
+    .then(({ data }) => {
+      dispatch({ type: CREATE_SMURF, payload: data });
+    })
+    .catch(err => {
+      dispatch({ type: ERROR, payload: err });
+    });
+};
+}
+
+
+
+
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
