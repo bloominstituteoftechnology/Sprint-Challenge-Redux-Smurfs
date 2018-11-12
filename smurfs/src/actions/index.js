f7611de18aa1b68657091db525d0cd2a65692cfc
kammagik <kammagik@yahoo.com>
2018-11-12 15:24:46 -0800

    get

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,17 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios'
+export const GET_SMURFS = "GET_SMURFS";
+export const GETTING_SMURFS = "GETTING_SMURFS";
+export const ERROR = "ERROR";
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const getSmurfs = () => {
+    return (dispatch) => {
+        dispatch({type: GETTING_SMURFS})
+        axios.get(`http://localhost:3333/smurfs`)
+        .then(resonse => {
+            dispatch({type: GET_SMURFS, smurfs: response.data})
+        })
+        .catch(err => {
+            dispatch({type: ERROR, errorMessage: "could not retrieve smurf"})
+        })
+    }
+}
