b6b1f6040fda1e4efa25e67d8a7f12186cf60b6f
Kyle Baker <k@gnomiq.com>
2018-11-12 22:02:06 -0800

    get call

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,19 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from "axios"
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const FAILURE = "FAILURE";
+export const SUCCESS = "SUCCESS";
+export const FETCHING = "FETCHING";
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: FETCHING, loading: true });
+
+    axios
+        .get(`http://localhost:3333/smurfs`)
+        .then(({ data }) => {
+            console.log(data);
+            dispatch({ type: SUCCESS, smurfs: data, loading: false });
+        })
+        .catch(err => {
+            dispatch({ type: FAILURE, error: err, loading: false});
+        });
+};
