40a4c372b3c40a77ce42382a6d9bc9d082dbd71e
Phinehas <kohler.nai@gmail.com>
2018-11-13 09:32:34 -0700

    added details to smurf.js

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -8,9 +8,9 @@ const Smurf = props => {
 
     return (
         <div key={smurf.id}>
-            <h2>{smurf.name}</h2>
-            <h2>{smurf.age}</h2>
-            <h2>{smurf.height}</h2>
+            <h2>Name: {smurf.name}</h2>
+            <h2>Age: {smurf.age}</h2>
+            <h2>Height: {smurf.height}</h2>
         </div>
     )
 }
