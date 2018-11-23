fb22c06f22d00931fc932a6439b51124858ef71c
unknown <jerrardsmith215@gmail.com>
2018-11-22 23:15:51 -0500

    staging completed

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,14 +4,8 @@ import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
-
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
