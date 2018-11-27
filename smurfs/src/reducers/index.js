27ca068c4d71070b46553ff3d96daaaf0c3f54a1
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-11-26 22:23:48 -0500

    (MVP : STRUCTURE) - created initial file structure for smurfs/src/reducers/index.js

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,14 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+}
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +17,22 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export default (state = initialState, action) => {
+  
+  switch(action.type) {
+    case "FETCHING_SMURFS":
+      return Object.assign({}, state, { fetchingSmurfs: true });
+    
+    case "ADDING_SMURF":
+      return Object.assign({}, state, { addingSmurf: true });
+
+    case "UPDATING_SMURF":
+      return Object.assign({}, state, { updatingSmurf: true });
+
+    case "ERROR":
+      return Object.assign({}, state, { error: action.payload });
+
+    default: 
+      return state;
+  }
+};
