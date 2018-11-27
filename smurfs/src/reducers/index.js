0d07e5a3b639830dcdec1c15bcd4f03023073c33
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-11-26 23:21:15 -0500

    (MVP : STRUCTURE) - changed switch statement to ES6 format with spread syntax

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,36 +1,107 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
 const initialState = {
   smurfs: [],
   fetchingSmurfs: false,
+  smurfsFetched: false,
   addingSmurf: false,
+  smurfAdded: false,
   updatingSmurf: false,
   deletingSmurf: false,
   error: null
 }
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
 export default (state = initialState, action) => {
   
   switch(action.type) {
     case "FETCHING_SMURFS":
-      return Object.assign({}, state, { fetchingSmurfs: true });
+      return {
+        ...state,
+        smurfs: action.smurfs,
+        fetchingSmurfs: true,
+        smurfsFetched: false,
+        addingSmurf: false,
+        smurfAdded: false,
+        updatingSmurf: false,
+        deletingSmurf: false,
+        error: null
+      }
+
+    case "FETCHED_SMURF":
+      return {
+        ...state,
+        smurfs: action.smurfs,
+        fetchingSmurfs: false,
+        smurfsFetched: true,
+        addingSmurf: false,
+        smurfAdded: false,
+        updatingSmurf: false,
+        deletingSmurf: false,
+        error: null
+      };
     
     case "ADDING_SMURF":
-      return Object.assign({}, state, { addingSmurf: true });
+      return {
+        ...state,
+        smurfs: action.smurfs,
+        fetchingSmurfs: false,
+        smurfsFetched: false,
+        addingSmurf: true,
+        smurfAdded: false,
+        updatingSmurf: false,
+        deletingSmurf: false,
+        error: null
+      };
+
+    case "ADDED_SMURF":
+      return {
+        ...state,
+        smurfs: action.smurfs,
+        fetchingSmurfs: false,
+        smurfsFetched: false,
+        addingSmurf: false,
+        smurfAdded: true,
+        updatingSmurf: true,
+        deletingSmurf: false,
+        error: null
+    }
 
     case "UPDATING_SMURF":
-      return Object.assign({}, state, { updatingSmurf: true });
+      return {
+          ...state,
+          smurfs: action.smurfs,
+          fetchingSmurfs: false,
+          smurfsFetched: false,
+          addingSmurf: false,
+          smurfAdded: false,
+          updatingSmurf: true,
+          deletingSmurf: false,
+          error: null
+      };
+  
+    case "DELETING_SMURF":
+      return {
+        ...state,
+        smurfs: action.smurfs,
+        fetchingSmurfs: false,
+        smurfsFetched: false,
+        addingSmurf: false,
+        smurfAdded: false,
+        updatingSmurf: false,
+        deletingSmurf: true,
+        error: null
+    }
 
     case "ERROR":
-      return Object.assign({}, state, { error: action.payload });
+      return {
+        ...state,
+        smurfs: action.smurfs,
+        fetchingSmurfs: false,
+        smurfsFetched: false,
+        addingSmurf: false,
+        smurfAdded: false,
+        updatingSmurf: true,
+        deletingSmurf: false,
+        error: true
+    };
 
     default: 
       return state;
