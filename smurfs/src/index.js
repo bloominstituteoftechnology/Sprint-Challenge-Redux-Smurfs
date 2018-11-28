d8ab11d54b580edcb1e38234028777531f3099b4
unknown <jerrardsmith215@gmail.com>
2018-11-28 17:40:44 -0500

    Reducers & Actions updated

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { Provider } from 'react-redux';
 import store from './store';
