3bb6458d484e37b0b468207b6421ba35005026f7
david pok <david-pok>
2018-12-03 13:06:21 -0800

    fixes naming inconsistencies and bugs, mvp complete

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
@@ -16,25 +16,31 @@ import axios from 'axios';
    U - updateSmurf
    D - deleteSmurf
 */
-export const fetchSmurfs = () => {
-  const request = axios.get('http://localhost:3333/smurfs'); 
-  return (dispatch) => {
-    dispatch({type: "FETCHING_DATA"})
-    request.then(({data}) => {
-      dispatch({type: 'DATA_FETCHED', payload: data}); 
-    }).catch((err) => {
-      dispatch({type: "ERROR", payload: err})
-    })
-  }
-}
 
-export const addSmurf = (newSmurf) => {
-  const request = axios.post('http://localhost:3333/smurfs', newSmurf); 
-  return (dispatch) => {
-    dispatch({type: "SAVING_DATA"})
-    request.then(({data}) => {
-      dispatch({type: 'DATA_SAVED', payload: data})
-    }).catch((err) => {
-      dispatch({type: "ERROR", payload: err})
-    })
-  }}
+export const getSmurfs = () => {
+  const request = axios.get("http://localhost:3333/smurfs");
+  return dispatch => {
+    dispatch({ type: "FETCHING_DATA" });
+    request
+      .then(({ data }) => {
+        dispatch({ type: "DATA_FETCHED", payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: "ERROR", payload: err });
+      });
+  };
+};
+
+export const addSmurf = newSmurf => {
+  const request = axios.post("http://localhost:3333/smurfs", newSmurf);
+  return dispatch => {
+    dispatch({ type: "SAVING_DATA" });
+    request
+      .then(({ data }) => {
+        dispatch({ type: "DATA_SAVED", payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: "ERROR", payload: err });
+      });
+  };
+};
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -6,6 +6,14 @@
  Your initial/default state for this project could 
  *Although does not have to* look a lot like this*/
 
+/*
+  You'll only need one smurf reducer for this project.
+  Feel free to export it as a default and import as rootReducer. 
+  This will guard your namespacing issues.
+  There is no need for 'combineReducers' in this project.
+  Components can then read your store as, `state` and not `state.fooReducer`.
+*/
+
 const initialState = {
   smurfs: [],
   fetchingSmurfs: false,
@@ -15,27 +23,25 @@ const initialState = {
   error: null
 };
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
-
 export const smurfReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case 'FETCHING_SMURF':
-    return Object.assign({}, state, {fetchingSmurfs: true}); 
-    case 'SMURF_FETCHED': 
-    return Object.assign({}, state, {fetchingSmurfs: false, smurfs: action.payload}); 
-    case 'SAVING_SMURF': 
-    return Object.assign({}, state, {addingSmurfs: true}); 
-    case'SMURF_SAVED': 
-    return Object.assign({}, state, {addingSmurfs: false, smurfs: action.payload}); 
-    case 'SMURF_ERROR': 
-    return Object.assign({}, state, {error: action.payload}); 
+  switch (action.type) {
+    case "FETCHING_DATA":
+      return Object.assign({}, state, { fetchingSmurfs: true });
+    case "DATA_FETCHED":
+      return Object.assign({}, state, {
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      });
+    case "SAVING_DATA":
+      return Object.assign({}, state, { addingSmurfs: true });
+    case "DATA_SAVED":
+      return Object.assign({}, state, {
+        addingSmurfs: false,
+        smurfs: action.payload
+      });
+    case "ERROR":
+      return Object.assign({}, state, { error: action.payload });
     default:
-    return state; 
+      return state;
   }
-}
+};
