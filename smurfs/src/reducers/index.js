aa2044e116989b80f65e6d3236a57c9e27c0fead
Wesley Meeks <wesmeeks88@gmail.com>
2018-12-06 11:50:25 -0700

    Created reducer and linked it to App. Added middleware as well.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import { FETCHING, GET_SMURFS, ERROR } from '../actions';
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -13,7 +13,14 @@
    error: null
  }
 */
-
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+}
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +28,17 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case FETCHING:
+      return Object.assign({}, state, {fetchingSmurfs: true})
+    case GET_SMURFS:
+      return Object.assign({}, state, {fetchingSmurfs: false, error: '', smurfs: action.smurfs})
+    case ERROR: 
+      return Object.assign({}, state, {fetchingSmurfs: false, error: action.error})
+    default:
+      return state
+  } 
+}
+
+export default reducer;
