9e6f989acb21414e8feaa2f49c0d5eae9745dd1c
Wesley Meeks <wesmeeks88@gmail.com>
2018-12-06 13:19:27 -0700

    Built out smurflist to show individual smurfs and also the smurf form to add new smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -14,21 +14,36 @@ export const ERROR = 'ERROR';
    Be sure to include action types for each type of action creator. Also, be sure to mind
      the "pending" states like, fetching, creating, updating and deleting.
    C - addSmurf
+   
    R - getSmurfs
    U - updateSmurf
    D - deleteSmurf
 */
 
+export const addSmurf = (addedSmurf) => {
+  return (dispatch) => {
+    dispatch({ type: FETCHING });
+    axios
+      .post(`http://localhost:3333/smurfs`, addedSmurf)
+      .then(response => {
+        dispatch({ type: GET_SMURFS, smurfs: response.data })
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, error: `Oh no! We couldn't add the Smurf!` })
+      });
+  }
+}
+
 export const getSmurfs = () => {
   return (dispatch) => {
-    dispatch({type: FETCHING});
+    dispatch({ type: FETCHING });
     axios
       .get(`http://localhost:3333/smurfs`)
       .then(response => {
-        dispatch({type: GET_SMURFS, smurfs: response.data})
+        dispatch({ type: GET_SMURFS, smurfs: response.data })
       })
       .catch(err => {
-        dispatch({type: ERROR, error: 'Oh no! We had some trouble loading the Smurfs!'})
+        dispatch({ type: ERROR, error: 'Oh no! We had some trouble loading the Smurfs!' })
       });
   }
 }
