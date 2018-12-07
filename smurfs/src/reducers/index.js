86a93344914e99ee6581fdfe1ef517134466f97e
ykrueng <yasirah.krueng@gmail.com>
2018-12-07 10:09:07 -0600

    initial commit, setup Redux store

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,6 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export default (state, action) => {
+  
+}
