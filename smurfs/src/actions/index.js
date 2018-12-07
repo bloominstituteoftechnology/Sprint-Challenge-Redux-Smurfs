114ceca2e173df538885d1c97b0c47ba79748780
Trai Lynne <trailynne@gmail.com>
2018-12-07 10:58:25 -0700

    Set up initial file structure for actions and reducers

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const START_REQUEST = 'START_REQUEST';
+export const REQUEST_SUCCESS = 'REQUEST_SUCCESS';
+export const REQUEST_FAIL = 'REQUEST_FAIL';
+export const ADD_SMURF = 'ADD_SMURF';
+export const GET_SMURFS = 'GET_SMURFS';
+export const UPDATE_SMURF = 'UPDATE_SMURF';
+export const DELETE_SMURF = 'DELETE_SMURF';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,7 +2,17 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
+import { 
+  START_REQUEST,
+  REQUEST_SUCCESS,
+  REQUEST_FAIL,
+  ADD_SMURF,
+  GET_SMURFS,
+  UPDATE_SMURF,
+  DELETE_SMURF
+ } from "../actions";
+
+ /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
    smurfs: [],
@@ -13,6 +23,38 @@
    error: null
  }
 */
+
+const initialState = {
+  smurfs: [],
+  requestingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+}
+
+ const reducer = (state, action) => {
+   switch(action.type){
+    // case START_REQUEST:
+
+    // case REQUEST_SUCCESS:
+
+    // case REQUEST_FAIL:
+
+    // case ADD_SMURF:
+
+    // case GET_SMURFS:
+
+    // case UPDATE_SMURF:
+
+    // case DELETE_SMURF:
+
+    default:
+      return state;
+   }
+ }
+
+ export default reducer;
 
 /*
   You'll only need one smurf reducer for this project.
