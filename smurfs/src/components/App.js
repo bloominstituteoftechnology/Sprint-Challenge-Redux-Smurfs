29099df54c8c3721b0dc22f89d0ad2560e53c317
David Freitag <freitagdavid@outlook.com>
2018-12-07 11:21:47 -0500

    smurfs/src/components/App: Got the fetch working

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfsList from './SmurfsList';
 import SmurfsForm from './SmurfsForm';
+import { fetchSmurfs } from './../actions';
+import { connect } from 'react-redux';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -9,14 +11,33 @@ import SmurfsForm from './SmurfsForm';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+    componentDidMount() {
+        this.props.fetchSmurfs();
+    }
+
     render() {
         return (
             <div className="App">
-                <SmurfsList />
+                <SmurfsList smurfs={this.props.smurfs} />
                 <SmurfsForm />
             </div>
         );
     }
 }
 
-export default App;
+const mapStateToProps = state => {
+    console.log(state);
+    return {
+        smurfs: state.smurfs,
+        fetching: state.fetchingSmurfs
+    };
+};
+
+const mapDispatchToProps = dispatch => ({
+    fetchSmurfs: () => dispatch(fetchSmurfs())
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
