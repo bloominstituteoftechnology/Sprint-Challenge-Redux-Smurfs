d85742191197a400eca8e6ac94cb1ab4f00d10b5
David Freitag <freitagdavid@outlook.com>
2018-12-07 13:16:50 -0500

    smurfs/src/components/SmurfsList: Adding unique key

--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Smurf from './Smurf';
+import shortid from 'shortid';
 const SmurfsList = props => {
     return (
         <ul>
             {props.smurfs.map(smurf => (
-                <Smurf smurf={smurf} />
+                <Smurf key={shortid.generate()} smurf={smurf} />
             ))}
         </ul>
     );
