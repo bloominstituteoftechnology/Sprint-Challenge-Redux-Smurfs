4a00764353776120d7a2b5c510b166d226f30d5c
ToMMaaY <thuynh629@gmail.com>
2018-12-07 08:57:41 -0800

    Created my actions and reducers

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,119 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  FETCHING_SMURFS,
+  FETCH_SUCCESS,
+  FETCH_FAILURE,
+  ADD_START,
+  ADD_SUCCESS,
+  ADD_FAILURE,
+  UPDATE_START,
+  UPDATE_SUCCESS,
+  UPDATE_FAILURE,
+  DELETE_START,
+  DELETE_SUCCESS,
+  DELETE_FAILURE
+} from "../actions";
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  error: null
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+
+    case FETCH_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+
+    // Add New Friend //
+
+    case ADD_START:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+
+    case ADD_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+
+    case ADD_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+
+    // Update Friend //
+
+    case UPDATE_START:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+
+    case UPDATE_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+
+    case UPDATE_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+
+    // Delete Friend //
+
+    case DELETE_START:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+
+    case DELETE_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+
+    case DELETE_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducers;
