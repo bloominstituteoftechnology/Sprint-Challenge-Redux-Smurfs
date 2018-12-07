ea48977f81eee3bfbed952c87e43a7ae92a73620
David Freitag <freitagdavid@outlook.com>
2018-12-07 11:28:04 -0500

    smurfs/src/components/SmurfsList: Got the smurfs rendering

--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Smurf from './Smurf';
 const SmurfsList = props => {
     return (
-        <>
-            <h1>Hey I'm gonna be a list</h1>
-        </>
+        <ul>
+            {props.smurfs.map(smurf => (
+                <Smurf smurf={smurf} />
+            ))}
+        </ul>
     );
 };
 
