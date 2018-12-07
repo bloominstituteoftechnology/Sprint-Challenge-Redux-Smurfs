149f6c47387a78d1fca01ae9407171af19fd22f0
jheinz1999 <jonathan.heinz@aol.com>
2018-12-07 08:32:49 -0800

    finished index.js

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -16,8 +16,14 @@
 
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default function() {
+
+  
+
+}
