ec683a04029395401dc8796a45379c42be700c02
David Freitag <freitagdavid@outlook.com>
2018-12-07 11:11:56 -0500

    smurfs/src/components/SmurfsList: Placeholder

---
+++ smurfs/src/components/SmurfsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Smurf from './Smurf';
+const SmurfsList = props => {
+    return (
+        <>
+            <h1>Hey I'm gonna be a list</h1>
+        </>
+    );
+};
+
+export default SmurfsList;
