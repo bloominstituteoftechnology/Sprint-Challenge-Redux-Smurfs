5569e44e59bdab39785e5f1fde4504629112ab0c
jheinz1999 <jonathan.heinz@aol.com>
2018-12-07 08:41:52 -0800

    finished action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,16 @@
-/* 
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const ADDING = 'ADDING';
+export const UPDATING = 'UPDATING';
+export const DELETING = 'DELETING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +22,55 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const fetchSmurfs = () => dispatch => {
+
+  dispatch({type: FETCHING});
+
+  axios.get('http://localhost:3333/smurfs/')
+    .then(res => dispatch({
+      type: SUCCESS,
+      payload: res.data
+    }))
+    .catch(err => console.log(err));
+
+}
+
+export const addSmurf = smurf => dispatch => {
+
+  dispatch({type: ADDING});
+
+  axios.post('http://localhost:3333/smurfs/', smurf)
+    .then(res => dispatch({
+      type: SUCCESS,
+      payload: res.data
+    }))
+    .catch(err => console.log(err));
+
+}
+
+export const updateSmurf = smurf => dispatch => {
+
+  dispatch({type: UPDATING});
+
+  axios.put(`http://localhost:3333/smurfs/${smurf.id}`, smurf)
+    .then(res => dispatch({
+      type: SUCCESS,
+      payload: res.data
+    }))
+    .catch(err => console.log(err));
+
+}
+
+export const deleteSmurf = smurf => dispatch => {
+
+  dispatch({type: DELETING});
+
+  axios.delete(`http://localhost:3333/smurfs/${smurf.id}`)
+    .then(res => dispatch({
+      type: SUCCESS,
+      payload: res.data
+    }))
+    .catch(err => console.log(err));
+
+}
