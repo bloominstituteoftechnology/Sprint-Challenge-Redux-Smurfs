0d91f8308787e87d966e5959f89089f1eddb0b82
tryingtokeepup <kl.hawaii@gmail.com>
2018-12-07 08:40:34 -0800

    Set up the actions, going to reducers now

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,35 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+
+export const FETCH_SMURFS_REQUEST = 'FETCH_SMURFS_REQUEST';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
+
+export const ADD_SMURFS_REQUEST = 'ADD_SMURFS_REQUEST';
+export const ADD_SMURFS_SUCCESS = 'ADD_SMURFS_SUCCESS';
+export const ADD_SMURFS_FAILURE = 'ADD_SMURFS_FAILURE';
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: FETCH_SMURFS_REQUEST });
+    axios
+      .get('http://localhost:3333/smurfs/')
+      .then(response => {
+        console.log('smurf fetch finished');
+        dispatch({ type: FETCH_SMURF_SUCCESS, payload: response.data });
+      })
+      .catch(err => dispatch({ type: FETCH_SMURF_FAILURE, payload: err }));
+  };
+
+  export const addSmurf= smurf => dispatch => {
+    dispatch({ type: ADD_FRIENDS_REQUEST });
+    axios
+      .post('http://localhost:3333/smurfs/', smurf)
+      .then(response => {
+        console.log('fetch finished');
+        dispatch({ type: ADD_SMURF_SUCCESS, payload: response.data });
+      })
+      .catch(err => dispatch({ type: ADD_SMURF_FAILURE, payload: err }));
+  };
