35e00a6e3f4e265c95784dbc5f0a2303cc4e59f2
David Freitag <freitagdavid@outlook.com>
2018-12-07 10:49:47 -0500

    smurfs/src/reducers/index: Basic layout still needs tweaking

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,44 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAILURE } from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+    smurfs: [],
+    fetchingSmurfs: false,
+    addingSmurf: false,
+    updatingSmurf: false,
+    deletingSmurf: false,
+    error: null
+};
+
+export default (smurfReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_START:
+            return {
+                ...state,
+                fetchingSmurfs: true
+            };
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                fetchingSmurfs: false
+            };
+        case FETCH_FAILURE:
+            return {
+                ...state,
+                error: err
+            };
+        case ADD_SMURF:
+            return {
+                ...state,
+                smurfs: [...state.smurfs, payload.smurf]
+            };
+        case REMOVE_SMURF:
+            return {
+                ...state,
+                //TODO filter smurfs
+                smurfs: state.smurfs.filter()
+            };
+    }
+});
 
 /*
   You'll only need one smurf reducer for this project.
