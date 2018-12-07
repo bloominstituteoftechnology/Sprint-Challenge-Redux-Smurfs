11bc2f21d344e03677fe46380087da8357e4bc46
Dennis Orbison <orbison.dennis@gmail.com>
2018-12-07 05:55:45 -0800

    basic reducer created

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,3 +1,11 @@
+import { combineReducers } from 'redux';
+
+import {
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAILURE
+} from '../actions';
+
 /*
   Be sure to import in all of the action types from `../actions`
 */
@@ -21,3 +29,53 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const initialSmurfsState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null,
+};
+
+const smurfReducer = (state = initialSmurfsState, action) => {
+  switch (action.type) {
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        fetching: true
+      };
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetching: false,
+        doggos: action.payload
+      };
+    case FETCH_SMURFS_FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  smurfReducer
+});
+
+
+import {
+  ADD_TODO,
+  FETCH_DOGGOS_START,
+  FETCH_DOGGOS_SUCCESS,
+  FETCH_DOGGOS_FAILURE
+} from '../actions';
+
+
+
+
