21aec03670c2d3df8fc2ec4a3a6c65d7ad4e4069
Thomas Claydon <tomclaydon102@gmail.com>
2018-12-07 11:04:50 -0500

    Wired Reducer and Actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,16 @@
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+export const FETCH_SUMRF_START = "FETCH_SUMRF_START";
+export const FETCH_SUMRF_SUCCESS = "FETCH_SUMRF_SUCCESS";
+export const FETCH_SUMRF_FAILURE = "FETCH_SUMRF_FAILURE";
+
+export const ADD_SUMRF_START = "ADD_SUMRF_START";
+export const ADD_SUMRF_SUCCESS = "ADD_SUMRF_SUCCESS";
+export const ADD_SUMRF_FAILURE = "ADD_SUMRF_FAILURE";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +22,29 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => {
+  dispatch({ type: FETCH_SUMRF_START });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      console.log('fetch finished');
+      dispatch({ type: FETCH_SUMRF_SUCCESS, payload: response.data });
+    })
+    .catch(err => dispatch({ type: FETCH_SUMRF_FAILURE, payload: err }));
+};
+
+
+export const addSmurf = item => dispatch => {
+  dispatch({ type: ADD_SUMRF_START });
+  axios
+    .post('http://localhost:3333/smurfs', item)
+    .then(response => {
+      console.log(response);
+      dispatch({
+        type: ADD_SUMRF_SUCCESS,
+        payload: response.data
+      });
+    })
+    .catch(err => dispatch({ type: ADD_SUMRF_FAILURE, payload: err }));
+};
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,17 +2,26 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+import {
+  FETCH_SUMRF_START,
+  FETCH_SUMRF_SUCCESS,
+  FETCH_SUMRF_FAILURE,
+  ADD_SUMRF_START,
+  ADD_SUMRF_SUCCESS,
+  ADD_SUMRF_FAILURE
+} from "../actions";
+
+
+ //Your initial/default state for this project could *Although does not have to* look a lot like this
+ initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetching: false,
    error: null
  }
-*/
+
+ // addingSmurf: false
+  //  updatingSmurf: false
+  //  deletingSmurf: false
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +30,46 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default smurfReducer = (state = initialState, action) => {
+  switch(action.type){
+    case FETCH_SUMRF_START:
+      return{
+        ...state,
+        fetching: true
+      };
+    case FETCH_SUMRF_SUCCESS:
+      return{
+        ...state,
+        error: null,
+        fetching: false,
+        smurfs: action.payload
+      };
+    case FETCH_SUMRF_FAILURE:
+      return{
+        ...state,
+        fetching: false,
+        error: action.payload
+      };
+    case ADD_SUMRF_START:
+      return{
+        ...state,
+        fetching: true
+      };
+    case ADD_SUMRF_SUCCESS:
+      return{
+        ...state,
+        fetching: false,
+        error: false,
+        smurfs: action.payload
+      };
+    case ADD_SUMRF_FAILURE:
+      return{
+        ...state,
+        fetching: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
