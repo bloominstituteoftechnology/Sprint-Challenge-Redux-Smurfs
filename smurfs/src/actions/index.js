5fb6c5ab65371af0a98b34530ecaeb59cf8a1c9c
Dennis Orbison <orbison.dennis@gmail.com>
2018-12-07 06:15:06 -0800

    exporting actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,17 @@
+
+export const FETCH_SMURFS_START = "  FETCH_SMURFS_START"
+export const FETCH_SMURFS_SUCCESS = "  FETCH_SMURFS_SUCCESS"
+export const FETCH_SMURFS_FAILURE = "  FETCH_SMURFS_FAILURE"
+export const ADD_SMURFS_START = "  ADD_SMURFS_START"
+export const ADD_SMURFS_SUCCESS = "  ADD_SMURFS_SUCCESS"
+export const ADD_SMURFS_FAILURE = "  ADD_SMURFS_FAILURE"
+export const UPDATE_SMURFS_START = "  UPDATE_SMURFS_START"
+export const UPDATE_SMURFS_SUCCESS = "  UPDATE_SMURFS_SUCCESS"
+export const UPDATE_SMURFS_FAILURE = "  UPDATE_SMURFS_FAILURE"
+export const DELETE_SMURFS_START = "  DELETE_SMURFS_START"
+export const DELETE_SMURFS_SUCCESS = "  DELETE_SMURFS_SUCCESS"
+export const DELETE_SMURFS_FAILURE = "  DELETE_SMURFS_FAILURE"
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
