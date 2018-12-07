141d943f7b88cbb26f4e9d8c87b4e20071ed540d
Grace <gracekang.93@gmail.com>
2018-12-07 12:35:41 -0800

    adds actions & reducers

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,11 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE"; 
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +18,80 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_START });
+    axios
+        .get('http://localhost:3333/smurfs')
+        .then(({data}) => {
+            dispatch({ 
+                type: FETCH_SUCCESS, 
+                payload: data 
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: FETCH_FAILURE,
+                payload: err
+            })
+        })
+}
+
+export const addSmurf = newSmurf => dispatch => {
+  axios
+      .post('http://localhost:3333/smurfs', {
+          name: newSmurf.name,
+          age: newSmurf.age,
+          height: newSmurf.height
+      })
+      .then(({data}) => {
+          dispatch({
+              type: FETCH_SUCCESS,
+              payload: data
+          })
+      })
+      .catch(err => {
+          dispatch({
+              type: FETCH_FAILURE,
+              payload: err
+          })
+      })
+}
+
+export const editSmurf = (editedSmurf, id) => dispatch => {
+    axios
+        .put(`http://localhost:3333/smurfs/${id}`, {
+            name: editedSmurf.name,
+            age: editedSmurf.age,
+            height: editedSmurf.height,
+        })
+        .then(({data}) => {
+            dispatch({
+                type: FETCH_SUCCESS,
+                payload: data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: FETCH_FAILURE,
+                payload: err
+            })
+        })
+}
+
+export const deleteSmurf = id => dispatch => {
+    axios
+        .delete(`http://localhost:3333/smurfs/${id}`)
+        .then(({data}) => {
+            dispatch({
+                type: FETCH_SUCCESS,
+                payload: data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: FETCH_FAILURE,
+                payload: err
+            })
+        })
+}
