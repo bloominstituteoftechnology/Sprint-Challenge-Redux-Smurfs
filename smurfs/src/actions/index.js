e78c3a830e5a6fcc645d35aba2b98c555b4412a7
kevinmarker1101 <kmarker1101@icloud.com>
2018-12-07 12:00:41 -0600

    add smurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,7 +4,13 @@ export const FETCHING_SMURFS = 'FETCHING_SMURFS';
 export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
 export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
 
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADDING_SMURF_SUCCESS = 'ADDING_SMURF_SUCCESS';
+export const ADDING_SMURF_FAILURE = 'ADDING_SMURF_FAILURE';
+
+
 const url = 'http://localhost:3333/smurfs'
+
 export const getSmurfs = () => dispatch => {
     dispatch({type: FETCHING_SMURFS});
     axios.get(url)
@@ -15,7 +21,18 @@ export const getSmurfs = () => dispatch => {
         .catch(({data}) => dispatch({
             type: FETCHING_SMURFS_FAILURE,
             payload: data
-        }))
+        }));
 };
 
-
+export const addSmurf = smurf => dispatch => {
+    dispatch({type: ADDING_SMURF});
+    axios.post(url, smurf)
+        .then(({data}) => dispatch({
+            type: ADDING_SMURF_SUCCESS,
+            payload: data
+        }))
+        .catch(({data}) => dispatch({
+            type: ADDING_SMURF_FAILURE,
+            payload: data
+        }));
+}
