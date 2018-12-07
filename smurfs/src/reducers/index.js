c3008d831e512100da0c2ff02dcc99852ebca0c8
BrandCam <BrandCam>
2018-12-07 11:09:37 -0600

    Redux store set up kinda I think

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,21 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  ADD_SMURF_START,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAILURE,
+  GET_SMURFS_START,
+  GET_SMURFS_SUCCESS,
+  GET_SMURFS_FAILURE
+} from "../actions";
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+//Your initial/default state for this project could *Although does not have to* look a lot like this
+const initialState = {
+  smurfs: [],
+  gettingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +24,46 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SMURFS_START:
+      return {
+        ...state,
+        gettingSmurfs: true
+      };
+    case GET_SMURFS_SUCCESS:
+      return {
+        ...state,
+        err: null,
+        gettingSmurfs: false,
+        smurfs: action.payload
+      };
+    case GET_SMURFS_FAILURE:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        err: action.payload
+      };
+    case ADD_SMURF_START:
+      return {
+        ...state,
+        gettingSmurfs: true
+      };
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        smurfs: action.payload
+      };
+    case ADD_SMURF_FAILURE:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        err: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default smurfReducer;
