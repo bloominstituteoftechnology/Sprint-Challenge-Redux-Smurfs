7a085f9c80252ac541b35a447190e1727159e07e
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2018-12-08 19:20:25 -0600

    adding smurf working

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,50 @@
+import axios from 'axios'
+import reducers from '../reducers';
+
+export const FETCH_START = 'FETCH_START';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAIL = 'FETCH_FAIL';
+export const ADD_SMURF_START = 'ADDING_START';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
+
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_START });
+ axios
+  .get('http://localhost:3333/smurfs')
+  .then(res => {
+     console.log(res.data, 'inside fetch')
+     dispatch({
+             type: FETCH_SUCCESS,
+             payload: res.data
+         })
+     })
+     .catch(err => {
+         dispatch({
+             type: FETCH_FAIL,
+             payload: err
+         })
+ })
+}
+
+export const addingSmurf = (data) => dispatch => {
+  dispatch({type: ADD_SMURF_START});
+  axios.post('http://localhost:3333/smurfs', data)
+    .then(res => {
+      dispatch({
+        type: ADD_SMURF_SUCCESS,
+        payload: reducers.data
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: ADD_SMURF_FAIL,
+        payload: err
+      })
+    })
+}
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import SmurfsList from '../components/SmurfsList';
+import Smurf from '../components//Smurf';
+import SmurfForm from '../components/SmurfForm'
 import './App.css';
+import { connect } from 'react-redux';
+import { fetchSmurfs } from '../actions/';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +12,34 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  constructor() {
+    super();
+  }
+  
+  componentDidMount () {
+    this.props.fetchSmurfs()
+  }
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
+        <SmurfForm/>
+          <SmurfsList 
+          smurfs={this.props.smurfs}
+          />
+        
+      
+
+        </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log(state, 'inside mSTP')
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  }
+}
+
+export default connect(mapStateToProps, {fetchSmurfs}) (App);
