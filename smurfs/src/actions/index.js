81d688c82bbef9ad561a84ac6473e5d3e423162c
Angelo Deleon <rudy209@comcast.net>
2018-12-10 10:06:46 -0800

    Fixed actions file

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
+import axios from 'axios';
+// ACTION TYPES
+export const GET_SMURFS = 'GET_SMURFS';
+export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS';
+export const GET_SMURFS_FAILURE = 'GET_SMURFS_FAILURE';
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+/*
+  For this project you'll need at least 2 action creators for the main portion,
+   and 2 more for the stretch problem.
+   Be sure to include action types for each type of action creator. Also, be sure to mind
+     the "pending" states like, fetching, creating, updating and deleting.
+   C - addSmurf
+   R - getSmurfs
+   U - updateSmurf
+   D - deleteSmurf
+*/
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: GET_SMURFS });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: GET_SMURFS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: GET_SMURFS_FAILURE, payload: err });
+    });
+};
