36cdca2e6dae2de4b9f190e3f809f119f33d6aa5
chrisgins <christiansgina@gmail.com>
2018-12-13 14:34:49 -0700

    init make directories and files

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,46 @@
+import { combineReducers } from 'redux'
+import { smurfin } from './smurfin'
+
+
+export default combineReducers({
+  smurfin
+})
+
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import { LOADING, FETCH_SMURFS, ADD_SMURF, UPDATE_SMURF, DELETE_SMURF, ERROR } from '../actions'
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
 */
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const intialState = {
+    smurfs: [],
+    loading: false,
+    fetchingSmurfs: false,
+    addingSmurf: false,
+    updatingSmurf: false,
+    deletingSmurf: false,
+    error: null
+}
+
+
+export const smurfin = (state = intialState, action) => {
+
+    switch (action.type) {
+        case LOADING:
+            return { ...state, loading: true }
+        case FETCH_SMURFS:
+            return { ...state, smurfs: action.payload, loading: false }
+        case ADD_SMURF:
+            return { ...state, smurfs: action.payload, addingSmurf: true, loading: false }
+        case UPDATE_SMURF:
+            return { ...state, smurfs: action.payload, updatingSmurf: true, loading: false }
+        case DELETE_SMURF:
+            return { ...state, smurfs: action.payload, deletingSmurf: true, loading: false }
+        case ERROR:
+            return { ...state, error: action.payload, loading: false }
+        default:
+            return state
+    }
+}
