dad9ac469135f7ab38684db69fe8ed1742947317
Jarrad Miller <jm020801@gmail.com>
2018-12-14 11:19:31 -0700

    redux smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,14 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const LOADING = 'LOADING';
+export const COMPLETE = 'COMPLETE';
+export const ERROR = 'ERROR';
+export const ADDING = 'ADDING';
+export const UPDATING = 'UPDATING';
+export const DELETING = 'DELETING';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +19,100 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfsAction = () => {
+  return dispatch => {
+    dispatch({
+      type: LOADING,
+    })
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log(response);
+        dispatch({
+          type: COMPLETE,
+          payload: response.data
+        })
+      })
+      .catch(error => {
+        dispatch({
+          type: ERROR,
+          payload: 'Unable to load Smurfs from server. Please refresh page to try again.'
+        })
+      })
+  }
+}
+export const addSmurfAction = (nameValue, ageValue, heightValue) => {
+  return dispatch => {
+    dispatch({ type: ADDING });
+    axios
+      .post('http://localhost:3333/smurfs', {
+        name: nameValue,
+        age: parseInt(ageValue, 10),
+        height: heightValue,
+      })
+      .then(response => {
+        console.log(response);
+        dispatch({
+          type: COMPLETE,
+          payload: response.data,
+        })
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch({
+          type: ERROR,
+          payload: 'Unable to add new Smurf. Please refresh page and try again.',
+        })
+      })
+  }
+}
+export const updateSmurfAction = (nameValue, ageValue, heightValue, idValue) => {
+  return dispatch => {
+    dispatch({
+      type: UPDATING,
+    })
+    axios
+      .put(`http://localhost:3333/smurfs/${idValue}`, {
+        name: nameValue,
+        age: parseInt(ageValue, 10),
+        height: heightValue,
+      })
+      .then(response => {
+        console.log(response);
+        dispatch({
+          type: COMPLETE,
+          payload: response.data,
+        })
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch({
+          type: ERROR,
+          payload: 'Unable to update this Smurf. Please refresh the page and try again.',
+        })
+      })
+  }
+}
+export const deleteSmurfAction = (idValue) => {
+  return dispatch => {
+    dispatch({
+      type: DELETING,
+    })
+    axios
+      .delete(`http://localhost:3333/smurfs/${idValue}`)
+      .then(response => {
+        console.log(response);
+        dispatch({
+          type: COMPLETE,
+          payload: response.data,
+        })
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch({
+          type: ERROR,
+          payload: 'Unable to delete the smurf, Please try again',
+        })
+      })
+  }
+}
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,12 +6,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import smurfsReducer from './reducers';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+const middleware = applyMiddleware(logger, thunk);
+const store = createStore(smurfsReducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
