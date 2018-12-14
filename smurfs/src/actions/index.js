6722c30d5608afa4ecf14d30a0e6f2d3771e986c
1@$@1github <asaofdiamonds@gmail.com>
2018-12-14 10:30:56 -0700

    add smurf action created

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,11 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from "axios";
+
+export const ADD_SMURF_START = 'ADD_SMURF_START';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAILURE = 'ADD_SMURF_FAILURE';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +18,19 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: ADD_SMURF_START });
+  axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(response => {
+      console.log(response);
+      dispatch({
+        type: ADD_SMURF_SUCCESS,
+        payload: response.data
+      });
+    })
+    .catch(err => dispatch({ type: ADD_SMURF_FAILURE, payload: err }))
+};
+
+
