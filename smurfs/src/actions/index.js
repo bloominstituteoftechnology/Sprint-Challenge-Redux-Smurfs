30684284880f0adf6fcd02c108de1a86cd97184f
1@$@1github <asaofdiamonds@gmail.com>
2018-12-14 10:59:23 -0700

    smurfsReducer created

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -8,6 +8,10 @@ export const ADD_SMURF_START = 'ADD_SMURF_START';
 export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
 export const ADD_SMURF_FAILURE = 'ADD_SMURF_FAILURE';
 
+export const FETCH_SMURF_START = 'FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,14 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import { 
+  FETCH_SMURF_FAILURE,
+  FETCH_SMURF_START,
+  FETCH_SMURF_SUCCESS,
+  ADD_SMURF_START,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAILURE
+ } from "../actions";
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -14,6 +21,12 @@
  }
 */
 
+const initState = {
+  smurfs: [],
+  error: null,
+  fetchingSmurfs: false
+};
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +34,47 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const smurfsReducer = (state = initState, action) => {
+  switch (action.type) {
+    case ADD_SMURF_START:
+      return{
+        ...state,
+        fetchingSmurfs: true
+      };
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+    case ADD_SMURF_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+    case FETCH_SMURF_START:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+    case FETCH_SMURF_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+    case FETCH_SMURF_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+    default:
+      return state;      
+  }
+};
+
+export default smurfsReducer;
