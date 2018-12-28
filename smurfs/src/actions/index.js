430248d2ba3a8cff078e202c82ef0712dee0827b
Ken Barlow <kenbrlow@gmail.com>
2018-12-28 15:48:38 -0800

    Created add smurf const.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -8,8 +8,10 @@ export const SMURF_DELETE = "SMURF_DELETE";
 
 export const SMURF_GET = "SMURF_GET";
 export const SMURF_ADDING = "SMURF_ADDING";
+export const SMURF_ADDED = "SMURF_ADDED"; 
 export const SMURF_FETCHED = "SMURF_FETCHED"; 
 export const SMURF_ERROR = "SMURF_ERROR";
+
 
 // get
 export const getSmurfs = () =>  {
@@ -28,6 +30,20 @@ export const getSmurfs = () =>  {
 };
 
 // post
+export const addSmurf = (smurf) =>  {
+  return dispatch => {
+    dispatch({ type: SMURF_ADDING});
+
+    axios
+    .post(URL, smurf)
+    .then(response => {
+      dispatch({type: SMURF_ADDED, payload: response.data});
+    })
+    .catch(error => {
+      dispatch({type: SMURF_ERROR, payload: error});
+    });
+  }
+};
 
 // put
 
