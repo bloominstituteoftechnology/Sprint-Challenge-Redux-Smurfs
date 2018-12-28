4d2f588eaf079f7fb3af5e405efaab9d8241a02f
Ken Barlow <kenbrlow@gmail.com>
2018-12-28 15:55:56 -0800

    const clean up following mvp

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,21 +2,19 @@ import axios from 'axios';
 
 const URL = "http://localhost:3333/smurfs"
 
-export const SMURF_POST = "SMURF_POST";
-export const SMURF_PUT = "SMURF_PUT";
-export const SMURF_DELETE = "SMURF_DELETE";
-
-export const SMURF_GET = "SMURF_GET";
-export const SMURF_ADDING = "SMURF_ADDING";
-export const SMURF_ADDED = "SMURF_ADDED"; 
+export const SMURF_GET = "SMURF_FETCH";
 export const SMURF_FETCHED = "SMURF_FETCHED"; 
+
+export const SMURF_ADDING = "SMURF_ADD";
+export const SMURF_ADDED = "SMURF_ADDED"; 
+
 export const SMURF_ERROR = "SMURF_ERROR";
 
 // TODO - review if should add promise.
 // get
 export const getSmurfs = () =>  {
   return dispatch => {
-    dispatch({ type: SMURF_GET});
+    dispatch({ type: SMURF_FETCH});
 
     axios
     .get(URL)
@@ -32,7 +30,7 @@ export const getSmurfs = () =>  {
 // post
 export const addSmurf = (smurf) =>  {
   return dispatch => {
-    dispatch({ type: SMURF_ADDING});
+    dispatch({ type: SMURF_ADD});
 
     axios
     .post(URL, smurf)
@@ -44,8 +42,3 @@ export const addSmurf = (smurf) =>  {
     });
   }
 };
-
-// put
-
-
-// delete
