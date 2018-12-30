793bfe2717394aff69111b405e57b69b0dd8df65
kingmetri <demetribethel@gmail.com>
2018-12-30 10:49:36 -0500

    Moving onto components for a visual, have defined erros.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,20 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case FETCHING:
+      return Object.assign({}, state, { fetchingSmurfs: true })
+    case GET_SMURFS:
+      return Object.assign({}, state, { fetchingSmurfs: false, error: "", smurfs: action.smurfs })
+    case ERROR:
+      return  Object.assign({}, state, { fetchingSmurfs: false, error: action.error })
+    default:
+      return state
+  }
+}
+
+
+
+export default reducer;
