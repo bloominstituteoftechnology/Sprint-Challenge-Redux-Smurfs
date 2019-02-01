24450dfdc17e3fe2c3b3d44550e74f77a842b189
john2796 <jbmiranda22796@gmail.com>
2019-02-01 08:15:24 -0800

    init

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import /* You need some sort of reducer */ "./reducers";
 
 const store = createStore(
   () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
@@ -17,5 +18,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
