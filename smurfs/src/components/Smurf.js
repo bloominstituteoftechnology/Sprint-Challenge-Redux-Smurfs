522abbe6a6eebbfe859a88cf6061e8e190f5ab52
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-02-09 13:09:22 -0500

    ADD FRIEND COMPONENT, PASS DELETE FX AND EDITFORM TOGGLE

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { deleteSmurf, editForm } from '../actions';
+
+const Smurf = props => {
+    return (
+        <div className="smurf-card">
+        
+            <div className="card-detail">
+                <h2>{props.smurf.name}</h2>
+            </div>
+            <div className="card-detail">
+                <h3>{props.smurf.age}</h3>
+            </div>
+            <div className="card-detail">
+                <h4>{props.smurf.email}</h4>
+            </div>
+            <button variant="warning" onClick={() => props.editForm(props.smurf)}>
+                Edit 
+            </button>
+            <button variant="danger" onClick={() => props.deleteSmurf(props.smurf.id)}>
+                Delete 
+            </button>    
+            
+        </div>
+            
+    )
+}
+
+
+export default connect(null, {deleteSmurf, editForm} )(Smurf);
