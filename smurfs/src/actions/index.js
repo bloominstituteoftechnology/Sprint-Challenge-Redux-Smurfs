e68fff066d091c6bbd72732e06d7262af15ccd08
thomasutsey <tomutsey@yahoo.com>
2019-02-23 12:39:59 -0500

    Set up index with store and thunk
    Add actions to the actions folder for mvp we need to get and create the smurfs.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,46 @@
+import axios from 'axios';
+
+export const ERROR = 'ERROR';
+export const GET_SMURFS = 'GET_SMURFS';
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const CREATING_SMURF = 'CREATING_SMURF';
+export const CREATE_SMURF = 'CREATE_SMURF';
+
+
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+const URL = 'http://localhost:3333/smurfs';
 
+export const getSmurfs = () => {
+  const friends = axios.get(`${URL}/get`);
+  return dispatch => {
+    dispatch({ type: GETTING_SMURFS });
+    smurfs
+      .then(response => {
+        dispatch({ type: GET_SMURFS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
+
+export const createSmurf = smurf => {
+  const newSmurf = axios.post(`${URL}/create`, smurf);
+  return dispatch => {
+    dispatch({ type: CREATING_SMURF });
+    newSmurf
+      .then(({ data }) => {
+        dispatch({ type: CREATE_SMURF, payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
