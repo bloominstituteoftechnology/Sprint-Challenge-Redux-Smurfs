e509ce15cf1f21baf732070de958ee86eb394953
Jor_Poon <jormpoon@gmail.com>
2019-03-08 12:38:21 -0500

    maptostattoprops not working right

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import SmurfsList from './SmurfsList';
 import './App.css';
+import {connect} from "react-redux";
+import {getSmurfs} from "../actions"
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +10,29 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  constructor() {
+    super();
+  }
+  
+  componentDidMount() {
+    this.props.getSmurfs();
+}
+
   render() {
+    console.log(this.props)
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfsList/>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  null,
+  {getSmurfs}
+)(App);;
---
+++ smurfs/src/components/SmurfsList.js
@@ -0,0 +1,31 @@
+import React from "react"
+import {connect} from "react-redux";
+
+
+class SmurfsList extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        console.log(this.props.smurfs)
+        return (
+            <div>
+                <h2>SMURFS LIST</h2>
+                {/* {this.props.smurfs.map(smurf => {
+                    return smurf;
+                })} */}
+            </div>
+        )
+    }
+  
+}
+
+const mapStateToProps = state => ({
+    smurfs: state
+});
+
+export default connect(
+    mapStateToProps,
+    {}
+)(SmurfsList);
