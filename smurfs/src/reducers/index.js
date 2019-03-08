a02e64f61ff330826e1b4db45ea12cf7fe371c95
Jor_Poon <jormpoon@gmail.com>
2019-03-08 11:46:01 -0500

    created get smurfs reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,40 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+import {
+  GET_SMURFS_START,
+  GET_SMURFS_FAILURE,
+  GET_SMURFS_SUCCESS
+} from "../actions"
+
+const initialState = {
+  smurfs: [],
+  gettingSmurfs: false,
+  //  addingSmurf: false
+  //  updatingSmurf: false
+  //  deletingSmurf: false
+  error: null
+}
+
+export const smurfReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case GET_SMURFS_START:
+      return {
+        ...state,
+        gettingSmurfs: true,
+      }
+    case GET_SMURFS_SUCCESS: 
+      return {
+        ...state,
+        smurfs: action.payload,
+        gettingSmurfs: false
+      }
+    case GET_SMURFS_FAILURE:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        err: action.payload
+      }
+  }
+}
