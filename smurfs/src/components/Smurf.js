ef82c4976cb692b698686b67a283b0d23630238c
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-08 10:44:14 +0000

    STRETCH: Added delete functionality

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,10 +1,17 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 
+const handleDeleteClick = (id, deleteSmurf) => (event) => {
+  event.preventDefault();
+  deleteSmurf(id);
+};
 const Smurf = (props) => {
-  const { name, height, age } = props;
+  const {
+    name, height, age, id, deleteSmurf,
+  } = props;
+
   return (
     <div className="smurf">
       <div className="smurf-name">
@@ -14,15 +21,19 @@ const Smurf = (props) => {
         <p><span>Height:</span>{height} tall</p>
         <p><span>Age:</span>{age} smurf years old</p>
       </div>
-      <button type="button">Ban from Village</button>
+      <div className="requests">
+        <button type="button" className="delete" onClick={handleDeleteClick(id, deleteSmurf)}>Ban from Village</button>
+      </div>
     </div>
   );
 };
 
 Smurf.propTypes = {
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   height: PropTypes.string.isRequired,
   age: PropTypes.number.isRequired,
+  deleteSmurf: PropTypes.func.isRequired,
 };
 
 export default Smurf;
--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,23 +1,26 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import Smurf from './Smurf';
 
+
 const Smurfs = (props) => {
-  const { smurfs } = props;
+  const { smurfs, deleteSmurf } = props;
   return (
-    <div className="smurfs-wrapper">
+    <div className="smurfs-container">
       <h2>Smurf Village</h2>
       {smurfs.map(smurf => (
         <Smurf
           name={smurf.name}
           age={smurf.age}
-          height={smurf.height}
           id={smurf.id}
           key={`${smurf.id}-${smurf.age}`}
+          height={smurf.height}
+          deleteSmurf={deleteSmurf}
         />
       ))}
     </div>
+
   );
 };
 
@@ -28,6 +31,7 @@ Smurfs.propTypes = {
     age: PropTypes.number.isRequired,
     height: PropTypes.string.isRequired,
   })).isRequired,
+  deleteSmurf: PropTypes.func.isRequired,
 };
 
 export default Smurfs;
