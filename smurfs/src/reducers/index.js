bf8e798e91f6ed41f9a53c2cf15412c26868fd01
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-08 08:35:55 +0000

    MVP: Created reducer and set initial state

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,8 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import { FETCH_SMURFS_SUCCESS, ADD_SMURF_SUCCESS, DELETE_SMURF_SUCCESS } from '../actions';
+
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -14,6 +16,8 @@
  }
 */
 
+const initialState = [];
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +25,18 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_SMURFS_SUCCESS:
+      return [...action.payload.smurfs];
+    case ADD_SMURF_SUCCESS:
+      return [...action.payload.newSmurfs];
+    case DELETE_SMURF_SUCCESS:
+      return [...action.payload.newSmurfs];
+    default:
+      return state;
+  }
+};
+
+export default reducer;
