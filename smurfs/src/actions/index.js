eb39f233da85358217e8a833639f9e22c4d16d12
jkaddu <jkaddu@yahoo.com>
2019-03-22 09:44:55 -0700

    reducer and actions built

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,36 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const ADDING = 'ADDING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE'
+
+export const getSmurfs = (dispatch) => {
+  dispatch = ({ type: FETCHING })
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => 
+      dispatch = ({ type: SUCCESS, payload: response.data})
+      )
+    .catch(error => {console.log(error)
+      dispatch = ({ type: FAILURE, payload: error})
+    })
+}
+
+export const addSmurf = (dispatch) => {
+  dispatch = ({ type: ADDING })
+  axios 
+    .post('http://localhost:3333/smurfs')
+    .then(response => 
+      dispatch = ({ type: SUCCESS, payload: response.data })
+      )
+    .catch(error => {console.log(error)
+      dispatch = ({ type: SUCCESS, payload: error})
+    })
+}
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,17 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import { FETCHING, ADDING, SUCCESS, FAILURE } from '../actions';
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
+ */
+const initialState =
  {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
    error: null
  }
-*/
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +20,24 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export const smurfReducer = (state = initialState, action) => {
+  switch(action.type){
+    case FETCHING: 
+      return {
+        ...state, fetching: true
+      }
+    case ADDING:
+      return {
+        ...state, fetching: true
+      }
+    case SUCCESS:
+      return {
+        ...state, smurfs: action.payload, fetching: false
+      }
+    case FAILURE: 
+      return {
+        ...state, error: action.payload, fetching: false
+      }
+    default: return {...state}
+  }
+}
