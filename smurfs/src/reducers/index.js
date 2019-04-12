c610a2e782b1cdc92bda5a99729e9d336d792624
Steve Lanier <stevekentlanier@gmail.com>
2019-04-12 09:14:47 -0700

    MVP complete, attempting to add keys to list items

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,20 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { FETCHING_SMURFS, SMURFS_FETCH_SUCCESS, SMURFS_FETCH_FAILURE, SMURF_ADDED } from '../actions';
 
 /*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+ Your initial/default state for this project could *Although does not have to* look a lot like this*/
+ 
+ const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
+
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +23,30 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+const smurfReducer = (state = initialState, action) => {
+  switch(action.type) {
+
+    case FETCHING_SMURFS:
+      return {...state, fetchingSmurfs: true};
+
+    case SMURFS_FETCH_SUCCESS:
+      return {...state,
+              fetchingSmurfs: false,
+              smurfs: [...state.smurfs, ...action.payload]
+      };
+
+    case SMURFS_FETCH_FAILURE:
+      return {
+        ...state, fetchingSmurfs: false, error: action.payload
+      };
+
+    case SMURF_ADDED:
+      return{...state, smurfs: [...action.payload] }
+      
+    default: 
+      return state;
+  }
+}
+
+
+export default smurfReducer;
