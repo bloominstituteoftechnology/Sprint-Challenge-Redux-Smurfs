825ab3e28bec5404fbb17b4aae2901bea4df40f9
Anna <townsendanna15@gmail.com>
2019-04-19 13:26:37 -0400

    stuck in index.js and not sure why my store isnt working

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,14 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios'
+export const FETCHING_SMURFS = 'FETCHING_SMURFS'
+export const FETCHING_SUCCESS = "FETCHING_SUCCESS"
+export const FETCHING_FAILURE = "FETCHING_FAILURE"
+export const ADD_SMURFS_START = "ADD_SMURFS_START"
+export const ADD_SMURFS = "ADD_SMURFS"
+export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE"
+
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +21,51 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getsmurf = smurf => dispatch =>{
+  dispatch({type: FETCHING_SMURFS})
+
+  axios.get(
+      `http://localhost:3333/smurfs`, smurf
+  )
+  .then(res =>{
+       console.log(res
+          )
+      dispatch({
+          type:FETCHING_SUCCESS,
+          payload: res.data, 
+
+      })
+  })
+  .catch(err =>{
+      dispatch({
+          type:FETCHING_FAILURE,
+          payload: err
+
+      })
+  })
+
+
+
+}
+
+export const addSmurf = (smurf) => dispatch =>{
+  axios.post(
+      `http://localhost:3333/smurfs`, smurf,
+  )
+      .then(res =>{
+          dispatch({ 
+              type:ADD_SMURFS,
+              payload: res.data 
+
+          })
+      })
+      .catch(err =>
+        dispatch({
+          type:ADD_SMURF_FAILURE,
+          payload:err
+        })
+        )
+      };
+
+
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,12 +6,12 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import rootReducer from './reducers';
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+/* be sure to throw in the proper middlewares here*/
+// this is the most basic reducer. A function that returns and object. Replace it.
+ 
 
 ReactDOM.render(
   <Provider store={store}>
