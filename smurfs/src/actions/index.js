9683b2a92aaa6ffbd6983f3927b2eba498130909
Nathan <s6nculve@gmail.com>
2019-05-17 21:03:52 -0700

    Added actions & Connected app component.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,33 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+export const ADD = 'ADD';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+const baseURL = 'http://localhost:3333/smurfs';
+
+ export const fetchSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: FETCHING });
+    axios.get(baseURL)
+      .then(response => {
+        dispatch({ type: FETCHED, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
+
+ export const addSmurfs = smurf => {
+  return dispatch => {
+    dispatch({ type: ADD });
+    axios.post(baseURL, smurf )
+      .then(() => {
+        dispatch(fetchSmurfs());
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
