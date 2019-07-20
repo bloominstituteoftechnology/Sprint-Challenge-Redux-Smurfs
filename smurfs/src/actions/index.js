2dc0208bdccbee6bee66da22184c4459c05ef166
Sir-Williams <sirjohnwilliams@live.com>
2019-07-19 18:25:19 -0700

    added addSmurf action

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
 export const FETCHING = 'FETCHING'
-
 export const FETCHING_SUCCESS = 'FETCHING_SUCCESS'
-
 export const FETCHING_FAILED = 'FETCHING_FAILED'
+
+export const ADD_SMURF = 'ADD_SMURF'
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS'
+export const ADD_SMURF_FAILED = 'ADD_SMURF_FAILED'
 
 export const getSmurfs = () => dispatch => {
   dispatch({ type: FETCHING })
@@ -17,3 +19,15 @@ export const getSmurfs = () => dispatch => {
       dispatch({ type: FETCHING_FAILED, payload: err.response })
     })
 }
+
+export const addSmurf = newSmurf => dispatch => {
+  dispatch({ type: ADD_SMURF })
+  axios
+    .post('http://localhost:3333/smurfs', newSmurf)
+    .then(response => {
+      dispatch({ type: ADD_SMURF_SUCCESS, payload: response.data })
+    })
+    .catch(err => {
+      dispatch({ type: ADD_SMURF_FAILURE, payload: err })
+    })
+} 
