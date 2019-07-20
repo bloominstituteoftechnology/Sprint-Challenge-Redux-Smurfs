3dc7708f5b205cb0283165285106c3ee9c99244a
Sir-Williams <sirjohnwilliams@live.com>
2019-07-19 17:35:30 -0700

    answered study Q's, added reducer and store middleware

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import logger from 'redux-logger'
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import reducer from './reducers';
 
 const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
+  reducer, // this is the most basic reducer. A function that returns and object. Replace it.
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
