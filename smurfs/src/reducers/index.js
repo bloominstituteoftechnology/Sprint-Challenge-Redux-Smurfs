10d8511aa9e3cfc01d81cc525999097fb487b0ed
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-07-20 11:09:56 -0700

    reducers are in and hopefully working

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,53 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  FETCH_SMURF,
+  CREATE_SMURF,
+  ADDING_SMURF_SUCC,
+  ADDING_SMURF_FAIL,
+  DELETING_SMURF
+} from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+    smurfs: [],
+    fetchingSmurfs: false,
+    addingSmurf: false,
+    updatingSmurf: false,
+    deletingSmurf: false,
+    error: null
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+function reducer( state = initialState, action) {
+  switch (action.type) {
+    case FETCH_SMURF:
+      return {
+        ...state,
+        error: '',
+        addingSmurf: false
+      };
+    case CREATE_SMURF:
+      return {
+        ...state,
+        error: '',
+        addingSmurf: true
+      }
+    case ADDING_SMURF_SUCC:
+      return {
+        ...state,
+        error: '',
+        smurfs: action.payload,
+        error: null
+      }
+    case ADDING_SMURF_FAIL: 
+      return {
+      ...state,
+      addingSmurf: false,
+      error: action.payload
+    }
+    case DELETING_SMURF:
+      return {
+        ...state,
+        smurfs: state.smurfs.filter(smurf => smurf.id !== action.payload)
+      }
+    default:
+      return state;
+  }
+}
